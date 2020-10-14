6d6e13d8a4ece05adeec4d812d0a1fe783d4e42a
streamich <streamich@gmail.com>
2020-10-14 11:29:14 +0200

    feat: 🎸 add batching to JsonRxClient

--- src/json-rx/JsonRxClient.ts
+++ src/json-rx/JsonRxClient.ts
@@ -1,9 +1,31 @@
-import {Message, MessageSubscribe, MessageError, MessageData, MessageComplete, MessageNotification} from './types';
+import {MessageSubscribe, MessageError, MessageData, MessageComplete, MessageNotification, MessageUnsubscribe, MessageOrMessageBatch} from './types';
 import {Observable, Observer} from 'rxjs';
 import {assertId, isArray} from './util';
+import {TimedQueue} from './TimedQueue';
+
+type IncomingMessage = MessageOrMessageBatch<MessageData | MessageComplete | MessageError>;
+type OutgoingMessage = MessageOrMessageBatch<MessageSubscribe | MessageUnsubscribe | MessageNotification>;
 
 export interface JsonRxClientParams {
-  send: (message: Message) => void;
+  /**
+   * Method to be called by client when it wants to send a message to the server.
+   * This is usually your WebSocket "send" method.
+   */
+  send: (message: OutgoingMessage) => void;
+
+  /**
+   * Number of messages to keep in buffer before sending them to the server.
+   * The buffer is flushed when the message reaches this limit or when the
+   * buffering time has reached the time specified in `bufferTime` parameter.
+   * Defaults to 100 messages.
+   */
+  bufferSize?: number;
+
+  /**
+   * Time in milliseconds for how long to buffer messages before sending them
+   * to the server. Defaults to 10 milliseconds.
+   */
+  bufferTime?: number;
 }
 
 interface ObserverEntry {
@@ -12,21 +34,28 @@ interface ObserverEntry {
 }
 
 export class JsonRxClient {
-  private send: (message: Message) => void;
   private cnt: number = 1;
+  private readonly buffer: TimedQueue<OutgoingMessage>;
 
   private readonly observers = new Map<number, ObserverEntry>();
 
-  constructor({send}: JsonRxClientParams) {
-    this.send = send;
+  constructor({send, bufferSize = 100, bufferTime = 10}: JsonRxClientParams) {
+    this.buffer = new TimedQueue();
+    this.buffer.itemLimit = bufferSize;
+    this.buffer.timeLimit = bufferTime;
+    this.buffer.onFlush = (messages) => {
+      send(messages.length === 1 ? messages[0] : messages as OutgoingMessage);
+    };
   }
 
   private sendSubscribe(message: MessageSubscribe): void {
-    this.send(message);
+    // this.send(message);
+    this.buffer.push(message);
   }
 
   private sendUnsubscribe(id: number): void {
-    this.send([-3, id]);
+    // this.send([-3, id]);
+    this.buffer.push([-3, id]);
   }
 
   private onData(message: MessageData): void {
@@ -56,7 +85,7 @@ export class JsonRxClient {
     observer.observer.error(error);
   }
 
-  public onMessage(message: MessageData | MessageComplete | MessageError): void {
+  public onMessage(message: IncomingMessage): void {
     if (!isArray(message)) throw new Error('Invalid message');
     const [type] = message;
     if (type === 0) return this.onComplete(message as MessageComplete);
@@ -81,11 +110,13 @@ export class JsonRxClient {
 
   public notify(name: string, payload?: unknown): void {
     const message: MessageNotification = payload !== undefined ? [name, payload] : [name];
-    this.send(message);
+    // this.send(message);
+    this.buffer.push(message);
   }
 
   public stop(): void {
-    this.send = (message: Message) => {};
+    // this.send = (message: OutgoingMessage) => {};
+    this.buffer.onFlush = (message) => {};
     for (const {observer} of this.observers.values())
       observer.complete();
   }
--- src/json-rx/__tests__/JsonRxClient.spec.ts
+++ src/json-rx/__tests__/JsonRxClient.spec.ts
@@ -3,80 +3,86 @@ import {take} from 'rxjs/operators';
 
 test('can create client', async () => {
   const send = jest.fn();
-  const client = new JsonRxClient({send});
+  const client = new JsonRxClient({send, bufferTime: 1});
 });
 
 test('does not send any messages on initialization', async () => {
   const send = jest.fn();
-  const client = new JsonRxClient({send});
-  await new Promise((r) => setTimeout(r, 10));
+  const client = new JsonRxClient({send, bufferTime: 1});
+  await new Promise((r) => setTimeout(r, 2));
   expect(send).toHaveBeenCalledTimes(0);
 });
 
 test('sends notification message on .notify() call', async () => {
   const send = jest.fn();
-  const client = new JsonRxClient({send});
+  const client = new JsonRxClient({send, bufferTime: 1});
   client.notify('foo', 'bar');
+  await new Promise((r) => setTimeout(r, 2));
   expect(send).toHaveBeenCalledTimes(1);
   expect(send).toHaveBeenCalledWith(['foo', 'bar']);
 });
 
 test('sends notification with no payload', async () => {
   const send = jest.fn();
-  const client = new JsonRxClient({send});
+  const client = new JsonRxClient({send, bufferTime: 1});
   client.notify('foo');
+  await new Promise((r) => setTimeout(r, 2));
   expect(send).toHaveBeenCalledTimes(1);
   expect(send).toHaveBeenCalledWith(['foo']);
 });
 
 test('returns Observable on new execution', async () => {
   const send = jest.fn();
-  const client = new JsonRxClient({send});
+  const client = new JsonRxClient({send, bufferTime: 1});
   const result = client.call('test', {foo: 'bar'});
+  await new Promise((r) => setTimeout(r, 2));
   expect(send).toHaveBeenCalledTimes(1);
   expect(typeof result.subscribe).toBe('function');
 });
 
 test('observable does not emit before it receives messages from server', async () => {
   const send = jest.fn();
-  const client = new JsonRxClient({send});
+  const client = new JsonRxClient({send, bufferTime: 1});
   const result = client.call('test', {foo: 'bar'});
   const sub = jest.fn();
   result.subscribe(sub);
-  await new Promise((r) => setTimeout(r, 10));
+  await new Promise((r) => setTimeout(r, 2));
   expect(sub).toHaveBeenCalledTimes(0);
 });
 
 test('sends subscription message to the server', async () => {
   const send = jest.fn();
-  const client = new JsonRxClient({send});
+  const client = new JsonRxClient({send, bufferTime: 1});
   const result = client.call('test', {foo: 'bar'});
+  await new Promise((r) => setTimeout(r, 2));
   expect(send).toHaveBeenCalledTimes(1);
   expect(send).toHaveBeenCalledWith([1, 'test', {foo: 'bar'}]);
 });
 
 test('sends un-subscription message to the server on unsubscribe', async () => {
   const send = jest.fn();
-  const client = new JsonRxClient({send});
+  const client = new JsonRxClient({send, bufferTime: 1});
   const result = client.call('test', {foo: 'bar'});
   const sub = jest.fn();
   const subscription = result.subscribe(sub);
+  await new Promise((r) => setTimeout(r, 2));
   subscription.unsubscribe();
-  await new Promise((r) => setTimeout(r, 1));
+  await new Promise((r) => setTimeout(r, 2));
   expect(send).toHaveBeenCalledTimes(2);
   expect(send).toHaveBeenCalledWith([-3, 1]);
 });
 
 test('server can immediately complete the subscription', async () => {
   const send = jest.fn();
-  const client = new JsonRxClient({send});
+  const client = new JsonRxClient({send, bufferTime: 1});
   const result = client.call('test', {foo: 'bar'});
   const next = jest.fn();
   const error = jest.fn();
   const complete = jest.fn();
   result.subscribe(next, error, complete);
-  await new Promise((r) => setTimeout(r, 1));
+  await new Promise((r) => setTimeout(r, 2));
   client.onMessage([0, 1]);
+  await new Promise((r) => setTimeout(r, 2));
   expect(next).toHaveBeenCalledTimes(0);
   expect(error).toHaveBeenCalledTimes(0);
   expect(complete).toHaveBeenCalledTimes(1);
@@ -84,13 +90,13 @@ test('server can immediately complete the subscription', async () => {
 
 test('server can immediately complete the subscription with payload', async () => {
   const send = jest.fn();
-  const client = new JsonRxClient({send});
+  const client = new JsonRxClient({send, bufferTime: 1});
   const result = client.call('test', {foo: 'bar'});
   const next = jest.fn();
   const error = jest.fn();
   const complete = jest.fn();
   result.subscribe(next, error, complete);
-  await new Promise((r) => setTimeout(r, 1));
+  await new Promise((r) => setTimeout(r, 20));
   client.onMessage([0, 1, {x: 'y'}]);
   expect(next).toHaveBeenCalledTimes(1);
   expect(error).toHaveBeenCalledTimes(0);
@@ -100,19 +106,19 @@ test('server can immediately complete the subscription with payload', async () =
 
 test('server can send multiple values before completing', async () => {
   const send = jest.fn();
-  const client = new JsonRxClient({send});
+  const client = new JsonRxClient({send, bufferTime: 1});
   const result = client.call('test', {foo: 'bar'});
   const next = jest.fn();
   const error = jest.fn();
   const complete = jest.fn();
   result.subscribe(next, error, complete);
-  await new Promise((r) => setTimeout(r, 1));
+  await new Promise((r) => setTimeout(r, 20));
   client.onMessage([-2, 1, 1]);
-  await new Promise((r) => setTimeout(r, 1));
+  await new Promise((r) => setTimeout(r, 20));
   client.onMessage([-2, 1, 2]);
-  await new Promise((r) => setTimeout(r, 1));
+  await new Promise((r) => setTimeout(r, 20));
   client.onMessage([0, 1, 3]);
-  await new Promise((r) => setTimeout(r, 1));
+  await new Promise((r) => setTimeout(r, 20));
   expect(next).toHaveBeenCalledTimes(3);
   expect(error).toHaveBeenCalledTimes(0);
   expect(complete).toHaveBeenCalledTimes(1);
@@ -123,20 +129,20 @@ test('server can send multiple values before completing', async () => {
 
 test('values are not emitted after observable is unsubscribed', async () => {
   const send = jest.fn();
-  const client = new JsonRxClient({send});
+  const client = new JsonRxClient({send, bufferTime: 1});
   const result = client.call('test', {foo: 'bar'});
   const next = jest.fn();
   const error = jest.fn();
   const complete = jest.fn();
   const subscription = result.subscribe(next, error, complete);
-  await new Promise((r) => setTimeout(r, 1));
+  await new Promise((r) => setTimeout(r, 20));
   client.onMessage([-2, 1, 1]);
-  await new Promise((r) => setTimeout(r, 1));
+  await new Promise((r) => setTimeout(r, 20));
   client.onMessage([-2, 1, 2]);
-  await new Promise((r) => setTimeout(r, 1));
+  await new Promise((r) => setTimeout(r, 20));
   subscription.unsubscribe();
   client.onMessage([0, 1, 3]);
-  await new Promise((r) => setTimeout(r, 1));
+  await new Promise((r) => setTimeout(r, 20));
   expect(next).toHaveBeenCalledTimes(2);
   expect(error).toHaveBeenCalledTimes(0);
   expect(complete).toHaveBeenCalledTimes(0);
@@ -146,7 +152,7 @@ test('values are not emitted after observable is unsubscribed', async () => {
 
 test('can subscribe to multiple methods', async () => {
   const send = jest.fn();
-  const client = new JsonRxClient({send});
+  const client = new JsonRxClient({send, bufferTime: 1});
 
   const result1 = client.call('foo', 1);
   const next1 = jest.fn();
@@ -154,26 +160,28 @@ test('can subscribe to multiple methods', async () => {
   const complete1 = jest.fn();
   const subscription1 = result1.subscribe(next1, error1, complete1);
 
+  await new Promise((r) => setTimeout(r, 2));
+
   const result2 = client.call('bar', 2);
   const next2 = jest.fn();
   const error2 = jest.fn();
   const complete2 = jest.fn();
   const subscription2 = result2.subscribe(next2, error2, complete2);
 
-  await new Promise((r) => setTimeout(r, 1));
+  await new Promise((r) => setTimeout(r, 2));
 
   expect(send).toHaveBeenCalledTimes(2);
   expect(send).toHaveBeenCalledWith([1, 'foo', 1]);
   expect(send).toHaveBeenCalledWith([2, 'bar', 2]);
 
   client.onMessage([-2, 2, 'gg']);
-  await new Promise((r) => setTimeout(r, 1));
+  await new Promise((r) => setTimeout(r, 20));
 
   expect(next1).toHaveBeenCalledTimes(0);
   expect(next2).toHaveBeenCalledWith('gg');
 
   client.onMessage([-2, 1, 'lala']);
-  await new Promise((r) => setTimeout(r, 1));
+  await new Promise((r) => setTimeout(r, 20));
 
   expect(next1).toHaveBeenCalledWith('lala');
   expect(next1).toHaveBeenCalledTimes(1);
@@ -194,15 +202,15 @@ test('can subscribe to multiple methods', async () => {
 
 test('can respond with error', async () => {
   const send = jest.fn();
-  const client = new JsonRxClient({send});
+  const client = new JsonRxClient({send, bufferTime: 1});
   const result = client.call('test', {foo: 'bar'});
   const next = jest.fn();
   const error = jest.fn();
   const complete = jest.fn();
   const subscription = result.subscribe(next, error, complete);
-  await new Promise((r) => setTimeout(r, 1));
+  await new Promise((r) => setTimeout(r, 20));
   client.onMessage([-1, 1, 1]);
-  await new Promise((r) => setTimeout(r, 1));
+  await new Promise((r) => setTimeout(r, 20));
   expect(next).toHaveBeenCalledTimes(0);
   expect(error).toHaveBeenCalledTimes(1);
   expect(complete).toHaveBeenCalledTimes(0);
@@ -211,43 +219,51 @@ test('can respond with error', async () => {
 
 test('does not send unsubscribe when complete has been received', async () => {
   const send = jest.fn();
-  const client = new JsonRxClient({send});
+  const client = new JsonRxClient({send, bufferTime: 1});
   const result = client.call('test', {foo: 'bar'});
   const next = jest.fn();
   const error = jest.fn();
   const complete = jest.fn();
   const subscription = result.subscribe(next, error, complete);
-  await new Promise((r) => setTimeout(r, 1));
+  await new Promise((r) => setTimeout(r, 20));
   expect(send).toHaveBeenCalledTimes(1);
   expect(next).toHaveBeenCalledTimes(0);
   client.onMessage([0, send.mock.calls[0][0][0], 1]);
-  await new Promise((r) => setTimeout(r, 1));
+  await new Promise((r) => setTimeout(r, 20));
   expect(next).toHaveBeenCalledTimes(1);
   expect(send).toHaveBeenCalledTimes(1);
 });
 
 test('does not send unsubscribe when complete has been received - 2', async () => {
   const send = jest.fn();
-  const client = new JsonRxClient({send});
+  const client = new JsonRxClient({send, bufferTime: 1});
+  await new Promise((r) => setTimeout(r, 2));
   expect(send).toHaveBeenCalledTimes(0);
   const observable = client.call('test', {foo: 'bar'});
+  await new Promise((r) => setTimeout(r, 2));
   expect(send).toHaveBeenCalledTimes(1);
   const promise = observable.pipe(take(1)).toPromise();
+  await new Promise((r) => setTimeout(r, 2));
   expect(send).toHaveBeenCalledTimes(1);
   client.onMessage([0, send.mock.calls[0][0][0], 25]);
+  await new Promise((r) => setTimeout(r, 2));
   expect(send).toHaveBeenCalledTimes(1);
   const result = await promise;
+  await new Promise((r) => setTimeout(r, 2));
   expect(send).toHaveBeenCalledTimes(1);
   expect(result).toBe(25);
 });
 
 test('does not send unsubscribe when error has been received', async () => {
   const send = jest.fn();
-  const client = new JsonRxClient({send});
+  const client = new JsonRxClient({send, bufferTime: 1});
+  await new Promise((r) => setTimeout(r, 2));
   expect(send).toHaveBeenCalledTimes(0);
   const observable = client.call('test', {foo: 'bar'});
+  await new Promise((r) => setTimeout(r, 2));
   expect(send).toHaveBeenCalledTimes(1);
   const promise = observable.pipe(take(1)).toPromise();
+  await new Promise((r) => setTimeout(r, 2));
   expect(send).toHaveBeenCalledTimes(1);
   client.onMessage([-1, send.mock.calls[0][0][0], 25]);
   expect(send).toHaveBeenCalledTimes(1);
@@ -257,17 +273,20 @@ test('does not send unsubscribe when error has been received', async () => {
   } catch (err) {
     error = err;
   }
+  await new Promise((r) => setTimeout(r, 2));
   expect(send).toHaveBeenCalledTimes(1);
   expect(error).toBe(25);
 });
 
 test('after .stop() completes subscriptions', async () => {
   const send = jest.fn();
-  const client = new JsonRxClient({send});
+  const client = new JsonRxClient({send, bufferTime: 1});
+  await new Promise((r) => setTimeout(r, 2));
   expect(send).toHaveBeenCalledTimes(0);
   const observable = client.call('test', {});
   const data = jest.fn();
   observable.subscribe(data);
+  await new Promise((r) => setTimeout(r, 2));
   expect(send).toHaveBeenCalledTimes(1);
   expect(data).toHaveBeenCalledTimes(0);
   client.onMessage([-2, 1, 1]);
@@ -277,4 +296,21 @@ test('after .stop() completes subscriptions', async () => {
   client.stop();
   client.onMessage([-2, 1, 3]);
   expect(data).toHaveBeenCalledTimes(2);
+});
+
+test('combines multiple messages in a batch', async () => {
+  const send = jest.fn();
+  const client = new JsonRxClient({send, bufferTime: 1});
+  await new Promise((r) => setTimeout(r, 2));
+  expect(send).toHaveBeenCalledTimes(0);
+  const observable1 = client.call('test', {});
+  const observable2 = client.call('test2', {foo: 'bar'});
+  client.notify('test3', {gg: 'bet'});
+  await new Promise((r) => setTimeout(r, 2));
+  expect(send).toHaveBeenCalledTimes(1);
+  expect(send.mock.calls[0][0]).toEqual([
+    [1, 'test', {}],
+    [2, 'test2', {foo: 'bar'}],
+    ['test3', {gg: 'bet'}],
+  ]);
 });
