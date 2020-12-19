0c7591c339102148f0aa2a56b0b91be7b81916d4
streamich <streamich@gmail.com>
2020-12-19 21:55:19 +0100

    fix: 🐛 fix json-patch-tests tests

--- src/json-cli/json-patch.ts
+++ src/json-cli/json-patch.ts
@@ -1,10 +1,16 @@
 import {readFileSync} from 'fs';
+import {validateOperation} from '../json-patch';
 import {applyPatch} from '../json-patch/patch';
 
-
-const buf = readFileSync(0);
-const doc = JSON.parse(buf.toString())
-const patch = JSON.parse(process.argv[2]);
-const res = applyPatch(doc, patch, true);
-
-process.stdout.write(JSON.stringify(res.doc, null, 4) + '\n');
+try {
+  const buf = readFileSync(0);
+  const doc = JSON.parse(buf.toString())
+  const patch = JSON.parse(process.argv[2]);
+  patch.forEach(validateOperation);
+  const res = applyPatch(doc, patch, true);
+  process.stdout.write(JSON.stringify(res.doc, null, 4) + '\n');
+} catch (error) {
+  const output = error instanceof Error ? error.message : String(error);
+  process.stderr.write(output + '\n');
+  process.exit(1);
+}
