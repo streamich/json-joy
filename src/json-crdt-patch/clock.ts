2f76ad98f67b8df48a3137ed9b59d3e1b2e92594
streamich <streamich@gmail.com>
2021-03-24 10:02:56 +0100

    test: 💍 add text insertion scenario tests

--- src/json-crdt-patch/clock.ts
+++ src/json-crdt-patch/clock.ts
@@ -175,4 +175,10 @@ export class VectorClock extends LogicalClock {
     }
     return clock;
   }
+
+  public fork(sessionId: number): VectorClock {
+    const clock = new VectorClock(sessionId, this.time);
+    for (const ts of this.clocks.values()) clock.observe(ts.tick(0));
+    return clock;
+  }
 }
