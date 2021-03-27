1d69a578d1e5cd5238fa644a4b7c3528102b52d3
streamich <streamich@gmail.com>
2021-03-27 17:48:57 +0100

    test: 💍 add another change buffer test

--- src/json-crdt/__tests__/ChangeBuffer.spec.ts
+++ src/json-crdt/__tests__/ChangeBuffer.spec.ts
@@ -11,3 +11,17 @@ test('can edit strings', () => {
   changes.strIns([], 0, 'const');
   expect(doc.toJson()).toBe('const foo = "bar";');
 });
+
+test('can edit strings - 2', () => {
+  const doc = new Document();
+  const changes = doc.changes;
+  changes.root({foo: [123, '', 5]});
+  changes.strIns(['foo', 1], 0, 'var foo = bar');
+  changes.strIns(['foo', 1], 9, '"');
+  changes.strIns(['foo', 1], 13, '";');
+  changes.strDel(['foo', 1], 0, 3);
+  changes.strIns(['foo', 1], 0, 'const');
+  expect(doc.toJson()).toEqual({
+    foo: [123, 'const foo = "bar";', 5],
+  });
+});
