83a5aad5e51860b70e629a135001bdc96fdd666b
streamich <streamich@gmail.com>
2021-03-27 20:48:00 +0100

    feat: 🎸 implement commit and flush for document api

--- src/json-crdt/api/DocumentApi.ts
+++ src/json-crdt/api/DocumentApi.ts
@@ -2,9 +2,13 @@ import type {Document} from "../document";
 import type {Path} from "../../json-pointer";
 import {StringType} from "../types/rga-string/StringType";
 import {PatchBuilder} from "../../json-crdt-patch/PatchBuilder";
+import {Patch} from "../../json-crdt-patch/Patch";
+import {NoopOperation} from "../../json-crdt-patch/operations/NoopOperation";
+import {LogicalTimestamp} from "../../json-crdt-patch/clock";
 
 export class DocumentApi {
-  private readonly builder: PatchBuilder;
+  public patches: Patch[] = [];
+  private builder: PatchBuilder;
 
   constructor(private readonly doc: Document) {
     this.builder = new PatchBuilder(doc.clock);
@@ -16,29 +20,66 @@ export class DocumentApi {
     throw new Error('NOT_STRING');
   }
 
-  public root(json: unknown) {
-    let i = this.builder.patch.ops.length;
+  public commit(): this {
+    const patch = this.builder.patch;
+    this.builder = new PatchBuilder(this.doc.clock);
+    if (patch.ops.length) {
+      this.patches.push(patch);
+      this.doc.applyPatch(patch);
+    }
+    return this;
+  }
+
+  public flush(): Patch[] {
+    const patches = this.patches;
+    this.patches = [];
+    return patches;
+  }
+
+  public flushPatch(): Patch {
+    const patches = this.flush()
+    const length = patches.length;
+    const result = new Patch();
+    let prev: null | Patch = null;
+    for (let i = 0; i < length; i++) {
+      const patch = patches[i];
+      if (!patch.ops) continue;
+      if (!prev) result.ops.push(...patch.ops);
+      else {
+        const id = patch.getId()!;
+        const nextTime = prev.nextTime();
+        const gap = id.time - nextTime;
+        if (gap > 0) result.ops.push(new NoopOperation(new LogicalTimestamp(id.sessionId, nextTime), gap));
+        result.ops.push(...patch.ops);
+      }
+      prev = patch;
+    }
+    return result;
+  }
+
+  public patch(cb: (api: this) => void) {
+    this.commit();
+    cb(this);
+    this.commit();
+  }
+
+  public root(json: unknown): this {
     const value = this.builder.json(json);
     this.builder.root(value);
-    for (; i < this.builder.patch.ops.length; i++)
-      this.doc.applyOperation(this.builder.patch.ops[i]);
+    return this;
   }
 
-  public strIns(path: Path, index: number, substr: string) {
-    let i = this.builder.patch.ops.length;
+  public strIns(path: Path, index: number, substr: string): this {
     const obj = this.asString(path);
-    const after = !index ? obj.id : obj.findId(index);
+    const after = !index ? obj.id : obj.findId(index - 1);
     this.builder.insStr(obj.id, after, substr);
-    for (; i < this.builder.patch.ops.length; i++)
-      this.doc.applyOperation(this.builder.patch.ops[i]);
+    return this;
   }
 
-  public strDel(path: Path, index: number, length: number) {
-    let i = this.builder.patch.ops.length;
+  public strDel(path: Path, index: number, length: number): this {
     const obj = this.asString(path);
     const after = obj.findId(index);
     this.builder.del(obj.id, after, length);
-    for (; i < this.builder.patch.ops.length; i++)
-      this.doc.applyOperation(this.builder.patch.ops[i]);
+    return this;
   }
 }
--- src/json-crdt/api/__tests__/DocumentApi.spec.ts
+++ src/json-crdt/api/__tests__/DocumentApi.spec.ts
@@ -1,27 +1,58 @@
 import {Document} from '../../document';
 
-test('can edit strings', () => {
-  const doc = new Document();
-  const api = doc.api;
-  api.root('');
-  api.strIns([], 0, 'var foo = bar');
-  api.strIns([], 9, '"');
-  api.strIns([], 13, '";');
-  api.strDel([], 0, 3);
-  api.strIns([], 0, 'const');
-  expect(doc.toJson()).toBe('const foo = "bar";');
+
+describe('string manipulation', () => {
+  test('can edit strings', () => {
+    const doc = new Document();
+    const api = doc.api;
+    api.root('')
+      .commit()
+      .strIns([], 0, 'var foo = bar')
+      .commit()
+      .strIns([], 10, '"')
+      .commit()
+      .strIns([], 14, '";')
+      .commit()
+      .strDel([], 0, 3)
+      .commit()
+      .strIns([], 0, 'const')
+      .commit();
+    expect(doc.toJson()).toBe('const foo = "bar";');
+  });
+  
+  test('can edit strings - 2', () => {
+    const doc = new Document();
+    const api = doc.api;
+    api.root({foo: [123, '', 5]})
+      .commit()
+      .strIns(['foo', 1], 0, 'var foo = bar')
+      .commit()
+      .strIns(['foo', 1], 10, '"')
+      .commit()
+      .strIns(['foo', 1], 14, '";')
+      .commit()
+      .strDel(['foo', 1], 0, 3)
+      .commit()
+      .strIns(['foo', 1], 0, 'const')
+      .commit();
+    expect(doc.toJson()).toEqual({
+      foo: [123, 'const foo = "bar";', 5],
+    });
+  });
 });
 
-test('can edit strings - 2', () => {
-  const doc = new Document();
-  const changes = doc.api;
-  changes.root({foo: [123, '', 5]});
-  changes.strIns(['foo', 1], 0, 'var foo = bar');
-  changes.strIns(['foo', 1], 9, '"');
-  changes.strIns(['foo', 1], 13, '";');
-  changes.strDel(['foo', 1], 0, 3);
-  changes.strIns(['foo', 1], 0, 'const');
-  expect(doc.toJson()).toEqual({
-    foo: [123, 'const foo = "bar";', 5],
+describe('patch()', () => {
+  test('can patch multiple operations into a single patch', () => {
+    const doc = new Document();
+    const api = doc.api;
+    api.root({foo: 'abc'}).commit();
+    expect(api.patches.length).toBe(1);
+    expect(doc.toJson()).toEqual({foo: 'abc'});
+    api.patch(() => {
+      api.strIns(['foo'], 1, '1');
+      api.strIns(['foo'], 3, '2');
+    });
+    expect(api.patches.length).toBe(2);
+    expect(doc.toJson()).toEqual({foo: 'a1bc2'});
   });
 });
