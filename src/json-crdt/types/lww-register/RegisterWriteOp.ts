36ee53cb8f7aa13373ec5d08721d4e3ebf938571
streamich <streamich@gmail.com>
2021-03-28 18:30:33 +0200

    refactor: 💡 rename lww-register to RegisterType

--- src/json-crdt/types/lww-register/RegisterWriteOp.ts
+++ src/json-crdt/types/lww-register/RegisterWriteOp.ts
@@ -3,6 +3,6 @@ import {LogicalTimestamp} from "../../../json-crdt-patch/clock";
 /**
  * Writes a value to LWW Register.
  */
-export class LWWRegisterWriteOp {
+export class RegisterWriteOp {
   constructor(public readonly id: LogicalTimestamp, public readonly value: LogicalTimestamp) {}
 }
