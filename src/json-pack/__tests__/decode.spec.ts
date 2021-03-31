6c723117a98bef27e76e76a2ecb8d85ffc4636cf
streamich <streamich@gmail.com>
2021-03-31 12:08:11 +0200

    refactor: 💡 return offste when decoding

--- src/json-pack/__tests__/decode.spec.ts
+++ src/json-pack/__tests__/decode.spec.ts
@@ -5,7 +5,7 @@ describe('null', () => {
   test('can decode null', () => {
     const buf = encode(null);
     const res = decode(buf, 0);
-    expect(res).toBe(null);
+    expect(res[0]).toBe(null);
   });
 });
 
@@ -13,13 +13,13 @@ describe('boolean', () => {
   test('can decode false', () => {
     const buf = encode(false);
     const res = decode(buf, 0);
-    expect(res).toBe(false);
+    expect(res[0]).toBe(false);
   });
 
   test('can decode true', () => {
     const buf = encode(true);
     const res = decode(buf, 0);
-    expect(res).toBe(true);
+    expect(res[0]).toBe(true);
   });
 });
 
@@ -27,31 +27,31 @@ describe('number', () => {
   test('can decode positive fixint', () => {
     const buf = encode(123);
     const res = decode(buf, 0);
-    expect(res).toBe(123);
+    expect(res[0]).toBe(123);
   });
 
   test('can decode 0', () => {
     const buf = encode(0);
     const res = decode(buf, 0);
-    expect(res).toBe(0);
+    expect(res[0]).toBe(0);
   });
 
   test('can decode negative fixint', () => {
     const buf = encode(-1);
     const res = decode(buf, 0);
-    expect(res).toBe(-1);
+    expect(res[0]).toBe(-1);
   });
 
   test('can decode negative fixint - 2', () => {
     const buf = encode(-32);
     const res = decode(buf, 0);
-    expect(res).toBe(-32);
+    expect(res[0]).toBe(-32);
   });
 
   test('can decode double', () => {
     const buf = encode(3456.12345678902234);
     const res = decode(buf, 0);
-    expect(res).toBe(3456.12345678902234);
+    expect(res[0]).toBe(3456.12345678902234);
   });
 });
 
@@ -59,52 +59,52 @@ describe('string', () => {
   test('can decode empty string', () => {
     const buf = encode('');
     const res = decode(buf, 0);
-    expect(res).toBe('');
+    expect(res[0]).toBe('');
   });
 
   test('can decode short string', () => {
     const buf = encode('abc');
     const res = decode(buf, 0);
-    expect(res).toBe('abc');
+    expect(res[0]).toBe('abc');
   });
 
   test('can decode 31 char string', () => {
     const buf = encode('1234567890123456789012345678901');
     const res = decode(buf, 0);
-    expect(res).toBe('1234567890123456789012345678901');
+    expect(res[0]).toBe('1234567890123456789012345678901');
   });
 
   test('can decode 32 char string', () => {
     const buf = encode('12345678901234567890123456789012');
     const res = decode(buf, 0);
-    expect(res).toBe('12345678901234567890123456789012');
+    expect(res[0]).toBe('12345678901234567890123456789012');
   });
 
   test('can decode 255 char string', () => {
     const str = 'a'.repeat(255);
     const buf = encode(str);
     const res = decode(buf, 0);
-    expect(res).toBe(str);
+    expect(res[0]).toBe(str);
   });
 
   test('can decode 256 char string', () => {
     const str = 'a'.repeat(256);
     const buf = encode(str);
     const res = decode(buf, 0);
-    expect(res).toBe(str);
+    expect(res[0]).toBe(str);
   });
 
   test('can decode 0xFFFF char string', () => {
     const str = 'a'.repeat(0xFFFF);
     const buf = encode(str);
     const res = decode(buf, 0);
-    expect(res).toBe(str);
+    expect(res[0]).toBe(str);
   });
 
   test('can decode 0xFFFF + 1 char string', () => {
     const str = 'a'.repeat(0xFFFF + 1);
     const buf = encode(str);
     const res = decode(buf, 0);
-    expect(res).toBe(str);
+    expect(res[0]).toBe(str);
   });
 });
--- src/json-pack/decode.ts
+++ src/json-pack/decode.ts
@@ -1,23 +1,47 @@
 import {decodeString} from "../util/decodeString";
 
-export const decode = (buf: ArrayBuffer, offset: number): unknown => {
-  const view = new DataView(buf);
+export const decodeView = (view: DataView, offset: number): [json: unknown, offset: number] => {
   const byte = view.getUint8(offset++);
   switch (byte) {
-    case 0xc0: return null;
-    case 0xc2: return false;
-    case 0xc3: return true;
-    case 0xcb: return view.getFloat64(offset);
-    case 0xd9: return decodeString(buf, offset + 1, view.getUint8(offset));
-    case 0xda: return decodeString(buf, offset + 2, view.getUint16(offset));
-    case 0xdb: return decodeString(buf, offset + 4, view.getUint32(offset));
+    case 0xc0: return [null, offset];
+    case 0xc2: return [false, offset];
+    case 0xc3: return [true, offset];
+    case 0xcb: return [view.getFloat64(offset), offset + 4];
+    case 0xd9: {
+      const size = view.getUint8(offset++);
+      return [decodeString(view.buffer, offset, size), offset + size];
+    }
+    case 0xda: {
+      const size = view.getUint16(offset);
+      offset += 2;
+      return [decodeString(view.buffer, offset, size), offset + size];
+    }
+    case 0xdb: {
+      const size = view.getUint32(offset);
+      offset += 4;
+      return [decodeString(view.buffer, offset, size), offset + size];
+    }
   }
-  if (byte <= 0b1111111) return byte;
-  const top4 = byte >>> 4;
+  if (byte <= 0b1111111) return [byte, offset];
   const top3 = byte >>> 5;
   switch (top3) {
-    case 0b111: return -(byte & 0b11111) - 1;
-    case 0b101: return decodeString(buf, offset, byte & 0b11111);
+    case 0b111: return [-(byte & 0b11111) - 1, offset];
+    case 0b101: {
+      const size = byte & 0b11111;
+      return [decodeString(view.buffer, offset, size), offset + size];
+    }
   }
-  return undefined;
+  const top4 = byte >>> 4;
+  switch(top4) {
+    case 0b1001: {
+      const size = byte & 0b1111;
+      const arr = [];
+      for (let i = 0; i < size; i++) arr.push(decodeView(view, offset))
+    }
+  }
+  return [undefined, offset];
+};
+
+export const decode = (buf: ArrayBuffer, offset: number): [json: unknown, offset: number] => {
+  return decodeView(new DataView(buf), offset);
 };
