a21e35e7c8f158643829477f68d38592077771c4
streamich <streamich@gmail.com>
2021-04-01 02:05:41 +0200

    test: 💍 add JSON example document tests

--- src/json-pack/__tests__/codec.spec.ts
+++ src/json-pack/__tests__/codec.spec.ts
@@ -147,6 +147,136 @@ const tests: Array<{name: string, json: unknown}> = [
       likes: 33,
     },
   },
+  {
+    name: 'user object',
+    json: {
+      "title": "Person",
+      "type": "object",
+      "properties": {
+        "firstName": {
+          "type": "string"
+        },
+        "lastName": {
+          "type": "string"
+        },
+        "age": {
+          "description": "Age in years",
+          "type": "integer",
+          "minimum": 0
+        }
+      },
+      "required": ["firstName", "lastName"]
+    },
+  },
+  {
+    name: 'completion response',
+    json: {
+      "done": false,
+      "text": "do something"
+    },
+  },
+  {
+    name: 'cooking receipt',
+    json: {
+      "id": "0001",
+      "type": "donut",
+      "name": "Cake",
+      "ppu": 0.55,
+      "batters":
+        {
+          "batter":
+            [
+              { "id": "1001", "type": "Regular" },
+              { "id": "1002", "type": "Chocolate" },
+              { "id": "1003", "type": "Blueberry" },
+              { "id": "1004", "type": "Devil's Food" }
+            ]
+        },
+      "topping":
+        [
+          { "id": "5001", "type": "None" },
+          { "id": "5002", "type": "Glazed" },
+          { "id": "5005", "type": "Sugar" },
+          { "id": "5007", "type": "Powdered Sugar" },
+          { "id": "5006", "type": "Chocolate with Sprinkles" },
+          { "id": "5003", "type": "Chocolate" },
+          { "id": "5004", "type": "Maple" }
+        ]
+    },
+  },
+  {
+    name: 'JSON-LD object',
+    json: {
+      "@context": {
+        "@version": 1.1,
+        "schema": "http://schema.org/",
+        "name": "schema:name",
+        "body": "schema:articleBody",
+        "words": "schema:wordCount",
+        "post": {
+          "@id": "schema:blogPost",
+          "@container": "@id"
+        },
+        "none": "@none"
+      },
+      "@id": "http://example.com/",
+      "@type": "schema:Blog",
+      "name": "World Financial News",
+      "post": {
+        "http://example.com/posts/1/en": {
+          "body": "World commodities were up today with heavy trading of crude oil...",
+          "words": 1539
+        },
+        "http://example.com/posts/1/de": {
+          "body": "Die Werte an Warenbörsen stiegen im Sog eines starken Handels von Rohöl...",
+          "words": 1204
+        },
+        "none": {
+          "body": "Description for object within an @id",
+          "words": 20
+        }
+      }
+    },
+  },
+  {
+    name: 'JSON-LD object - 2',
+    json: {
+      "@context": {
+        "@version": 1.1,
+        "generatedAt": {
+          "@id": "http://www.w3.org/ns/prov#generatedAtTime",
+          "@type": "http://www.w3.org/2001/XMLSchema#date"
+        },
+        "Person": "http://xmlns.com/foaf/0.1/Person",
+        "name": "http://xmlns.com/foaf/0.1/name",
+        "knows": "http://xmlns.com/foaf/0.1/knows",
+        "graphMap": {
+          "@id": "http://example.org/graphMap",
+          "@container": ["@graph", "@id"]
+        }
+      },
+      "@id": "_:graph",
+      "generatedAt": "2012-04-09",
+      "graphMap": {
+        "_:manu": {
+          "@id": "http://manu.sporny.org/about#manu",
+          "@type": "Person",
+          "name": "Manu Sporny",
+          "knows": "http://greggkellogg.net/foaf#me"
+        },
+        "_:gregg": {
+          "@id": "http://greggkellogg.net/foaf#me",
+          "@type": "Person",
+          "name": "Gregg Kellogg",
+          "knows": "http://manu.sporny.org/about#manu"
+        }
+      }
+    },
+  },
+  {
+    name: 'JSON-RPC request',
+    json: {"version": "1.1", "method": "confirmFruitPurchase", "params": [["apple", "orange", "mangoes"], 1.123], "id": "194521489"},
+  },
   /*
   {
     name: 'simple ArrayBuffer',
