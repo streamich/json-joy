799b39636899c18d4f8cf2eb932da0885d9f6001
streamich <streamich@gmail.com>
2021-04-12 11:53:36 +0200

    test: 💍 add automated document tests to codecs

---
+++ src/json-crdt/codec/compact-binary/__tests__/automated.spec.ts
@@ -0,0 +1,18 @@
+import {VectorClock} from '../../../../json-crdt-patch/clock';
+import {Document} from '../../../document';
+import {Encoder} from '../Encoder';
+import {Decoder} from '../Decoder';
+import {documents} from '../../../../util/__tests__/json-documents'
+
+for (const {name, json} of documents) {
+  test(name, () => {
+    const doc1 = new Document(new VectorClock(222, 0));
+    doc1.api.root(json).commit();
+    const encoder = new Encoder();
+    const decoder = new Decoder();
+    const encoded = encoder.encode(doc1);
+    const doc2 = decoder.decode(encoded);
+    expect(doc1.toJson()).toEqual(json);
+    expect(doc2.toJson()).toEqual(json);
+  });
+}
