349baac98afa0fb3fcf5e8b298558782164f3e95
streamich <streamich@gmail.com>
2021-04-15 01:51:09 +0200

    test: 💍 improve string fuzz tester

--- src/json-crdt/model/__tests__/fuzzer/string.spec.ts
+++ src/json-crdt/model/__tests__/fuzzer/string.spec.ts
@@ -1,11 +1,18 @@
 import {StringFuzzer} from './StringFuzzer';
 
-test('...', () => {
-  const fuzzer = new StringFuzzer();
-  fuzzer.setupModel();
-  const session = fuzzer.executeConcurrentSession();
-  const json = session.models[0].toJson();
-  for (let i = 1; i < session.models.length; i++) {
-    expect(json).toEqual(session.models[i].toJson());
+const runs = 1000;
+const sessionNum = 10;
+
+test('string fuzz testing', () => {
+  for (let r = 0; r < runs; r++) {
+    const fuzzer = new StringFuzzer();
+    fuzzer.setupModel();
+    for (let ses = 0; ses < sessionNum; ses++) {
+      const session = fuzzer.executeConcurrentSession();
+      const json = session.models[0].toJson();
+      for (let i = 1; i < session.models.length; i++) {
+        expect(json).toEqual(session.models[i].toJson());
+      }
+    }
   }
 });
