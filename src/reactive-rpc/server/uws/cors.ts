f7f3e7af59dec521530d3bf7c0c0e11005fb6223
streamich <streamich@gmail.com>
2021-05-17 20:40:46 +0200

    feat: 🎸 add CORS helper for uws

---
+++ src/reactive-rpc/server/uws/cors.ts
@@ -0,0 +1,16 @@
+import {TemplatedApp} from 'uWebSockets.js';
+
+export const enableCors = (uws: TemplatedApp) => {
+  const AccessControlAllowOrigin = Buffer.from('Access-Control-Allow-Origin');
+  const AccessControlAllowOriginAllowAll = Buffer.from('*');
+  const AccessControlAllowCredentials = Buffer.from('Access-Control-Allow-Credentials');
+  const AccessControlAllowCredentialsTrue = Buffer.from('true');
+
+  uws.options('/*', (res, req) => {
+    res.cork(() => {
+      res.writeHeader(AccessControlAllowOrigin, AccessControlAllowOriginAllowAll);
+      res.writeHeader(AccessControlAllowCredentials, AccessControlAllowCredentialsTrue);
+      res.end();
+    });
+  });
+};
