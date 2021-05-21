447939b9758c9c5890b1cbcbc6062599dea242f8
streamich <streamich@gmail.com>
2021-05-21 17:49:27 +0200

    perf: ⚡️ encode a single message if there is only one in a batch

--- src/reactive-rpc/common/codec/compact-json/Encoder.ts
+++ src/reactive-rpc/common/codec/compact-json/Encoder.ts
@@ -4,7 +4,7 @@ import {encode} from "../compact/encode";
 import {CompactMessage} from "../compact/types";
 
 export class Encoder {
-  public encode(messages: ReactiveRpcMessage[]): json_string<CompactMessage[]> {
+  public encode(messages: ReactiveRpcMessage[]): json_string<CompactMessage | CompactMessage[]> {
     const compact = encode(messages);
     return JSON.stringify(compact);
   }
