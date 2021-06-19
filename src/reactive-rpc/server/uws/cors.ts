8170d77dbb260e34f858da0e8c61cf5b14d76d12
streamich <streamich@gmail.com>
2021-06-19 18:08:53 +0200

    feat: 🎸 improve enableCors() method

--- src/reactive-rpc/server/uws/cors.ts
+++ src/reactive-rpc/server/uws/cors.ts
@@ -1,6 +1,15 @@
-import type {TemplatedApp} from 'uWebSockets.js';
+export interface UwsApp {
+  options(route: string, handler: (
+    res: {
+      writeHeader(key: string | Buffer, value: string | Buffer): void;
+      cork(cb: () => void): void;
+      end(): void;
+    },
+    req: {},
+  ) => void): void;
+}
 
-export const enableCors = (uws: TemplatedApp) => {
+export const enableCors = (uws: UwsApp) => {
   const AccessControlAllowOrigin = Buffer.from('Access-Control-Allow-Origin');
   const AccessControlAllowOriginAllowAll = Buffer.from('*');
   const AccessControlAllowCredentials = Buffer.from('Access-Control-Allow-Credentials');
