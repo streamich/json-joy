71a1d567d0ce2a7d5e1bdf0234a9c590c4f37169
streamich <streamich@gmail.com>
2021-11-16 12:35:07 +0100

    feat: 🎸 rewrite $apply as codege $apply function

--- src/json-patch/codegen/apply.ts
+++ src/json-patch/codegen/apply.ts
@@ -6,7 +6,7 @@ import {ApplyPatchOptions} from '../applyPatch/types';
 import type {JsonPatchOptions} from '..';
 import {$test} from './ops/test';
 import type {ApplyFn} from './types';
-import {CompiledFunction} from '../../util/codegen';
+import {compile, CompiledFunction, JavaScript} from '../../util/codegen';
 
 export const apply = (patch: readonly Operation[], applyOptions: ApplyPatchOptions, doc: unknown): unknown => {
   const {mutate, createMatcher} = applyOptions;
@@ -41,43 +41,24 @@ export const $$apply = (operations: readonly Operation[], applyOptions: ApplyPat
   }
 
   const js = /* js */ `
-(function(deepClone) {
+(function(deepClone, fns) {
   return function(doc){
     ${!mutate && hasNonPredicateOperations ? /* js */ `doc = deepClone(doc);` : ''};
-    for (let i = 0; i < length; i++) doc = ops[i](doc);
+    for (var i = 0; i < ${length}; i++) {
+      doc = fns[i](doc);
+    }
     return doc;
   };
 })`;
 
-  return (doc: unknown): unknown => {
-    if (!mutate && hasNonPredicateOperations) doc = deepClone(doc);
-    for (let i = 0; i < length; i++) doc = fns[i](doc);
-    return doc;
+  return {
+    deps: [deepClone, fns],
+    js: js as JavaScript<(...deps: unknown[]) => ApplyFn>,
   };
 };
 
 export const $apply = (operations: readonly Operation[], applyOptions: ApplyPatchOptions): ApplyFn => {
-  const {mutate, createMatcher} = applyOptions;
-  const operationOptions: JsonPatchOptions = {createMatcher};
-  const ops: ApplyFn[] = [];
-  const length = operations.length;
-
-  let hasNonPredicateOperations = false;
-
-  for (let i = 0; i < length; i++) {
-    const op = operationToOp(operations[i], operationOptions);
-    const isPredicateOp = op instanceof AbstractPredicateOp;
-    if (!isPredicateOp) hasNonPredicateOperations = true;
-    if (op.op() === 'test') {
-      ops.push($test(op as OpTest));
-    } else {
-      ops.push(doc => op.apply(doc).doc);
-    }
-  }
-
-  return (doc: unknown): unknown => {
-    if (!mutate && hasNonPredicateOperations) doc = deepClone(doc);
-    for (let i = 0; i < length; i++) doc = ops[i](doc);
-    return doc;
-  };
+  const fn = $$apply(operations, applyOptions);
+  const compiled = compile(fn.js)(...fn.deps);
+  return compiled;
 };
