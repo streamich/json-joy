76f7ef05ee6b684a4701003fca6cf0c5ad96d26a
streamich <streamich@gmail.com>
2021-11-18 23:37:17 +0100

    test: 💍 improve json-type MesagePack codegen tests

--- src/json-type-codegen/__tests__/msgpack.spec.ts
+++ src/json-type-codegen/__tests__/msgpack.spec.ts
@@ -2,11 +2,22 @@ import {t} from '../../json-type/type';
 import {EncoderFull} from '../../json-pack/EncoderFull';
 import {Decoder} from '../../json-pack/Decoder';
 import {EncodingPlan} from '../msgpack';
+import {TType} from '../../json-type/types/json';
 
 const encoder = new EncoderFull();
 const decoder = new Decoder();
 
-test('serializes according to schema', () => {
+const exec = (type: TType, json: unknown) => {
+  const plan = new EncodingPlan();
+  const js = plan.createPlan(type);
+  const fn = eval(js)(encoder);
+  const blob = fn(json);
+  const decoded = decoder.decode(blob);
+
+  expect(decoded).toStrictEqual(json);
+};
+
+test('serializes according to schema a POJO object', () => {
   const type = t.Object({
     fields: [
       t.Field('a', t.num),
@@ -21,10 +32,6 @@ test('serializes according to schema', () => {
       t.Field('bin', t.bin),
     ],
   });
-
-  const plan = new EncodingPlan();
-  const js = plan.createPlan(type);
-  const fn = eval(js)(encoder);
   const json = {
     a: 1.1,
     b: 'sdf',
@@ -33,8 +40,54 @@ test('serializes according to schema', () => {
     arr: [{foo: [1]}, {foo: [4, 4, 4.4]}],
     bin: new Uint8Array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]),
   };
-  const blob = fn(json);
-  const decoded = decoder.decode(blob);
 
-  expect(decoded).toStrictEqual(json);
+  exec(type, json);
+});
+
+describe('single value', () => {
+  test('null', () => {
+    const type = t.nil;
+    const json = null;
+    exec(type, json);
+  });
+
+  test('true and false', () => {
+    exec(t.bool, true);
+    exec(t.bool, false);
+    exec(t.Boolean({const: false}), false);
+    exec(t.Boolean({const: true}), true);
+  });
+
+  test('number', () => {
+    exec(t.num, 1);
+    exec(t.num, 0);;
+    exec(t.num, 1.2);;
+    exec(t.num, -3);;
+    exec(t.num, -232123);
+    exec(t.num, -232123.9);
+    exec(t.Number({const: 3}), 3);
+  });
+
+  test('string', () => {
+    exec(t.str, '');
+    exec(t.str, 'asdf');
+    exec(t.String({const: ''}), '');
+    exec(t.String({const: 'asdf'}), 'asdf');
+  });
+
+  test('array', () => {
+    exec(t.arr, []);
+    exec(t.arr, [1, 2, 3]);
+    exec(t.Array(t.num), [1, 2, 3]);
+    exec(t.Array(t.str), ['1', '2', '3']);
+  });
+
+  test('object', () => {
+    exec(t.Object({ fields: [] }), {});
+    exec(t.Object({
+      fields: [
+        t.Field('a', t.str),
+      ]
+    }), {a: 'b'});
+  });
 });
