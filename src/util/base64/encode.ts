5f685c69182bd93d2eb84a61e3630409fbc1e5ea
streamich <streamich@gmail.com>
2021-11-24 18:52:28 +0100

    perf(util): ⚡️ optimize base64 encoder

--- src/util/base64/encode.ts
+++ src/util/base64/encode.ts
@@ -7,40 +7,42 @@ export const createEncode = (chars: string = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefgh
 
   const table = chars.split('');
 
-  return (source: Uint8Array): string => {
+  return (uint8: Uint8Array, length: number): string => {
     let out = ''
-
-    const length = source.byteLength;
     const extraLength = length % 3;
     const baseLength = length - extraLength;
-
     for (let i = 0; i < baseLength; i += 3) {
-      const o1 = source[i];
-      const o2 = source[i + 1];
-      const o3 = source[i + 2];
+      const o1 = uint8[i];
+      const o2 = uint8[i + 1];
+      const o3 = uint8[i + 2];
       const v1 = o1 >> 2;
       const v2 = ((o1 & 0b11) << 4) | (o2 >> 4);
       const v3 = ((o2 & 0b1111) << 2) | (o3 >> 6);
       const v4 = o3 & 0b111111;
       out += table[v1] + table[v2] + table[v3] + table[v4];
     }
-
     if (extraLength) {
       if (extraLength === 1) {
-        const o1 = source[baseLength] & 0xFF;
+        const o1 = uint8[baseLength] & 0xFF;
         out += table[o1 >> 2] + table[(o1 & 0b11) << 4] + EE;
       } else {
-        const o1 = source[baseLength];
-        const o2 = source[baseLength + 1];
+        const o1 = uint8[baseLength];
+        const o2 = uint8[baseLength + 1];
         const v1 = o1 >> 2;
         const v2 = ((o1 & 0b11) << 4) | (o2 >> 4);
         const v3 = ((o2 & 0b1111) << 2);
         out += table[v1] + table[v2] + table[v3] + E;
       }
     }
-
     return out;
   };
 };
 
-export const encode = createEncode();
+const encodeSmall = createEncode();
+const hasBuffer = typeof Buffer === 'function' && (typeof Buffer.from === 'function');
+
+export const encode = !hasBuffer ? encodeSmall : (uint8: Uint8Array): string => {
+  const length = uint8.byteLength;
+  if (length <= 25) return encodeSmall(uint8, length);
+  return Buffer.from(uint8).toString('base64');
+};
