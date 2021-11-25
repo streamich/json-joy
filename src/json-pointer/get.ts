81691dec94b7d74bb6a063082ac8b8215867badc
streamich <streamich@gmail.com>
2021-11-25 20:05:31 +0100

    feat(json-pointer): 🎸 implement get() method

---
+++ src/json-pointer/get.ts
@@ -0,0 +1,24 @@
+import type {Path} from './types';
+import {hasOwnProperty} from './util';
+
+export const get = (val: unknown, path: Path): unknown | undefined => {
+  const pathLength = path.length;
+  let key: string | number;
+  if (!pathLength) return val;
+  for (let i = 0; i < pathLength; i++) {
+    key = path[i];
+    if (val instanceof Array) {
+      const length = val.length;
+      if (key === '-') return undefined;
+      const key2 = ~~key;
+      if ('' + key2 !== key) return undefined;
+      key = key2;
+      if (key < 0) return undefined;
+      val = val[key];
+    } else if (typeof val === 'object') {
+      if (!val || !hasOwnProperty(val as object, key as string)) return undefined;
+      val = (val as any)[key];
+    } else return undefined;
+  }
+  return val;
+};
--- src/json-pointer/index.ts
+++ src/json-pointer/index.ts
@@ -9,5 +9,6 @@
 export * from './types';
 export * from './util';
 export * from './validate';
+export * from './get';
 export * from './find';
 export * from './findByPointer';
