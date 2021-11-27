fd59781c9ad8a2451bb10d63cc445c8bb73962cf
streamich <streamich@gmail.com>
2021-11-27 12:24:45 +0100

    feat(json-pointer): 🎸 use .hasOwnProperty() from prototype

--- src/json-pointer/codegen/find.ts
+++ src/json-pointer/codegen/find.ts
@@ -2,10 +2,10 @@ import {JavaScript} from '../../util/codegen';
 import {Path} from '../types';
 
 export const $$find = (path: Path): JavaScript<(doc: unknown) => unknown> => {
-  let fn = '(function(o){var k,u=undefined;try{';
+  let fn = '(function(o){var k,u=undefined,h=Object.prototype.hasOwnProperty;try{';
   for (let i = 0; i < path.length; i++) {
     fn += 'k=' + JSON.stringify(path[i]) + ';';
-    fn += 'if(!o.hasOwnProperty(k))return u;o=o[k];';
+    fn += 'if(!h.call(o,k))return u;o=o[k];';
   }
   fn += 'return o}catch(e){return u}})';
   return fn as JavaScript<(doc: unknown) => unknown>;
