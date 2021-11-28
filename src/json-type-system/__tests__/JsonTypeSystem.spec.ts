19b625f3a7ba1d370ac74a31cabab967c2966a25
streamich <streamich@gmail.com>
2021-11-28 14:59:48 +0100

    test(json-type): 💍 add two recursive type test

--- src/json-type-system/__tests__/JsonTypeSystem.spec.ts
+++ src/json-type-system/__tests__/JsonTypeSystem.spec.ts
@@ -1,4 +1,5 @@
 import {t} from '../../json-type';
+import {BooleanValidator} from '../../json-type-validator';
 import {JsonTypeSystem} from '../JsonTypeSystem';
 
 const types = {
@@ -16,6 +17,13 @@ const types = {
     t.Field('id', t.str),
     t.Field('name', t.str),
     t.Field('partner', t.Ref('User'), {isOptional: true}),
+    t.Field('address', t.Ref('Address'), {isOptional: true}),
+  ]),
+
+  'Address': t.Object([
+    t.Field('street', t.str),
+    t.Field('zip', t.str),
+    t.Field('owner', t.Ref('User'), {isOptional: true}),
   ]),
 };
 
@@ -70,4 +78,89 @@ test('can resolve own recursive refs', () => {
   expect(result1).toBe(false);
   expect(result2).toBe(true);
   expect(result3).toBe(false);
+});
+
+const assertUserAndAddress = (userValidator: BooleanValidator, addressValidator: BooleanValidator) => {
+  const result1 = userValidator({
+    id: '123',
+    name: 'John',
+    address: {},
+  });
+  const result2 = userValidator({
+    id: '123',
+    name: 'John',
+    address: {
+      street: '123',
+      zip: '123',
+    },
+  });
+  const result3 = addressValidator({
+    street: '123',
+  });
+  const result4 = addressValidator({
+    street: '123',
+    zip: '123',
+  });
+  const result5 = addressValidator({
+    street: '123',
+    zip: '123',
+    owner: {},
+  });
+  const result6 = addressValidator({
+    street: '123',
+    zip: '123',
+    owner: {
+      id: '1',
+      name: 'Lol',
+    },
+  });
+  const result7 = userValidator({
+    id: '123',
+    name: 'John',
+    address: {
+      street: '123',
+      zip: '123',
+      owner: {
+
+      },
+    },
+  });
+  const result8 = userValidator({
+    id: '123',
+    name: 'John',
+    address: {
+      street: '123',
+      zip: '123',
+      owner: {
+        id: '1',
+        name: 'Lol',
+      },
+    },
+  });
+  expect(result1).toBe(true);
+  expect(result2).toBe(false);
+  expect(result3).toBe(true);
+  expect(result4).toBe(false);
+  expect(result5).toBe(true);
+  expect(result6).toBe(false);
+  expect(result7).toBe(true);
+  expect(result8).toBe(false);
+};
+
+test('can resolve two types reffing each other', () => {
+  const system = new JsonTypeSystem({
+    types,
+  });
+  const userValidator = system.getFastValidator('User');
+  const addressValidator = system.getFastValidator('Address');
+  assertUserAndAddress(userValidator, addressValidator);
+});
+
+test('can resolve two types reffing each other (in reverse)', () => {
+  const system = new JsonTypeSystem({
+    types,
+  });
+  const addressValidator = system.getFastValidator('Address');
+  const userValidator = system.getFastValidator('User');
+  assertUserAndAddress(userValidator, addressValidator);
 });
