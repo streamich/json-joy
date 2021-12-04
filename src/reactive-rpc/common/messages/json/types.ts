244dc1b41f6abd5b1db9d0ae44232d4895b57382
streamich <streamich@gmail.com>
2021-12-04 20:15:20 +0100

    feat(reactive-rpc): 🎸 implement JSON messages

--- src/reactive-rpc/common/messages/json/types.ts
+++ src/reactive-rpc/common/messages/json/types.ts
@@ -4,24 +4,22 @@ import type {JsonNotificationMessage} from './JsonNotificationMessage';
 import type {JsonRequestCompleteMessage} from './JsonRequestCompleteMessage';
 import type {JsonResponseUnsubscribeMessage} from './JsonResponseUnsubscribeMessage';
 import type {JsonResponseErrorMessage} from './JsonResponseErrorMessage';
-
-import {BinaryRequestDataMessage} from './BinaryRequestDataMessage';
-import {BinaryRequestErrorMessage} from './BinaryRequestErrorMessage';
-import {BinaryRequestUnsubscribeMessage} from './BinaryRequestUnsubscribeMessage';
-import {BinaryResponseCompleteMessage} from './BinaryResponseCompleteMessage';
-import {BinaryResponseDataMessage} from './BinaryResponseDataMessage';
+import type {JsonResponseDataMessage} from './JsonResponseDataMessage';
+import type {JsonResponseCompleteMessage} from './JsonResponseCompleteMessage';
+import type {JsonRequestUnsubscribeMessage} from './JsonRequestUnsubscribeMessage';
+import type {JsonRequestErrorMessage} from './JsonRequestErrorMessage';
+import type {JsonRequestDataMessage} from './JsonRequestDataMessage';
 
 export type ReactiveRpcJsonMessage<Data = unknown> =
   | JsonNotificationMessage<Data>
   | JsonRequestCompleteMessage<Data>
   | JsonResponseUnsubscribeMessage
   | JsonResponseErrorMessage
-
-  | BinaryRequestDataMessage
-  | BinaryRequestErrorMessage
-  | BinaryRequestUnsubscribeMessage
-  | BinaryResponseDataMessage
-  | BinaryResponseCompleteMessage;
+  | JsonResponseDataMessage
+  | JsonResponseCompleteMessage
+  | JsonRequestUnsubscribeMessage
+  | JsonRequestErrorMessage
+  | JsonRequestDataMessage;
 
 export interface JsonMessage<Data = unknown> {
   toCompactJson(): json_string<CompactMessage<Data>>;
