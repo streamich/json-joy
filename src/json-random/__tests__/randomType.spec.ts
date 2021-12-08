e84665c14949ef8b41d8c2ac6a009a797bb2899b
streamich <streamich@gmail.com>
2021-12-08 20:30:29 +0100

    feat(json-random): 🎸 implement random generator according to json-type

---
+++ src/json-random/__tests__/randomType.spec.ts
@@ -0,0 +1,21 @@
+import {t} from '../../json-type';
+import {randomType} from '../randomType';
+
+test('generates random JSON', () => {
+  const mathRandom = Math.random;
+  let i = 0.0;
+  Math.random = () => {
+    i += 0.0379;
+    if (i >= 1) i -= 1;
+    return i;
+  };
+  const type = t.Object([
+    t.Field('id', t.str),
+    t.Field('name', t.str),
+    t.Field('tags', t.Array(t.str)),
+    t.Field('scores', t.Array(t.num), {isOptional: true}),
+  ]);
+  const json = randomType(type, {});
+  expect(json).toStrictEqual({ id: 'ᴛ', name: '58<', tags: [ 'JNQUY\\`', 'jnquy|"%),04', '>BEIL' ] });
+  Math.random = mathRandom;
+});
---
+++ src/json-random/randomType.ts
@@ -0,0 +1,63 @@
+import {RandomJson} from ".";
+import {deepClone} from "../json-patch/util";
+import {TAnyType} from "../json-type";
+
+export interface RandomTypeOptions {
+  ref?: (ref: string) => TAnyType;
+}
+
+export const randomType = (type: TAnyType, options: RandomTypeOptions): unknown => {
+  switch (type.__t) {
+    case 'nil': return null;
+    case 'bool': {
+      if (type.const) return type.const;
+      return RandomJson.genBoolean();
+    }
+    case 'num': {
+      if (type.const) return type.const;
+      return RandomJson.genNumber();
+    }
+    case 'str': {
+      if (type.const) return type.const;
+      return RandomJson.genString();
+    }
+    case 'arr': {
+      const length = Math.round(Math.random() * 10);
+      const arr = [];
+      for (let i = 0; i < length; i++) {
+        arr.push(randomType(type.type as TAnyType, options));
+      }
+      return arr;
+    }
+    case 'obj': {
+      const obj: {[key: string]: unknown} = type.unknownFields ? RandomJson.genObject() as {[key: string]: unknown} : {};
+      for (const field of type.fields) {
+        if (field.isOptional) if (Math.random() > 0.5) continue;
+        obj[field.key] = randomType(field.type as TAnyType, options);
+      }
+      return obj;
+    }
+    case 'enum': {
+      const index = Math.floor(Math.random() * type.values.length);
+      const value = deepClone(type.values[index]);
+      return value;
+    }
+    case 'ref': {
+      if (!options.ref) {
+        throw new Error('Reference resolution callback not provided.');
+      }
+      return randomType(options.ref(type.ref), options);
+    }
+    case 'any': {
+      return RandomJson.generate({nodeCount: 5});
+    }
+    case 'bin': {
+      const octets = RandomJson.genString().split('').map(c => c.charCodeAt(0));
+      return new Uint8Array(octets);
+    }
+    case 'or': {
+      const index = Math.floor(Math.random() * type.types.length);
+      return randomType(type.types[index] as TAnyType, options);
+    }
+  }
+};
