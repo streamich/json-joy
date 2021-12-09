0e07b3238418ab5d8e08c897889826a67d1df3a5
streamich <streamich@gmail.com>
2021-12-09 19:56:03 +0100

    perf(util): ⚡️ make word wrap return a list of lines

--- src/util/__tests__/wordWrap.spec.ts
+++ src/util/__tests__/wordWrap.spec.ts
@@ -1,7 +1,7 @@
 import {wordWrap} from '../wordWrap';
 
 test('does not format a short line', () => {
-  expect(wordWrap('Hello')).toBe('Hello');
+  expect(wordWrap('Hello')).toStrictEqual(['Hello']);
 });
 
 const text =
@@ -10,42 +10,33 @@ const text =
 test('wraps long text', () => {
   const result = wordWrap(text);
   expect(result).toMatchInlineSnapshot(`
-    "Acclaimed Harvard professor and entrepreneur Dr.
-    David Sinclair believes that we will see human
-    life expectancy increase to at least 100 years
-    within this century. A world in which humans live
-    significantly longer will have a major impact on
-    economies, policies, healthcare, education,
-    ethics, and more. Sinclair joined  Bridgewater
-    Portfolio Strategist Atul Lele to discuss the
-    science and societal, political, systemic and
-    ethical implications of humans living
-    significantly longer lives."
+    Array [
+      "Acclaimed Harvard professor and entrepreneur Dr. ",
+      "David Sinclair believes that we will see human ",
+      "life expectancy increase to at least 100 years ",
+      "within this century. A world in which humans live ",
+      "significantly longer will have a major impact on ",
+      "economies, policies, healthcare, education, ",
+      "ethics, and more. Sinclair joined  Bridgewater ",
+      "Portfolio Strategist Atul Lele to discuss the ",
+      "science and societal, political, systemic and ",
+      "ethical implications of humans living ",
+      "significantly longer lives.",
+    ]
   `);
 });
 
 test('can specify line width', () => {
   const result = wordWrap(text, {width: 80});
   expect(result).toMatchInlineSnapshot(`
-    "Acclaimed Harvard professor and entrepreneur Dr. David Sinclair believes that we
-    will see human life expectancy increase to at least 100 years within this
-    century. A world in which humans live significantly longer will have a major
-    impact on economies, policies, healthcare, education, ethics, and more. Sinclair
-    joined  Bridgewater Portfolio Strategist Atul Lele to discuss the science and
-    societal, political, systemic and ethical implications of humans living
-    significantly longer lives."
-  `);
-});
-
-test('can specify indent', () => {
-  const result = wordWrap(text, {width: 80, indent: '    '});
-  expect(result).toMatchInlineSnapshot(`
-    "    Acclaimed Harvard professor and entrepreneur Dr. David Sinclair believes that we
-        will see human life expectancy increase to at least 100 years within this
-        century. A world in which humans live significantly longer will have a major
-        impact on economies, policies, healthcare, education, ethics, and more. Sinclair
-        joined  Bridgewater Portfolio Strategist Atul Lele to discuss the science and
-        societal, political, systemic and ethical implications of humans living
-        significantly longer lives."
+    Array [
+      "Acclaimed Harvard professor and entrepreneur Dr. David Sinclair believes that we ",
+      "will see human life expectancy increase to at least 100 years within this ",
+      "century. A world in which humans live significantly longer will have a major ",
+      "impact on economies, policies, healthcare, education, ethics, and more. Sinclair ",
+      "joined  Bridgewater Portfolio Strategist Atul Lele to discuss the science and ",
+      "societal, political, systemic and ethical implications of humans living ",
+      "significantly longer lives.",
+    ]
   `);
 });
--- src/util/wordWrap.ts
+++ src/util/wordWrap.ts
@@ -1,21 +1,16 @@
 export interface WrapOptions {
-  indent?: string;
   width?: number;
-  newline?: string;
 }
 
-const lineMap = (line: string) => (line.slice(-1) === '\n' ? line.slice(0, line.length - 1) : line);
+const lineMap = (line: string) => (line.slice(-1) === '\n' ? line.slice(0, line.length - 1).replace(/[ \t]*$/gm, '') : line);
 
-export const wordWrap = (str: string, options: WrapOptions = {}): string => {
-  if (!str) return '';
+export const wordWrap = (str: string, options: WrapOptions = {}): string[] => {
+  if (!str) return [];
 
   const width = options.width || 50;
-  const indent = options.indent || '';
-  const newline = options.newline || '\n' + indent;
   const regexString = '.{1,' + width + '}([\\s\u200B]+|$)|[^\\s\u200B]+?([\\s\u200B]+|$)';
   const re = new RegExp(regexString, 'g');
-  const lines = str.match(re) || [];
-  const result = indent + lines.map(lineMap).join(newline);
+  const lines = (str.match(re) || []).map(lineMap);
 
-  return result.replace(/[ \t]*$/gm, '');
+  return lines;
 };
