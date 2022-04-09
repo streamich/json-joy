d1f6f042cab7f55152eb73e7396a1981df008889
streamich <streamich@gmail.com>
2022-04-09 17:36:33 +0200

    test(json-crdt): 💍 harden codec automated tests

--- src/json-crdt/codec/compact/__tests__/automated-logical.spec.ts
+++ src/json-crdt/codec/compact/__tests__/automated-logical.spec.ts
@@ -6,14 +6,36 @@ import {documents} from '../../../../__tests__/json-documents';
 import {binaryDocuments} from '../../../../__tests__/binary-documents';
 
 for (const {name, json} of [...documents, ...binaryDocuments]) {
-  test(name, () => {
-    const doc1 = Model.withLogicalClock(new LogicalVectorClock(222, 0));
-    doc1.api.root(json).commit();
+  describe('fresh encoder and decoder', () => {
+    test(name, () => {
+      const doc1 = Model.withLogicalClock(new LogicalVectorClock(222, 0));
+      doc1.api.root(json).commit();
+      const encoder = new LogicalEncoder();
+      const decoder = new LogicalDecoder();
+      const encoded1 = encoder.encode(doc1);
+      const doc2 = decoder.decode(encoded1);
+      const encoded2 = encoder.encode(doc1);
+      const doc3 = decoder.decode(encoded2);
+      expect(doc1.toView()).toEqual(json);
+      expect(doc2.toView()).toEqual(json);
+      expect(doc3.toView()).toEqual(json);
+    });
+  });
+
+  describe('shared encoder and decoder', () => {
     const encoder = new LogicalEncoder();
     const decoder = new LogicalDecoder();
-    const encoded = encoder.encode(doc1);
-    const doc2 = decoder.decode(encoded);
-    expect(doc1.toJson()).toEqual(json);
-    expect(doc2.toJson()).toEqual(json);
+
+    test(name, () => {
+      const doc1 = Model.withLogicalClock(new LogicalVectorClock(222, 0));
+      doc1.api.root(json).commit();
+      const encoded1 = encoder.encode(doc1);
+      const doc2 = decoder.decode(encoded1);
+      const encoded2 = encoder.encode(doc1);
+      const doc3 = decoder.decode(encoded2);
+      expect(doc1.toView()).toEqual(json);
+      expect(doc2.toView()).toEqual(json);
+      expect(doc3.toView()).toEqual(json);
+    });
   });
 }
