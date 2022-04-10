03cd51a1240489ac83241ced3a923f5ec3a4c215
streamich <streamich@gmail.com>
2022-04-10 12:09:56 +0200

    feat(json-crdt-patch): 🎸 add compact-binary patch codec

---
+++ src/json-crdt-patch/codec/compact-binary/encode.ts
@@ -0,0 +1,8 @@
+import {encode as encodeCompact} from '../compact/encode'
+import {encodeFull as encodeMsgPack} from '../../../json-pack/util';
+import {Patch} from '../../Patch';
+import type {MsgPack} from '../../../json-pack';
+
+export const encode = (patch: Patch): MsgPack<unknown[]> => {
+  return encodeMsgPack(encodeCompact(patch));
+};
