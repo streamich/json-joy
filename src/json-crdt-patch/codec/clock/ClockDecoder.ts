64f1d26a585097a144b93842ea631bc111024e33
streamich <streamich@gmail.com>
2022-04-12 15:50:27 +0200

    feat(json-crdt-patch): 🎸 use constants in vector clock codec

--- src/json-crdt-patch/codec/clock/ClockDecoder.ts
+++ src/json-crdt-patch/codec/clock/ClockDecoder.ts
@@ -1,4 +1,5 @@
 import {ITimestamp, LogicalTimestamp, LogicalVectorClock} from '../../clock';
+import {FALSE_ID, NULL_ID, SESSION, SYSTEM_SESSION_TIME, TRUE_ID, UNDEFINED_ID} from '../../constants';
 
 export class ClockDecoder {
   /** Clock session index to logical clock. */
@@ -27,7 +28,15 @@ export class ClockDecoder {
   }
 
   public decodeId(sessionIndex: number, timeDiff: number): ITimestamp {
-    if (!sessionIndex) return new LogicalTimestamp(0, timeDiff);
+    if (sessionIndex === SESSION.SYSTEM) {
+      switch (timeDiff) {
+        case SYSTEM_SESSION_TIME.NULL: return NULL_ID;
+        case SYSTEM_SESSION_TIME.TRUE: return TRUE_ID;
+        case SYSTEM_SESSION_TIME.FALSE: return FALSE_ID;
+        case SYSTEM_SESSION_TIME.UNDEFINED: return UNDEFINED_ID;
+      }
+      return new LogicalTimestamp(SESSION.SYSTEM, timeDiff);
+    }
     const ts = this.table.get(sessionIndex);
     if (!ts) throw new Error('INVALID_CLOCK_TABLE');
     return new LogicalTimestamp(ts.getSessionId(), ts.time - timeDiff);
