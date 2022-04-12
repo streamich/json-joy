0f572e95938e587bf4721cc8defd084c87b80972
streamich <streamich@gmail.com>
2022-04-12 16:43:24 +0200

    refactor(json-crdt-patch): 💡 add .rewriteTime() method to Patch

--- src/json-crdt-patch/Draft.ts
+++ src/json-crdt-patch/Draft.ts
@@ -1,21 +1,6 @@
-import {DeleteOperation} from './operations/DeleteOperation';
-import {InsertArrayElementsOperation} from './operations/InsertArrayElementsOperation';
-import {InsertBinaryDataOperation} from './operations/InsertBinaryDataOperation';
-import {InsertStringSubstringOperation} from './operations/InsertStringSubstringOperation';
 import {LogicalVectorClock, ITimestamp, IClock, LogicalTimestamp} from './clock';
-import {MakeArrayOperation} from './operations/MakeArrayOperation';
-import {MakeBinaryOperation} from './operations/MakeBinaryOperation';
-import {MakeConstantOperation} from './operations/MakeConstantOperation';
-import {MakeNumberOperation} from './operations/MakeNumberOperation';
-import {MakeObjectOperation} from './operations/MakeObjectOperation';
-import {MakeStringOperation} from './operations/MakeStringOperation';
-import {MakeValueOperation} from './operations/MakeValueOperation';
-import {NoopOperation} from './operations/NoopOperation';
 import {Patch} from './Patch';
 import {PatchBuilder} from './PatchBuilder';
-import {SetNumberOperation} from './operations/SetNumberOperation';
-import {SetObjectKeysOperation} from './operations/SetObjectKeysOperation';
-import {SetRootOperation} from './operations/SetRootOperation';
 
 /**
  * Draft class provides a way to build a patch for which it is not known the
@@ -45,38 +30,7 @@ export class Draft {
    * with correct timestamps.
    */
   public patch(clock: IClock): Patch {
-    const patch = new Patch();
-    const ops = this.builder.patch.ops;
-    const ts = (id: ITimestamp) =>
-      id.getSessionId() !== -1 ? id : new LogicalTimestamp(clock.getSessionId(), clock.time + id.time);
-    for (const op of ops) {
-      if (op instanceof DeleteOperation) patch.ops.push(new DeleteOperation(ts(op.id), ts(op.obj), op.after));
-      else if (op instanceof InsertArrayElementsOperation)
-        patch.ops.push(new InsertArrayElementsOperation(ts(op.id), ts(op.arr), ts(op.after), op.elements.map(ts)));
-      else if (op instanceof InsertStringSubstringOperation)
-        patch.ops.push(new InsertStringSubstringOperation(ts(op.id), ts(op.obj), ts(op.after), op.substring));
-      else if (op instanceof InsertBinaryDataOperation)
-        patch.ops.push(new InsertBinaryDataOperation(ts(op.id), ts(op.obj), ts(op.after), op.data));
-      else if (op instanceof MakeArrayOperation) patch.ops.push(new MakeArrayOperation(ts(op.id)));
-      else if (op instanceof MakeConstantOperation) patch.ops.push(new MakeConstantOperation(ts(op.id), op.value));
-      else if (op instanceof MakeValueOperation) patch.ops.push(new MakeValueOperation(ts(op.id), op.value));
-      else if (op instanceof MakeNumberOperation) patch.ops.push(new MakeNumberOperation(ts(op.id)));
-      else if (op instanceof MakeObjectOperation) patch.ops.push(new MakeObjectOperation(ts(op.id)));
-      else if (op instanceof MakeStringOperation) patch.ops.push(new MakeStringOperation(ts(op.id)));
-      else if (op instanceof MakeBinaryOperation) patch.ops.push(new MakeBinaryOperation(ts(op.id)));
-      else if (op instanceof SetNumberOperation)
-        patch.ops.push(new SetNumberOperation(ts(op.id), ts(op.num), op.value));
-      else if (op instanceof SetObjectKeysOperation)
-        patch.ops.push(
-          new SetObjectKeysOperation(
-            ts(op.id),
-            ts(op.object),
-            op.tuples.map(([key, value]) => [key, ts(value)]),
-          ),
-        );
-      else if (op instanceof SetRootOperation) patch.ops.push(new SetRootOperation(ts(op.id), ts(op.value)));
-      else if (op instanceof NoopOperation) patch.ops.push(new NoopOperation(ts(op.id), op.length));
-    }
-    return patch;
+    return this.builder.patch.rewriteTime((id: ITimestamp) =>
+      id.getSessionId() !== -1 ? id : new LogicalTimestamp(clock.getSessionId(), clock.time + id.time));
   }
 }
