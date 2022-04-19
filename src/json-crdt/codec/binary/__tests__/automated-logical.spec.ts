650063ae18f1ad19e1a077081337c03830298167
streamich <streamich@gmail.com>
2022-04-19 19:11:37 +0200

    test(json-crdt): 💍 add ViewDecoder to automated test suite

--- src/json-crdt/codec/binary/__tests__/automated-logical.spec.ts
+++ src/json-crdt/codec/binary/__tests__/automated-logical.spec.ts
@@ -2,6 +2,7 @@ import {LogicalVectorClock} from '../../../../json-crdt-patch/clock';
 import {Model} from '../../../model';
 import {Encoder} from '../Encoder';
 import {Decoder} from '../Decoder';
+import {ViewDecoder} from '../ViewDecoder';
 import {documents} from '../../../../__tests__/json-documents';
 import {binaryDocuments} from '../../../../__tests__/binary-documents';
 
@@ -12,19 +13,23 @@ for (const {name, json} of [...documents, ...binaryDocuments]) {
       doc1.api.root(json).commit();
       const encoder = new Encoder();
       const decoder = new Decoder();
+      const viewDecoder = new ViewDecoder();
       const encoded1 = encoder.encode(doc1);
       const doc2 = decoder.decode(encoded1);
       const encoded2 = encoder.encode(doc1);
       const doc3 = decoder.decode(encoded2);
+      const json2 = viewDecoder.decode(encoded2);
       expect(doc1.toView()).toEqual(json);
       expect(doc2.toView()).toEqual(json);
       expect(doc3.toView()).toEqual(json);
+      expect(json2).toEqual(json);
     });
   });
 
   describe('shared encoder and decoder', () => {
     const encoder = new Encoder();
     const decoder = new Decoder();
+    const viewDecoder = new ViewDecoder();
 
     test(name, () => {
       const doc1 = Model.withLogicalClock(new LogicalVectorClock(222, 0));
@@ -33,9 +38,11 @@ for (const {name, json} of [...documents, ...binaryDocuments]) {
       const doc2 = decoder.decode(encoded1);
       const encoded2 = encoder.encode(doc1);
       const doc3 = decoder.decode(encoded2);
+      const json2 = viewDecoder.decode(encoded2);
       expect(doc1.toView()).toEqual(json);
       expect(doc2.toView()).toEqual(json);
       expect(doc3.toView()).toEqual(json);
+      expect(json2).toEqual(json);
     });
   });
 }
