9204ecfee6c60bff1306c808c7ff251cd63f0fde
streamich <streamich@gmail.com>
2023-05-19 21:58:37 +0200

    feat(json-crdt): 🎸 add string RGA data type

---
+++ src/json-crdt-patch/Identifiable.ts
@@ -0,0 +1,24 @@
+import type {ITimestampStruct} from './clock';
+
+/**
+ * Something in the document that can be identified by ID. All operations have
+ * IDs and operations result into JSON nodes and chunks, which also have IDs.
+ */
+export interface Identifiable {
+  /**
+   * Unique ID within a document.
+   */
+  id: ITimestampStruct;
+
+  /**
+   * Sometimes an Identifiable can be a compound entity, which holds multiple
+   * entries with sequentially growing timestamps. In this case `span` represents
+   * the number of entries.
+   */
+  span?(): number;
+
+  /**
+   * Used for debugging.
+   */
+  toString(tab?: string): string;
+}
