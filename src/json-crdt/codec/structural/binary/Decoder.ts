e2b8345768f973e8fdcf97980f5f2e515f5aee17
streamich <streamich@gmail.com>
2023-08-04 21:52:23 +0200

    feat(json-crdt): 🎸 add binary JSON CRDT encoding

---
+++ src/json-crdt/codec/structural/binary/Decoder.ts
@@ -0,0 +1,208 @@
+import {ArrayRga, ArrayChunk} from '../../../types/rga-array/ArrayRga';
+import {BinaryRga, BinaryChunk} from '../../../types/rga-binary/BinaryRga';
+import {ClockDecoder} from '../../../../json-crdt-patch/codec/clock/ClockDecoder';
+import {Const} from '../../../types/const/Const';
+import {CrdtDecoder} from '../../../../json-crdt-patch/util/binary/CrdtDecoder';
+import {ITimestampStruct, Timestamp} from '../../../../json-crdt-patch/clock';
+import {Model, UNDEFINED} from '../../../model';
+import {MsgPackDecoderFast} from '../../../../json-pack/msgpack';
+import {ObjectLww} from '../../../types/lww-object/ObjectLww';
+import {RootLww} from '../../../types/lww-root/RootLww';
+import {SESSION} from '../../../../json-crdt-patch/constants';
+import {StringRga, StringChunk} from '../../../types/rga-string/StringRga';
+import {ValueLww} from '../../../types/lww-value/ValueLww';
+import {ArrayLww} from '../../../types/lww-array/ArrayLww';
+import type {JsonNode} from '../../../types';
+
+export class Decoder extends MsgPackDecoderFast<CrdtDecoder> {
+  protected doc!: Model;
+  protected clockDecoder?: ClockDecoder;
+  protected time: number = -1;
+
+  constructor() {
+    super(new CrdtDecoder());
+  }
+
+  public decode(data: Uint8Array): Model {
+    delete this.clockDecoder;
+    this.time = -1;
+    const reader = this.reader;
+    reader.reset(data);
+    const isServerTime = reader.u8() === 0;
+    if (isServerTime) {
+      const time = (this.time = reader.vu57());
+      this.doc = Model.withServerClock(time);
+    } else {
+      this.decodeClockTable();
+      const clock = this.clockDecoder!.clock;
+      this.doc = Model.withLogicalClock(clock);
+    }
+    this.doc.root = new RootLww(this.doc, this.cRoot().id);
+    delete this.clockDecoder;
+    return this.doc;
+  }
+
+  protected decodeClockTable(): void {
+    const reader = this.reader;
+    const clockTableOffset = reader.u32();
+    const offset = reader.x;
+    reader.x += clockTableOffset;
+    const length = reader.vu39();
+    const [sessionId, time] = reader.u53vu39();
+    this.clockDecoder = new ClockDecoder(sessionId, time);
+    for (let i = 1; i < length; i++) {
+      const [sid, time] = reader.u53vu39();
+      this.clockDecoder.pushTuple(sid, time);
+    }
+    reader.x = offset;
+  }
+
+  protected ts(): ITimestampStruct {
+    const decoderTime = this.time!;
+    const isLogical = decoderTime < 0;
+    if (isLogical) {
+      const [sessionIndex, timeDiff] = this.reader.id();
+      return this.clockDecoder!.decodeId(sessionIndex, timeDiff);
+    } else {
+      return new Timestamp(SESSION.SERVER, this.reader.vu57());
+    }
+  }
+
+  protected cRoot(): JsonNode {
+    const reader = this.reader;
+    const peek = reader.uint8[reader.x];
+    return !peek ? UNDEFINED : this.cNode();
+  }
+
+  public cNode(): JsonNode {
+    const reader = this.reader;
+    const id = this.ts();
+    const byte = reader.u8();
+    if (byte <= 0b10001111) return this.cObj(id, byte & 0b1111);
+    else if (byte <= 0b10011111) return this.cArr(id, byte & 0b1111);
+    else if (byte <= 0b10111111) return this.cStr(id, byte & 0b11111);
+    else {
+      switch (byte) {
+        case 0xc4:
+          return this.cBin(id, reader.u8());
+        case 0xc5:
+          return this.cBin(id, reader.u16());
+        case 0xc6:
+          return this.cBin(id, reader.u32());
+        case 0xd4: {
+          const obj = new Const(id, this.val());
+          this.doc.index.set(obj);
+          return obj;
+        }
+        case 0xd5: {
+          const obj = new Const(id, this.ts());
+          this.doc.index.set(obj);
+          return obj;
+        }
+        case 0xd6: {
+          const val = this.cNode();
+          const obj = new ValueLww(this.doc, id, val.id);
+          this.doc.index.set(obj);
+          return obj;
+        }
+        case 0xde:
+          return this.cObj(id, reader.u16());
+        case 0xdf:
+          return this.cObj(id, reader.u32());
+        case 0xdc:
+          return this.cArr(id, reader.u16());
+        case 0xdd:
+          return this.cArr(id, reader.u32());
+        case 0xd9:
+          return this.cStr(id, reader.u8());
+        case 0xda:
+          return this.cStr(id, reader.u16());
+        case 0xdb:
+          return this.cStr(id, reader.u32());
+        case 0xc7:
+          return this.cTup(id);
+      }
+    }
+    throw new Error('UNKNOWN_NODE');
+  }
+
+  public cObj(id: ITimestampStruct, length: number): ObjectLww {
+    const obj = new ObjectLww(this.doc, id);
+    for (let i = 0; i < length; i++) this.cObjChunk(obj);
+    this.doc.index.set(obj);
+    return obj;
+  }
+
+  private cObjChunk(obj: ObjectLww): void {
+    const key: string = this.key();
+    obj.keys.set(key, this.cNode().id);
+  }
+
+  public cTup(id: ITimestampStruct): ArrayLww {
+    const reader = this.reader;
+    const length = this.reader.u8();
+    reader.x++;
+    const obj = new ArrayLww(this.doc, id);
+    const elements = obj.elements;
+    for (let i = 0; i < length; i++) {
+      const octet = reader.peak();
+      if (!octet) {
+        reader.x++;
+        elements.push(undefined);
+      } else elements.push(this.cNode().id);
+    }
+    this.doc.index.set(obj);
+    return obj;
+  }
+
+  public cArr(id: ITimestampStruct, length: number): ArrayRga {
+    const obj = new ArrayRga(this.doc, id);
+    obj.ingest(length, this.cArrChunk);
+    this.doc.index.set(obj);
+    return obj;
+  }
+
+  private readonly cArrChunk = (): ArrayChunk => {
+    const [deleted, length] = this.reader.b1vu28();
+    const id = this.ts();
+    if (deleted) return new ArrayChunk(id, length, undefined);
+    const ids: ITimestampStruct[] = [];
+    for (let i = 0; i < length; i++) ids.push(this.cNode().id);
+    return new ArrayChunk(id, length, ids);
+  };
+
+  public cStr(id: ITimestampStruct, length: number): StringRga {
+    const node = new StringRga(id);
+    if (length) node.ingest(length, this.cStrChunk);
+    this.doc.index.set(node);
+    return node;
+  }
+
+  private cStrChunk = (): StringChunk => {
+    const reader = this.reader;
+    const id = this.ts();
+    const isTombstone = reader.uint8[reader.x] === 0;
+    if (isTombstone) {
+      reader.x++;
+      const length = reader.vu39();
+      return new StringChunk(id, length, '');
+    }
+    const text: string = this.str() as string;
+    return new StringChunk(id, text.length, text);
+  };
+
+  public cBin(id: ITimestampStruct, length: number): BinaryRga {
+    const node = new BinaryRga(id);
+    if (length) node.ingest(length, this.cBinChunk);
+    this.doc.index.set(node);
+    return node;
+  }
+
+  private cBinChunk = (): BinaryChunk => {
+    const reader = this.reader;
+    const [deleted, length] = reader.b1vu28();
+    const id = this.ts();
+    if (deleted) return new BinaryChunk(id, length, undefined);
+    else return new BinaryChunk(id, length, reader.buf(length));
+  };
+}
---
+++ src/json-crdt/codec/structural/binary/Encoder.ts
@@ -0,0 +1,188 @@
+import {ArrayRga} from '../../../types/rga-array/ArrayRga';
+import {BinaryRga} from '../../../types/rga-binary/BinaryRga';
+import {ClockEncoder} from '../../../../json-crdt-patch/codec/clock/ClockEncoder';
+import {Const} from '../../../types/const/Const';
+import {CrdtWriter} from '../../../../json-crdt-patch/util/binary/CrdtEncoder';
+import {ITimestampStruct, Timestamp} from '../../../../json-crdt-patch/clock';
+import {JsonNode} from '../../../types';
+import {MsgPackEncoder} from '../../../../json-pack/msgpack';
+import {ObjectLww} from '../../../types/lww-object/ObjectLww';
+import {RootLww} from '../../../types/lww-root/RootLww';
+import {SESSION} from '../../../../json-crdt-patch/constants';
+import {StringRga} from '../../../types/rga-string/StringRga';
+import {ValueLww} from '../../../types/lww-value/ValueLww';
+import {ArrayLww} from '../../../types/lww-array/ArrayLww';
+import type {Model} from '../../../model';
+
+export class Encoder extends MsgPackEncoder<CrdtWriter> {
+  protected clockEncoder: ClockEncoder = new ClockEncoder();
+  protected time: number = 0;
+  protected doc!: Model;
+
+  constructor() {
+    super(new CrdtWriter());
+  }
+
+  public encode(doc: Model): Uint8Array {
+    this.doc = doc;
+    this.writer.reset();
+    if (doc.clock.sid === SESSION.SERVER) this.encodeServer(doc);
+    else this.encodeLogical(doc);
+    return this.writer.flush();
+  }
+
+  public encodeLogical(model: Model): void {
+    const writer = this.writer;
+    this.ts = this.tsLogical;
+    writer.u8(1);
+    this.clockEncoder.reset(model.clock);
+    writer.ensureCapacity(4);
+    const x0 = writer.x0;
+    const x = writer.x;
+    writer.x += 4;
+    this.cRoot(model.root);
+    this.encodeClockTable(x0, x);
+  }
+
+  public encodeServer(model: Model): void {
+    this.ts = this.tsServer;
+    this.writer.u8(0);
+    this.writer.vu57((this.time = model.clock.time));
+    this.cRoot(model.root);
+  }
+
+  protected encodeClockTable(x0: number, x: number) {
+    const writer = this.writer;
+    const shift = writer.x0 - x0;
+    writer.view.setUint32(writer.x0 + (x - x0), writer.x - x - shift - 4);
+    const clockEncoder = this.clockEncoder!;
+    const table = clockEncoder.table;
+    const length = table.size;
+    writer.vu39(length);
+    table.forEach(this.cTableEntry);
+  }
+
+  protected readonly cTableEntry = (entry: {clock: ITimestampStruct}) => {
+    const clock = entry.clock;
+    this.writer.u53vu39(clock.sid, clock.time);
+  };
+
+  protected readonly tsLogical = (ts: ITimestampStruct): void => {
+    const relativeId = this.clockEncoder!.append(ts);
+    this.writer.id(relativeId.sessionIndex, relativeId.timeDiff);
+  };
+
+  protected readonly tsServer = (ts: ITimestampStruct): void => {
+    this.writer.vu57(ts.time);
+  };
+
+  protected ts: (ts: ITimestampStruct) => void = this.tsLogical;
+
+  protected cRoot(root: RootLww): void {
+    const val = root.val;
+    if (val.sid === SESSION.SYSTEM) this.writer.u8(0);
+    else this.cNode(root.node());
+  }
+
+  protected cNode(node: JsonNode): void {
+    // TODO: PERF: use a switch
+    if (node instanceof Const) this.cConst(node);
+    else if (node instanceof ValueLww) this.cVal(node);
+    else if (node instanceof StringRga) this.cStr(node);
+    else if (node instanceof ObjectLww) this.cObj(node);
+    else if (node instanceof ArrayLww) this.cTup(node);
+    else if (node instanceof ArrayRga) this.cArr(node);
+    else if (node instanceof BinaryRga) this.cBin(node);
+  }
+
+  protected cObj(obj: ObjectLww): void {
+    this.ts(obj.id);
+    this.encodeObjectHeader(obj.keys.size);
+    obj.keys.forEach(this.cKey);
+  }
+
+  protected readonly cKey = (val: ITimestampStruct, key: string) => {
+    this.encodeString(key);
+    this.cNode(this.doc.index.get(val)!);
+  };
+
+  protected cTup(obj: ArrayLww): void {
+    this.ts(obj.id);
+    const elements = obj.elements;
+    const length = elements.length;
+    const writer = this.writer;
+    writer.u8u16(0xc7, length << 8);
+    const index = this.doc.index;
+    for (let i = 0; i < length; i++) {
+      const elementId = elements[i];
+      if (!elementId) this.writer.u8(0);
+      else this.cNode(index.get(elementId)!);
+    }
+  }
+
+  protected cArr(obj: ArrayRga): void {
+    const ts = this.ts;
+    const writer = this.writer;
+    ts(obj.id);
+    this.encodeArrayHeader(obj.size());
+    const index = this.doc.index;
+    for (let chunk = obj.first(); chunk; chunk = obj.next(chunk)) {
+      const span = chunk.span;
+      const deleted = chunk.del;
+      writer.b1vu28(deleted, span);
+      ts(chunk.id);
+      if (deleted) continue;
+      const nodes = chunk.data!;
+      for (let i = 0; i < span; i++) this.cNode(index.get(nodes[i])!);
+    }
+  }
+
+  protected cStr(obj: StringRga): void {
+    const ts = this.ts;
+    const writer = this.writer;
+    ts(obj.id);
+    const length = obj.size();
+    this.encodeStringHeader(length);
+    for (let chunk = obj.first(); chunk; chunk = obj.next(chunk)) {
+      ts(chunk.id);
+      if (chunk.del) {
+        writer.u8(0);
+        writer.vu39(chunk.span);
+      } else this.encodeString(chunk.data!);
+    }
+  }
+
+  protected cBin(obj: BinaryRga): void {
+    const ts = this.ts;
+    const writer = this.writer;
+    ts(obj.id);
+    const length = obj.size();
+    this.encodeBinaryHeader(length);
+    for (let chunk = obj.first(); chunk; chunk = obj.next(chunk)) {
+      const length = chunk.span;
+      const deleted = chunk.del;
+      writer.b1vu28(chunk.del, length);
+      ts(chunk.id);
+      if (deleted) continue;
+      writer.buf(chunk.data!, length);
+    }
+  }
+
+  protected cVal(obj: ValueLww): void {
+    this.ts(obj.id);
+    this.writer.u8(0xd6);
+    this.cNode(obj.node());
+  }
+
+  protected cConst(obj: Const): void {
+    this.ts(obj.id);
+    const val = obj.val;
+    if (val instanceof Timestamp) {
+      this.writer.u8(0xd5);
+      this.ts(val as Timestamp);
+    } else {
+      this.writer.u8(0xd4);
+      this.writeAny(val);
+    }
+  }
+}
