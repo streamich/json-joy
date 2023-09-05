834646e184e5ee37b652b159ccf7b02b278e54aa
streamich <streamich@gmail.com>
2023-09-05 14:43:20 +0200

    feat(json-type): 🎸 add ability to extend type router

--- src/json-type/system/TypeRouter.ts
+++ src/json-type/system/TypeRouter.ts
@@ -1,20 +1,38 @@
-import {TypeSystem} from "./TypeSystem";
 import * as classes from "../type/classes";
+import type {TypeSystem} from "./TypeSystem";
 import type {TypeBuilder} from "../type/TypeBuilder";
 
-export interface TypeRouterOptions<R extends Record<string, classes.FunctionType<any, any>>> {
+export interface TypeRouterOptions<R extends RoutesBase> {
   system: TypeSystem;
   routes: R;
 }
 
-export class TypeRouter<R extends Record<string, classes.FunctionType<any, any>>> {
+export class TypeRouter<Routes extends RoutesBase> {
   public system: TypeSystem;
   public t: TypeBuilder;
-  public routes: R;
+  public routes: Routes;
 
-  constructor(options: TypeRouterOptions<R>) {
+  constructor(options: TypeRouterOptions<Routes>) {
     this.system = options.system;
     this.t = this.system.t;
     this.routes = options.routes;
   }
+
+  protected merge<Router extends TypeRouter<any>>(router: Router): TypeRouter<Routes & TypeRouterRoutes<Router>> {
+    return new TypeRouter({
+      system: this.system,
+      routes: {
+        ...this.routes,
+        ...router.routes,
+      },
+    });
+  }
+
+  public extend<NewRoutes extends RoutesBase>(routes: (t: this) => NewRoutes): TypeRouter<Routes & NewRoutes> {
+    const router = new TypeRouter({system: this.system, routes: routes(this)});
+    return this.merge(router);
+  }
 }
+
+type RoutesBase = Record<string, classes.FunctionType<any, any>>;
+type TypeRouterRoutes<R extends TypeRouter<any>> = R extends TypeRouter<infer R2> ? R2 : never;
