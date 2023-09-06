2f16ae6d39b402528c927409c3012f41b3b6f21a
streamich <streamich@gmail.com>
2023-09-06 11:10:57 +0200

    feat(json-cli): 🎸 support input from STDIN

--- src/json-type-cli/Cli.ts
+++ src/json-type-cli/Cli.ts
@@ -6,12 +6,17 @@ import {TypeRouter} from '../json-type/system/TypeRouter';
 import {TypeRouterCaller} from '../reactive-rpc/common/rpc/caller/TypeRouterCaller';
 import type {Value} from '../reactive-rpc/common/messages/Value';
 import type {TypeBuilder} from '../json-type/type/TypeBuilder';
-import type {WriteStream} from 'tty';
+import type {WriteStream, ReadStream} from 'tty';
 
 export interface CliOptions<Router extends TypeRouter<any>> {
   router?: Router;
+}
+
+export interface RunOptions {
+  argv?: string[];
   stdout?: WriteStream;
   stderr?: WriteStream;
+  stdin?: ReadStream;
 }
 
 export class Cli<Router extends TypeRouter<any>> {
@@ -21,8 +26,6 @@ export class Cli<Router extends TypeRouter<any>> {
   public readonly caller: TypeRouterCaller<Router>;
   protected readonly writer: Writer;
   protected readonly codecs: Codecs;
-  protected readonly stdout: WriteStream;
-  protected readonly stderr: WriteStream;
 
   public constructor(options: CliOptions<Router> = {}) {
     const router = this.router = options.router ?? TypeRouter.create() as any;
@@ -31,11 +34,18 @@ export class Cli<Router extends TypeRouter<any>> {
     this.t = this.system.t;
     this.writer = new Writer();
     this.codecs = new Codecs(this.writer);
-    this.stdout = options.stdout ?? process.stdout;
-    this.stderr = options.stderr ?? process.stderr;
   }
 
-  public run(argv: string[] = process.argv.slice(2)): void {
+  public run(options?: RunOptions): void {
+    this.runAsync(options).catch(() => {});
+  }
+
+  public async runAsync(options: RunOptions = {}): Promise<void> {
+    const argv: string[] = options.argv ?? process.argv.slice(2);
+    const stdin = options.stdin ?? process.stdin;
+    const stdout = options.stdout ?? process.stdout;
+    const stderr = options.stderr ?? process.stderr;
+    const input = await this.getStdin(stdin);
     const args = parseArgs({
       args: argv,
       strict: false,
@@ -43,6 +53,7 @@ export class Cli<Router extends TypeRouter<any>> {
     });
     const methodName = args.positionals[0];
     const request = {
+      ...(input ? JSON.parse(input.toString()) : {}),
       ...JSON.parse(args.positionals[1] || '{}'),
       ...args.values,
     };
@@ -51,14 +62,25 @@ export class Cli<Router extends TypeRouter<any>> {
         this.writer.reset();
         value.encode(this.codecs.json);
         const buf = this.writer.flush();
-        this.stdout.write(buf);
+        stdout.write(buf);
       })
       .catch((err) => {
         const value = err as Value;
         this.writer.reset();
         value.encode(this.codecs.json);
         const buf = this.writer.flush();
-        this.stderr.write(buf);
+        stderr.write(buf);
       });
   }
+
+  private async getStdin(stdin: ReadStream): Promise<Buffer> {
+    if (stdin.isTTY) return Buffer.alloc(0);
+    const result = [];
+    let length = 0;
+    for await (const chunk of stdin) {
+      result.push(chunk);
+      length += chunk.length;
+    }
+    return Buffer.concat(result, length);
+  };
 }
