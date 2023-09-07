50a4a4eb69144f954a4514b1e3f41d297488a2e9
streamich <streamich@gmail.com>
2023-09-07 10:25:22 +0200

    feat: 🎸 run Prettier

--- src/json-cli/routes/util.ts
+++ src/json-cli/routes/util.ts
@@ -3,7 +3,8 @@ import {RoutesBase, TypeRouter} from '../../json-type/system/TypeRouter';
 
 export const defineUtilRoutes = <Routes extends RoutesBase>(router: TypeRouter<Routes>) => {
   const router2 = router.extend(({t}) => ({
-    'util.echo': t.Function(t.any, t.any)
+    'util.echo': t
+      .Function(t.any, t.any)
       .options({
         title: 'Echo input',
         description: 'Echo the input value back to the caller',
@@ -11,20 +12,15 @@ export const defineUtilRoutes = <Routes extends RoutesBase>(router: TypeRouter<R
       .implement(async (req) => {
         return req;
       }),
-    'util.time': t.Function(t.undef, t.num)
+    'util.time': t
+      .Function(t.undef, t.num)
       .options({
         title: 'Get time',
         description: 'Returns the current time',
       })
       .implement(async () => Date.now()),
-    'util.schema': t.Function(
-      t.Object(
-        t.prop('alias', t.str),
-      ),
-      t.Object(
-        t.prop('schema', t.any),
-      ),
-    )
+    'util.schema': t
+      .Function(t.Object(t.prop('alias', t.str)), t.Object(t.prop('schema', t.any)))
       .options({
         title: 'Get schema',
         description: 'Returns the schema definition of a type',
