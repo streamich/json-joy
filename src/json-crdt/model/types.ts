50d200c0e8682e422fe2fa115233ada304429e65
streamich <streamich@gmail.com>
2023-09-18 20:13:40 +0200

    feat(json-crdt): 🎸 add view to proxy node conversion types

--- src/json-crdt/model/types.ts
+++ src/json-crdt/model/types.ts
@@ -13,10 +13,9 @@ export type ProxyNodeVector<Elements extends ProxyNode<any, any>[]> = ProxyNode<
 export type ProxyNodeObject<Dictionary extends Record<string, ProxyNode<any, any>>> = ProxyNode<ViewOfProxyNode<Dictionary>, nodes.ObjectApi<ViewOfProxyNode<Dictionary>>> & Dictionary;
 export type ProxyNodeArray<Elements extends ProxyNode<any, any>[]> = ProxyNode<ViewOfProxyNode<Elements>, nodes.ArrayApi<ViewOfProxyNode<Elements>>> & Elements;
 
-export type ViewOfProxyNode<N> = N extends ProxyNode<infer V, any>
-  ? V
-  : {[K in keyof N]: ViewOfProxyNode<N[K]>};
+export type ViewOfProxyNode<N> = N extends ProxyNode<infer V, any> ? V : {[K in keyof N]: ViewOfProxyNode<N[K]>};
 
+// prettier-ignore
 export type ViewToProxyNodeDirect<V> = V extends string
   ? ProxyNodeString
   : V extends Uint8Array
@@ -29,4 +28,21 @@ export type ViewToProxyNodeDirect<V> = V extends string
           ? ProxyNodeConst<null>
           : V extends undefined
             ? ProxyNodeConst<undefined>
-            : never;
+            : V extends [infer E]
+              ? ProxyNodeVector<[ViewToProxyNodeDirect<E>]>
+              : V extends (infer E)[]
+                ? ProxyNodeArray<ViewToProxyNodeValueWrapped<E>[]>
+                : V extends Record<string, any>
+                  ? ProxyNodeObject<{[K in keyof V]: ViewToProxyNodeDirect<V[K]>}>
+                  : never;
+
+// prettier-ignore
+export type ViewToProxyNodeValueWrapped<V> = V extends boolean
+  ? ProxyNodeValue<ProxyNodeConst<boolean>>
+  : V extends number
+    ? ProxyNodeValue<ProxyNodeConst<number>>
+    : V extends null
+      ? ProxyNodeValue<ProxyNodeConst<null>>
+      : V extends undefined
+        ? ProxyNodeValue<ProxyNodeConst<undefined>>
+        : ViewToProxyNodeDirect<V>;
