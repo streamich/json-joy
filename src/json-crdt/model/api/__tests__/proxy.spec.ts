a509227465e6c7bf75ad16d064e6be5fb38ebe5b
Vadim Dalecky <streamich@gmail.com>
2023-09-25 22:58:17 +0200

    test(json-crdt): 💍 remove unused test case

--- src/json-crdt/model/api/__tests__/proxy.spec.ts
+++
@@ -1,8 +0,0 @@
-import {Model} from "../../Model";
-
-test('...', () => {
-  const model = Model.withLogicalClock();
-  model.api.root({a: 123});
-  // console.log(model + '');
-  console.log(model.find.val.a.toView() + '');
-});
