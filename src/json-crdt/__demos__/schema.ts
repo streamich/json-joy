a2f589cce9ddbd8768d6525128f97ce0dc4ef481
streamich <streamich@gmail.com>
2023-11-02 00:51:57 +0100

    docs(json-crdt): ✏️ update scheam example

--- src/json-crdt/__demos__/schema.ts
+++ src/json-crdt/__demos__/schema.ts
@@ -7,32 +7,54 @@
  */
 
 import {Model} from '..';
-import {nodes, s} from '../../json-crdt-patch/builder/schema';
+import {s} from '../../json-crdt-patch';
 
-const schema = s
-  .obj(
-    {
-      id: s.con(''),
-      counter: s.con(0),
-      bool: s.con(true),
-      nil: s.con(null),
-      undef: s.con(undefined),
-      arr: s.arr([s.val(s.con(1)), s.val(s.con(2)), s.val(s.con(3))]),
-      polymorphicArr: s.arr([s.con(1), s.val(s.con('asdf')), s.str('hello')]),
-      vec: s.vec(s.con(123), s.con('<div>')),
-      obj: s.obj({
-        num: s.con(123),
-        str: s.str('hello'),
-      }),
-    },
-    {
-      verified: s.con(false),
-    },
-  )
-  .optional<{
-    num: nodes.con<number>;
-  }>();
+const schema = s.obj({
+  text: s.con('hello'),
+  counter: s.con(0),
+  checked: s.con(true),
+  friend: s.obj({
+    name: s.con('John'),
+    age: s.con(42),
+    tags: s.arr([s.con('foo'), s.con('bar')]),
+  }),
+});
 
 const model = Model.withLogicalClock(1234).setSchema(schema);
 
 console.log(model + '');
+// Model
+// ├─ RootLww "val" 0.0
+// │  └─ ObjectLww "obj" 1234.1
+// │     ├─ "text"
+// │     │   └─ Const "con" 1234.2 { "hello" }
+// │     ├─ "counter"
+// │     │   └─ Const "con" 1234.3 { 0 }
+// │     ├─ "checked"
+// │     │   └─ Const "con" 1234.4 { true }
+// │     └─ "friend"
+// │         └─ ObjectLww "obj" 1234.5
+// │            ├─ "name"
+// │            │   └─ Const "con" 1234.6 { "John" }
+// │            ├─ "age"
+// │            │   └─ Const "con" 1234.7 { 42 }
+// │            └─ "tags"
+// │                └─ ArrayRga "arr" 1234.8
+// │                   └─ ArrayChunk 1234.11!2 len:2
+// │                      ├─ [0]: Const "con" 1234.9 { "foo" }
+// │                      └─ [1]: Const "con" 1234.10 { "bar" }
+// │
+// └─ VectorClock 1234.16
+
+
+console.log(model.view().friend.name);
+// "John"
+console.log(model.view().friend.tags[1]);
+// "bar"
+
+
+console.log(model.api.node.get('text').view());
+// "hello"
+console.log(model.api.node.get('friend').get('name').view());
+// "John"
+
