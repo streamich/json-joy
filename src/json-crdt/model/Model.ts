a6e83c807b52f0ff5ddd3477acd3ee2e9a1e39fb
streamich <streamich@gmail.com>
2023-11-05 14:56:23 +0100

    style(json-crdt): 💄 run Prettier

--- src/json-crdt/model/Model.ts
+++ src/json-crdt/model/Model.ts
@@ -344,12 +344,18 @@ export class Model<RootJsonNode extends JsonNode = JsonNode> implements Printabl
         nl,
         (tab) => {
           const nodes: JsonNode[] = [];
-          this.index.forEach(item => nodes.push(item.v));
-          return `Index (${nodes.length} nodes)` + printTree(tab, nodes.map(node => (tab) => `${node.constructor.name} ${toDisplayString(node.id)}`));
+          this.index.forEach((item) => nodes.push(item.v));
+          return (
+            `Index (${nodes.length} nodes)` +
+            printTree(
+              tab,
+              nodes.map((node) => (tab) => `${node.constructor.name} ${toDisplayString(node.id)}`),
+            )
+          );
         },
         nl,
         // (tab) => `View ${toTree(this.view(), tab)}`,
-        (tab) => `View${printTree(tab, [tab => JSON.stringify(this.view(), null, 2).replace(/\n/g, '\n' + tab)])}`,
+        (tab) => `View${printTree(tab, [(tab) => JSON.stringify(this.view(), null, 2).replace(/\n/g, '\n' + tab)])}`,
         nl,
         (tab) => this.clock.toString(tab),
         hasExtensions ? nl : null,
