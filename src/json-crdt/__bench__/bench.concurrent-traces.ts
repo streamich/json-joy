99e005a39502fcc234aaaaf0011102c2d980af85
streamich <streamich@gmail.com>
2023-11-10 09:31:59 +0100

    feat(json-crdt): 🎸 load concurrent editing traces from json-crdt-traces repo

--- src/json-crdt/__bench__/bench.concurrent-traces.ts
+++ src/json-crdt/__bench__/bench.concurrent-traces.ts
@@ -1,13 +1,12 @@
 /* tslint:disable no-console */
 // npx ts-node src/json-crdt/__bench__/bench.concurrent-traces.ts
 
-import {convertConcurrentTraceToPatches, loadConcurrentTrace} from './util/concurrent-trace-patch-builder';
+import {loadConcurrentTrace} from './util/concurrent-trace';
 import {Patch} from '../../json-crdt-patch';
 import {Model} from '../model';
 
 const traceName = 'friendsforever';
-const json = loadConcurrentTrace(traceName);
-const batch = convertConcurrentTraceToPatches(json);
+const [batch, view] = loadConcurrentTrace(traceName);
 
 const timeBatchRun = (batch: Patch[]) => {
   const model = Model.withLogicalClock(123123123);
@@ -25,7 +24,7 @@ const timeBatchRun = (batch: Patch[]) => {
 
 const timeAndReportBatchRun = (batch: Patch[]) => {
   const res = timeBatchRun(batch);
-  console.log('Time:', res.ms.toFixed(2) + 'ms', ',', 'Correct:', res.model.view() === json.endContent ? '✅' : '❌');
+  console.log('Time:', res.ms.toFixed(2) + 'ms', ',', 'Correct:', res.model.view() === view ? '✅' : '❌');
 };
 
 for (let i = 0; i < 50; i++) {
--- src/json-crdt/__tests__/editing-traces.spec.ts
+++ src/json-crdt/__tests__/editing-traces.spec.ts
@@ -1,8 +1,8 @@
 import {runTrace} from '../__bench__/util/execute';
 import {sequentialTraceNames, traces} from '../__bench__/util/traces';
 import {editors} from '../__bench__/util/editors';
-import {convertConcurrentTraceToPatches, loadConcurrentTrace} from '../__bench__/util/concurrent-trace-patch-builder';
 import {Model} from '../model';
+import {loadConcurrentTrace} from '../__bench__/util/concurrent-trace';
 
 describe('sequential traces', () => {
   const editor = editors['json-joy'];
@@ -19,11 +19,10 @@ describe('concurrent traces', () => {
   const traces: string[] = ['friendsforever'];
   for (const traceName of traces) {
     test(`"${traceName}" trace`, async () => {
-      const json = loadConcurrentTrace(traceName);
-      const batch = convertConcurrentTraceToPatches(json);
+      const [batch, view] = loadConcurrentTrace(traceName);
       const model = Model.withLogicalClock(123123123);
       model.applyBatch(batch);
-      expect(model.view()).toBe(json.endContent);
+      expect(model.view()).toBe(view);
     });
   }
 });
