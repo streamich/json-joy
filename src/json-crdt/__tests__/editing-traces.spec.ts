6b55b3a310d86fbd394ebac8499ef449620ef823
streamich <streamich@gmail.com>
2023-11-14 12:04:30 +0100

    test(json-crdt): 💍 run latest editing traces and run fuzzer traces

--- src/json-crdt/__tests__/editing-traces.spec.ts
+++ src/json-crdt/__tests__/editing-traces.spec.ts
@@ -3,6 +3,7 @@ import {sequentialTraceNames, traces} from '../__bench__/util/traces';
 import {editors} from '../__bench__/util/editors';
 import {Model} from '../model';
 import {loadConcurrentTrace} from '../__bench__/util/concurrent-trace';
+import {loadFuzzerTrace} from '../__bench__/util/fuzzer-traces';
 
 describe('sequential traces', () => {
   const editor = editors['json-joy'];
@@ -15,7 +16,7 @@ describe('sequential traces', () => {
   }
 });
 
-describe.skip('concurrent traces', () => {
+describe('concurrent traces', () => {
   const traces: string[] = ['friendsforever'];
   for (const traceName of traces) {
     test(`"${traceName}" trace`, async () => {
@@ -23,6 +24,30 @@ describe.skip('concurrent traces', () => {
       const model = Model.withLogicalClock(123123123);
       model.applyBatch(batch);
       expect(model.view()).toBe(view);
+    });
+  }
+});
+
+describe('fuzzer traces', () => {
+  const traces = [
+    <const>'trace-1',
+    <const>'trace-2',
+    <const>'trace-3',
+    <const>'long',
+    <const>'short',
+    <const>'low-concurrency',
+    <const>'high-concurrency',
+    <const>'str-only',
+    <const>'bin-only',
+  ];
+
+  for (const traceName of traces) {
+    test(`"${traceName}" trace`, async () => {
+      const [batch, doc] = loadFuzzerTrace(traceName);
+      const model = Model.withLogicalClock(1000000);
+      model.applyBatch(batch);
+      expect(Model.fromBinary(model.toBinary()).toString()).toBe(doc.toString());
+      expect(model.view()).toStrictEqual(doc.view());
     });
   }
 });
