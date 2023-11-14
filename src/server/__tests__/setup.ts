a15b18537a5091877e80a3902c672872a9aa5fda
streamich <streamich@gmail.com>
2023-11-14 22:54:22 +0100

    feat: 🎸 allow to inject services dependency

--- src/server/__tests__/setup.ts
+++ src/server/__tests__/setup.ts
@@ -1,6 +1,8 @@
 import {createCaller} from '../routes';
+import {Services} from '../services/Services';
 
 export const setup = () => {
-  const caller = createCaller();
-  return {caller};
+  const services = new Services();
+  const caller = createCaller(services);
+  return {services, caller};
 };
--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -1,21 +1,19 @@
 import {TypeSystem} from '../../json-type';
 import {TypeRouter} from '../../json-type/system/TypeRouter';
 import {TypeRouterCaller} from '../../reactive-rpc/common/rpc/caller/TypeRouterCaller';
-import {Services} from '../services/Services';
 import {routes} from './routes';
+import type {Services} from '../services/Services';
 import type {RouteDeps} from './types';
 
-export const createRouter = () => {
+export const createRouter = (services: Services) => {
   const system = new TypeSystem();
   const r = new TypeRouter({system, routes: {}});
-  const deps: RouteDeps = {
-    services: new Services(),
-  };
+  const deps: RouteDeps = {services};
   return routes(deps)(r);
 };
 
-export const createCaller = () => {
-  const router = createRouter();
+export const createCaller = (services: Services) => {
+  const router = createRouter(services);
   const caller = new TypeRouterCaller<typeof router>({router});
   return caller;
 };
