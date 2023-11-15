e7b9f4b6109866752c81f6640f22cc956d9ad338
streamich <streamich@gmail.com>
2023-11-15 22:15:44 +0100

    feat: 🎸 log internal errors

--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -1,7 +1,8 @@
+import {routes} from './routes';
 import {TypeSystem} from '../../json-type';
 import {TypeRouter} from '../../json-type/system/TypeRouter';
 import {TypeRouterCaller} from '../../reactive-rpc/common/rpc/caller/TypeRouterCaller';
-import {routes} from './routes';
+import {RpcError} from '../../reactive-rpc/common/rpc/caller';
 import type {Services} from '../services/Services';
 import type {RouteDeps} from './types';
 
@@ -14,6 +15,13 @@ export const createRouter = (services: Services) => {
 
 export const createCaller = (services: Services) => {
   const router = createRouter(services);
-  const caller = new TypeRouterCaller<typeof router>({router});
+  const caller = new TypeRouterCaller<typeof router>({
+    router,
+    wrapInternalError: (error: unknown) => {
+      if (error instanceof RpcError) console.error(error.toJson());
+      else console.error(error);
+      return RpcError.valueFrom(error);
+    },
+  });
   return caller;
 };
