5e119851716b521cdba9f1b9cdea6f5c216122d5
streamich <streamich@gmail.com>
2023-11-18 17:21:12 +0100

    test(reactive-rpc): 💍 move all demo tests to use E2E client implementation

--- src/server/__tests__/presence.spec.ts
+++ src/server/__tests__/presence.spec.ts
@@ -1,15 +1,14 @@
-import {of} from 'rxjs';
 import {setup} from './setup';
 import {tick, until} from '../../__tests__/util';
 
 describe('presence', () => {
   test('can subscribe and receive published presence entries', async () => {
-    const {caller} = setup();
+    const {call, call$} = setup();
     const emits: any[] = [];
-    caller.call$('presence.listen', of({room: 'my-room'}), {}).subscribe((res) => {
-      emits.push(res.data);
+    call$('presence.listen', {room: 'my-room'}).subscribe((res) => {
+      emits.push(res);
     });
-    await caller.call(
+    await call(
       'presence.update',
       {
         room: 'my-room',
@@ -18,7 +17,6 @@ describe('presence', () => {
           hello: 'world',
         },
       },
-      {},
     );
     await until(() => emits.length === 1);
     expect(emits[0]).toMatchObject({
@@ -37,12 +35,12 @@ describe('presence', () => {
   });
 
   test('can receive an existing record when subscribing after it was created', async () => {
-    const {caller} = setup();
+    const {call, call$} = setup();
     const emits: any[] = [];
-    caller.call$('presence.listen', of({room: 'my-room'}), {}).subscribe((res) => {
-      emits.push(res.data);
+    call$('presence.listen', {room: 'my-room'}).subscribe((res) => {
+      emits.push(res);
     });
-    await caller.call(
+    await call(
       'presence.update',
       {
         room: 'my-room',
@@ -51,12 +49,11 @@ describe('presence', () => {
           hello: 'world',
         },
       },
-      {},
     );
     await until(() => emits.length === 1);
     const emits2: any[] = [];
-    caller.call$('presence.listen', of({room: 'my-room'}), {}).subscribe((res) => {
-      emits2.push(res.data);
+    call$('presence.listen', {room: 'my-room'}).subscribe((res) => {
+      emits2.push(res);
     });
     await until(() => emits2.length === 1);
     expect(emits2[0]).toMatchObject({
@@ -75,12 +72,12 @@ describe('presence', () => {
   });
 
   test('can remove existing entries', async () => {
-    const {caller} = setup();
+    const {call, call$} = setup();
     const emits: any[] = [];
-    caller.call$('presence.listen', of({room: 'my-room'}), {}).subscribe((res) => {
-      emits.push(res.data);
+    call$('presence.listen', {room: 'my-room'}).subscribe((res) => {
+      emits.push(res);
     });
-    await caller.call(
+    await call(
       'presence.update',
       {
         room: 'my-room',
@@ -89,22 +86,21 @@ describe('presence', () => {
           hello: 'world',
         },
       },
-      {},
     );
     await until(() => emits.length === 1);
-    await caller.call('presence.remove', {room: 'my-room', id: 'user-1'}, {});
+    await call('presence.remove', {room: 'my-room', id: 'user-1'});
     await until(() => emits.length === 2);
     const emits2: any[] = [];
-    caller.call$('presence.listen', of({room: 'my-room'}), {}).subscribe((res) => {
-      emits2.push(res.data);
+    call$('presence.listen', {room: 'my-room'}).subscribe((res) => {
+      emits2.push(res);
     });
     await tick(50);
     expect(emits2.length).toBe(0);
   });
 
   test('emits entry deletion messages', async () => {
-    const {caller} = setup();
-    await caller.call(
+    const {call, call$} = setup();
+    await call(
       'presence.update',
       {
         room: 'my-room',
@@ -113,13 +109,12 @@ describe('presence', () => {
           hello: 'world',
         },
       },
-      {},
     );
     const emits: any[] = [];
-    caller.call$('presence.listen', of({room: 'my-room'}), {}).subscribe((res) => {
-      emits.push(res.data);
+    call$('presence.listen', {room: 'my-room'}).subscribe((res) => {
+      emits.push(res);
     });
-    await caller.call('presence.remove', {room: 'my-room', id: 'user-1'}, {});
+    await call('presence.remove', {room: 'my-room', id: 'user-1'});
     await until(() => emits.length === 2);
     expect(emits[1].entries[0]).toMatchObject({
       id: 'user-1',
