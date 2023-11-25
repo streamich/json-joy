a220c2ad14628b4014309140fb8ce88435e95659
streamich <streamich@gmail.com>
2023-11-25 16:29:44 +0100

    test(json-crdt): 💍 check events stop flowing for deleted nodes

---
+++ src/json-crdt/model/api/__tests__/NodeEvents.spec.ts
@@ -0,0 +1,25 @@
+import {Model} from "../..";
+
+test('does not fire events after node is deleted', () => {
+  const model = Model.withLogicalClock();
+  model.api.root({
+    foo: {
+      bar: {
+        baz: 'asdf',
+      },
+    },
+  });
+  const bar = model.api.obj(['foo', 'bar']);
+  let cnt = 0;
+  bar.events.changes.listen(() => {
+    cnt++;
+  });
+  expect(cnt).toBe(0);
+  bar.set({
+    gg: 'wp',
+  });
+  expect(cnt).toBe(1);
+  model.api.obj(['foo']).del(['bar']);
+  model.api.obj(['foo']).set({'gl': 'hf'});
+  expect(cnt).toBe(1);
+});
