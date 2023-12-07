7162014cd61e723bbfcaa43cac88bc79fd98b56c
streamich <streamich@gmail.com>
2023-12-07 10:54:55 +0100

    refactor(json-type): 💡 move StringType to its own file

--- src/json-type/type/classes.ts
+++ src/json-type/type/classes.ts
@@ -29,6 +29,7 @@ import {AnyType} from './classes/AnyType';
 import {ConstType} from './classes/ConstType';
 import {BooleanType} from './classes/BooleanType';
 import {NumberType} from './classes/NumberType';
+import {StringType} from './classes/StringType';
 import type * as jsonSchema from '../../json-schema';
 import type {SchemaOf, SchemaOfObjectFields, Type} from './types';
 import type {TypeSystem} from '../system/TypeSystem';
@@ -57,106 +58,8 @@ export {
   ConstType,
   BooleanType,
   NumberType,
+  StringType,
 };
-
-export class StringType extends AbstractType<schema.StringSchema> {
-  constructor(protected schema: schema.StringSchema) {
-    super();
-  }
-
-  public toJsonSchema(ctx?: TypeExportContext): jsonSchema.JsonSchemaString {
-    const schema = this.getSchema();
-    const jsonSchema = <jsonSchema.JsonSchemaString>{
-      type: 'string',
-      ...super.toJsonSchema(ctx),
-    };
-    if (schema.min !== undefined) jsonSchema.minLength = schema.min;
-    if (schema.max !== undefined) jsonSchema.maxLength = schema.max;
-    return jsonSchema;
-  }
-
-  public validateSchema(): void {
-    const schema = this.getSchema();
-    validateTType(schema, 'str');
-    validateWithValidator(schema);
-    const {min, max, ascii, noJsonEscape} = schema;
-    validateMinMax(min, max);
-    if (ascii !== undefined) {
-      if (typeof ascii !== 'boolean') throw new Error('ASCII');
-    }
-    if (noJsonEscape !== undefined) {
-      if (typeof noJsonEscape !== 'boolean') throw new Error('NO_JSON_ESCAPE_TYPE');
-    }
-  }
-
-  public codegenValidator(ctx: ValidatorCodegenContext, path: ValidationPath, r: string): void {
-    const error = ctx.err(ValidationError.STR, path);
-    ctx.js(/* js */ `if(typeof ${r} !== "string") return ${error};`);
-    const {min, max} = this.schema;
-    if (typeof min === 'number' && min === max) {
-      const err = ctx.err(ValidationError.STR_LEN, path);
-      ctx.js(/* js */ `if(${r}.length !== ${min}) return ${err};`);
-    } else {
-      if (typeof min === 'number') {
-        const err = ctx.err(ValidationError.STR_LEN, path);
-        ctx.js(/* js */ `if(${r}.length < ${min}) return ${err};`);
-      }
-      if (typeof max === 'number') {
-        const err = ctx.err(ValidationError.STR_LEN, path);
-        ctx.js(/* js */ `if(${r}.length > ${max}) return ${err};`);
-      }
-    }
-    ctx.emitCustomValidators(this, path, r);
-  }
-
-  public codegenJsonTextEncoder(ctx: JsonTextEncoderCodegenContext, value: JsExpression): void {
-    if (this.schema.noJsonEscape) {
-      ctx.writeText('"');
-      ctx.js(/* js */ `s += ${value.use()};`);
-      ctx.writeText('"');
-    } else ctx.js(/* js */ `s += asString(${value.use()});`);
-  }
-
-  private codegenBinaryEncoder(ctx: BinaryEncoderCodegenContext<BinaryJsonEncoder>, value: JsExpression): void {
-    const ascii = this.schema.ascii;
-    const v = value.use();
-    if (ascii) ctx.js(/* js */ `encoder.writeAsciiStr(${v});`);
-    else ctx.js(/* js */ `encoder.writeStr(${v});`);
-  }
-
-  public codegenCborEncoder(ctx: CborEncoderCodegenContext, value: JsExpression): void {
-    this.codegenBinaryEncoder(ctx, value);
-  }
-
-  public codegenMessagePackEncoder(ctx: MessagePackEncoderCodegenContext, value: JsExpression): void {
-    this.codegenBinaryEncoder(ctx, value);
-  }
-
-  public codegenJsonEncoder(ctx: JsonEncoderCodegenContext, value: JsExpression): void {
-    this.codegenBinaryEncoder(ctx, value);
-  }
-
-  public codegenCapacityEstimator(ctx: CapacityEstimatorCodegenContext, value: JsExpression): void {
-    ctx.inc(MaxEncodingOverhead.String);
-    ctx.codegen.js(`size += ${MaxEncodingOverhead.StringLengthMultiplier} * ${value.use()}.length;`);
-  }
-
-  public random(): string {
-    let length = Math.round(Math.random() * 10);
-    const {min, max} = this.schema;
-    if (min !== undefined && length < min) length = min + length;
-    if (max !== undefined && length > max) length = max;
-    return RandomJson.genString(length);
-  }
-
-  public toTypeScriptAst(): ts.TsStringKeyword {
-    return {node: 'StringKeyword'};
-  }
-
-  public toJson(value: unknown, system: TypeSystem | undefined = this.system): json_string<unknown> {
-    return <json_string<string>>(this.schema.noJsonEscape ? '"' + value + '"' : asString(value as string));
-  }
-}
 
 export class BinaryType<T extends Type> extends AbstractType<schema.BinarySchema> {
   protected schema: schema.BinarySchema;
