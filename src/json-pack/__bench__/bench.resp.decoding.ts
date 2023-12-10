1abbcab6d2e137e0b73f28a3184232624458a0ae
streamich <streamich@gmail.com>
2023-12-10 21:50:35 +0100

    perf(json-pack): ⚡️ add streaming decoder to benchmarks

--- src/json-pack/__bench__/bench.resp.decoding.ts
+++ src/json-pack/__bench__/bench.resp.decoding.ts
@@ -3,6 +3,7 @@
 import {runBenchmark, IBenchmark} from '../../__bench__/runBenchmark';
 import {RespEncoder} from '../resp/RespEncoder';
 import {RespDecoder} from '../resp/RespDecoder';
+import {RespStreamingDecoder} from '../resp/RespStreamingDecoder';
 
 const encoder = new RespEncoder();
 const data = encoder.encode(['set', 'production:project-name:keys:foobarbaz', 'PX', 'NX', 'EX', 60000, 'KEEPTTL']);
@@ -18,11 +19,21 @@ const benchmark: IBenchmark = {
   ],
   runners: [
     {
-      name: 'json-joy/json-pack RespDecoder.decode()',
+      name: 'json-joy/json-pack RespDecoder',
       setup: () => {
         const decoder = new RespDecoder();
         return (data: any) => {
           decoder.read(data);
+        }
+      },
+    },
+    {
+      name: 'json-joy/json-pack RespStreamingDecoder',
+      setup: () => {
+        const decoder = new RespStreamingDecoder();
+        return (data: any) => {
+          decoder.push(data);
+          decoder.read();
         }
       },
     },
