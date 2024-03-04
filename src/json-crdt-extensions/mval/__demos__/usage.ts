0c600f94a806e65736e8cc96eca23c4f9d4753a9
streamich <streamich@gmail.com>
2024-03-04 15:33:37 +0100

    docs(json-crdt-extensions): ✏️ add MV-Register demo

---
+++ src/json-crdt-extensions/mval/__demos__/usage.ts
@@ -0,0 +1,44 @@
+/* tslint:disable no-console */
+
+/**
+ * Run this demo with:
+ *
+ *     npx nodemon -q -x ts-node src/json-crdt-extensions/mval/__demos__/usage.ts
+ */
+
+import {Model} from '../../../json-crdt';
+import {ValueMvExt} from '..';
+
+console.clear();
+
+const model = Model.withLogicalClock(1234);
+
+model.ext.register(ValueMvExt)
+
+model.api.root({
+  mv: ValueMvExt.new(1),
+});
+
+console.log('');
+console.log('Initial value:');
+console.log(model + '');
+
+const api = model.api.in('mv').asExt(ValueMvExt);
+
+api.set(5);
+
+console.log('');
+console.log('After update:');
+console.log(model + '');
+
+const model2 = model.fork();
+
+const api2 = model2.api.in('mv').asExt(ValueMvExt);
+
+api.set(10);
+api2.set(20);
+model.applyPatch(model2.api.flush());
+
+console.log('');
+console.log('After two users update concurrently:');
+console.log(model + '');
