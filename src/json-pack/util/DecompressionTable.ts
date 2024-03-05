748b4fae3c4243d691165c9cafcabdcea6afab9e
streamich <streamich@gmail.com>
2024-03-05 09:59:02 +0100

    feat(json-pack): 🎸 implement DecompressionTable

---
+++ src/json-pack/util/DecompressionTable.ts
@@ -0,0 +1,35 @@
+const isSafeInteger = Number.isSafeInteger;
+
+export class DecompressionTable {
+  protected readonly table: unknown[] = [];
+  
+  public importTable(rleTable: unknown[]) {
+    const length = rleTable.length;
+    if (!length) return;
+    const table = this.table;
+    const first = rleTable[0];
+    table.push(first);
+    let i = 1;
+    if (isSafeInteger(first)) {
+      let prev: number = <number>first;
+      let value: unknown;
+      while (i < length) {
+        value = rleTable[i];
+        if (isSafeInteger(value)) {
+          prev = prev + <number>value;
+          table.push(prev);
+          i++;
+        } else {
+          break;
+        }
+      }
+    }
+    while (i < length) table.push(rleTable[i++]);
+  }
+
+  public getLiteral(index: number): unknown {
+    const table = this.table;
+    // if (index < 0 || index >= table.length) throw new Error('OUT_OF_BOUNDS');
+    return table[index];
+  }
+}
