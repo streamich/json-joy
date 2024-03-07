cc72152738652feac4c419b60482122188955161
streamich <streamich@gmail.com>
2024-03-07 08:45:01 +0100

    docs(json-crdt-extensions): ✏️ improve mval demo

--- src/json-crdt-extensions/mval/__demos__/usage.ts
+++ src/json-crdt-extensions/mval/__demos__/usage.ts
@@ -17,7 +17,8 @@ model.ext.register(ValueMvExt);
 
 model.api.root({
   obj: {
-    mv: ValueMvExt.new(s.con(1)),
+    name: s.con('John'),
+    score: ValueMvExt.new(s.con(1)),
   },
 });
 
@@ -25,7 +26,7 @@ console.log('');
 console.log('Initial value:');
 console.log(model + '');
 
-const api = model.api.in(['obj', 'mv']).asExt(ValueMvExt);
+const api = model.api.in(['obj', 'score']).asExt(ValueMvExt);
 
 api.set(s.con(5));
 
@@ -35,7 +36,7 @@ console.log(model + '');
 
 const model2 = model.fork();
 
-const api2 = model2.api.in(['obj', 'mv']).asExt(ValueMvExt);
+const api2 = model2.api.in(['obj', 'score']).asExt(ValueMvExt);
 
 api.set(s.con(10));
 api2.set(s.con(20));
