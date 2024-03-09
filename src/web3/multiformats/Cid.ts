27062c49cdba102bebdeab083218f7ce59b133ea
streamich <streamich@gmail.com>
2024-03-09 08:55:54 +0100

    feat: 🎸 add CID serialization

--- src/web3/multiformats/Cid.ts
+++ src/web3/multiformats/Cid.ts
@@ -1,7 +1,10 @@
 import {decode} from './multibase';
-import {readUvint} from "../util/readUvint";
+import * as vuint from "../util/uvint";
 import {Multihash} from "./Multihash";
 import {Multicodec} from "./constants";
+import * as multibase from "./multibase";
+
+export type CidVersion = 0 | 1;
 
 export class Cid {
   public static fromBinary(buf: Uint8Array): Cid {
@@ -15,9 +18,9 @@ export class Cid {
   }
 
   public static fromBinaryV1(buf: Uint8Array): Cid {
-    const [v1Tag, offset1] = readUvint(buf, 0);
+    const [v1Tag, offset1] = vuint.read(buf, 0);
     if (v1Tag !== Multicodec.CidV1) throw new Error('EXPECTED_CIDV1');
-    const [contentType, offset2] = readUvint(buf, offset1);
+    const [contentType, offset2] = vuint.read(buf, offset1);
     const hash = new Multihash(buf.slice(offset2));
     return new Cid(1, contentType, hash);
   }
@@ -33,8 +36,43 @@ export class Cid {
   }
 
   constructor(
-    public readonly v: 0 | 1,
+    public readonly v: CidVersion,
     public readonly contentType: number,
     public readonly hash: Multihash,
   ) {}
+
+  public toBinary(version: CidVersion = this.v): Uint8Array {
+    if (version === 0) return this.toBinaryV0();
+    return this.toBinaryV1();
+  }
+
+  public toBinaryV0(): Uint8Array {
+    const hashBuf = this.hash.buf;
+    const hashLen = hashBuf.length;
+    const buf = new Uint8Array(2 + hashLen);
+    buf[0] = 0x12;
+    buf[1] = 0x20;
+    buf.set(hashBuf, 2);
+    return buf;
+  }
+
+  public toBinaryV1(): Uint8Array {
+    let size = 2;
+    const contentType = this.contentType;
+    if (contentType > 0b1111111) size += 1;
+    if (contentType > 0b1111111_1111111) size += 1;
+    const hash = this.hash;
+    size += hash.length();
+    const buf = new Uint8Array(size);
+    buf[0] = Multicodec.CidV1;
+    const offset = vuint.write(buf, 1, contentType);
+    hash.buf.set(buf, offset);
+    return buf;
+  }
+
+  public toText(format: multibase.BaseNameOrCode = 'base64url', version?: CidVersion): string {
+    const buf = multibase.encode(format, this.toBinary(version));
+    const str = String.fromCharCode(...buf);
+    return str;
+  }
 }
--- src/web3/multiformats/multibase.ts
+++ src/web3/multiformats/multibase.ts
@@ -1,29 +1,29 @@
-import {encode, decode} from 'multibase';
+import {encode, decode, BaseNameOrCode} from 'multibase';
 
-export {encode, decode};
+export {encode, decode, BaseNameOrCode};
 
-export type MultibaseFormat =
-  | 'base2'
-  | 'base8'
-  | 'base10'
-  | 'base16'
-  | 'base16upper'
-  | 'base32hex'
-  | 'base32hexupper'
-  | 'base32hexpad'
-  | 'base32hexpadupper'
-  | 'base32'
-  | 'base32upper'
-  | 'base32pad'
-  | 'base32padupper'
-  | 'base32z'
-  | 'base36'
-  | 'base36upper'
-  | 'base58btc'
-  | 'base58flickr'
-  | 'base64'
-  | 'base64pad'
-  | 'base64url'
-  | 'base64urlpad'
-  | 'proquint'
-  | 'base256emoji';
+// export type MultibaseFormat =
+//   | 'base2'
+//   | 'base8'
+//   | 'base10'
+//   | 'base16'
+//   | 'base16upper'
+//   | 'base32hex'
+//   | 'base32hexupper'
+//   | 'base32hexpad'
+//   | 'base32hexpadupper'
+//   | 'base32'
+//   | 'base32upper'
+//   | 'base32pad'
+//   | 'base32padupper'
+//   | 'base32z'
+//   | 'base36'
+//   | 'base36upper'
+//   | 'base58btc'
+//   | 'base58flickr'
+//   | 'base64'
+//   | 'base64pad'
+//   | 'base64url'
+//   | 'base64urlpad'
+//   | 'proquint'
+//   | 'base256emoji';
