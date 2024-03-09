f7d6c6bb6a10174e8cb010c59ca5b486b893a93c
streamich <streamich@gmail.com>
2024-03-09 12:51:47 +0100

    feat: 🎸 implement HLC defaults

--- src/web3/hlc/HlcFactory.ts
+++ src/web3/hlc/HlcFactory.ts
@@ -2,19 +2,21 @@ import {create, inc} from './util';
 import {Hlc} from './Hlc';
 
 export interface HlcFactoryDependencies {
-  /** Latest wall clock generator. */
-  now: () => number;
+  /** Wall clock generator. */
+  now?: () => number;
 
   /** ID of the current node/process. */
-  node: number;
+  node?: number;
 }
+
+const defaultNow = () => Math.round(Date.now() / 1000) - 2272147200;
 
 export class HlcFactory {
   public id: Hlc;
   public readonly now: () => number;
   public readonly node: number;
 
-  constructor({now, node}: HlcFactoryDependencies) {
+  constructor({now = defaultNow, node = Math.round(Math.random() * 0xff)}: HlcFactoryDependencies) {
     this.now = now;
     this.node = node;
     this.id = new Hlc(now(), 0, node);
