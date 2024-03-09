d3be04ea2b605e0045792a0fc6d0f6aaf06935e3
streamich <streamich@gmail.com>
2024-03-09 18:18:03 +0100

    style: 💄 run Prettier

--- src/web3/store/cas/CidCasMemory.ts
+++ src/web3/store/cas/CidCasMemory.ts
@@ -7,7 +7,7 @@ export class CidCasMemory implements CidCas {
 
   public async get(cid: Cid): Promise<Uint8Array> {
     const key = cid.toText();
-    await new Promise(resolve => setTimeout(resolve, 1));
+    await new Promise((resolve) => setTimeout(resolve, 1));
     const value = this.store.get(key);
     if (!value) throw new Error(`No value for CID: ${key}`);
     return value;
@@ -15,19 +15,19 @@ export class CidCasMemory implements CidCas {
 
   public async has(cid: Cid): Promise<boolean> {
     const key = cid.toText();
-    await new Promise(resolve => setTimeout(resolve, 1));
+    await new Promise((resolve) => setTimeout(resolve, 1));
     return this.store.has(key);
   }
 
   public async del(cid: Cid): Promise<void> {
     const key = cid.toText();
-    await new Promise(resolve => setTimeout(resolve, 1));
+    await new Promise((resolve) => setTimeout(resolve, 1));
     this.store.delete(key);
   }
 
   public async put(value: Uint8Array, ipldType: MulticodecIpld = MulticodecIpld.Raw): Promise<Cid> {
     const cid = await Cid.fromData(value, ipldType);
-    await new Promise(resolve => setTimeout(resolve, 1));
+    await new Promise((resolve) => setTimeout(resolve, 1));
     const key = cid.toText();
     this.store.set(key, value);
     return cid;
