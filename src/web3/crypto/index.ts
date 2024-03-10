6c6927e1aabbe64f88897d94640318574a444707
streamich <streamich@gmail.com>
2024-03-11 00:15:00 +0100

    feat: 🎸 add sha256() method

--- src/web3/crypto/index.ts
+++ src/web3/crypto/index.ts
@@ -1,8 +1,2 @@
-import {isNode} from '../constants';
-
-/**
- * Universal Node.js/browser Web Crypto API reference.
- *
- * @todo Maybe create an isomorphic package for this?
- */
-export const crypto: Crypto = isNode ? require('node:crypto').webcrypto : window.crypto;
+export * from './webcrypto';
+export * from './sha256';
