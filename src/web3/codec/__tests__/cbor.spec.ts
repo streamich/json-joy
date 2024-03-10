1f0bee84dd73829be0a1b5911334714026a55dc0
streamich <streamich@gmail.com>
2024-03-10 23:42:57 +0100

    test: 💍 add CBOR code fixture test

--- src/web3/codec/__tests__/cbor.spec.ts
+++ src/web3/codec/__tests__/cbor.spec.ts
@@ -8,3 +8,15 @@ test('can encode and decode CID', async () => {
   const decoded = cbor.decoder.decode(encoded);
   expect(decoded).toStrictEqual(data);
 });
+
+test('can encode simplest fixture', async () => {
+  const data = [2];
+  const encoded = cbor.encoder.encode(data);
+  const decoded = cbor.decoder.decode(encoded);
+  expect(decoded).toStrictEqual(data);
+  expect(encoded.length).toBe(2);
+  expect(encoded[0]).toBe(0x81);
+  expect(encoded[1]).toBe(0x02);
+  const cid = await Cid.fromDagCbor(encoded);
+  expect(cid.toText('base32')).toBe('bafyreihdb57fdysx5h35urvxz64ros7zvywshber7id6t6c6fek37jgyfe');
+});
