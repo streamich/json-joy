a697deb809128ff3d9d7daac8a1345c7547cc9f4
streamich <streamich@gmail.com>
2024-03-11 17:18:12 +0100

    test(json-pack): 💍 add extra CborEncoderDag test

--- src/json-pack/cbor/__tests__/CborEncoderDag.spec.ts
+++ src/json-pack/cbor/__tests__/CborEncoderDag.spec.ts
@@ -2,6 +2,7 @@ import {Writer} from '../../../util/buffers/Writer';
 import {CborEncoderDag} from '../CborEncoderDag';
 import {CborDecoder} from '../CborDecoder';
 import {JsonPackExtension} from '../../JsonPackExtension';
+import {CborDecoderDag} from '../CborDecoderDag';
 
 const writer = new Writer(1);
 const encoder = new CborEncoderDag(writer);
@@ -50,37 +51,50 @@ describe('only extension = 42 is permitted', () => {
     expect(val).toStrictEqual({a: 'a', b: 'b'});
   });
 
-  test('can encode CID using inlined custom class', () => {
-    class CID {
-      constructor(public readonly value: string) {}
+  class CID {
+    constructor(public readonly value: string) {}
+  }
+  class NotCID {
+    constructor(public readonly value: string) {}
+  }
+
+  class IpfsCborEncoder extends CborEncoderDag {
+    public writeUnknown(val: unknown): void {
+      if (val instanceof CID) this.writeTag(42, val.value);
+      else throw new Error('Unknown value type');
     }
-    const encoder = new (class extends CborEncoderDag {
-      public writeUnknown(val: unknown): void {
-        if (val instanceof CID) encoder.writeTag(42, val.value);
-        else throw new Error('Unknown value type');
-      }
-    })();
+  }
+
+  class IpfsCborDecoder extends CborDecoderDag {
+    public readTagRaw(tag: number): CID | unknown {
+      if (tag === 42) return new CID(this.val() as any);
+      throw new Error('UNKNOWN_TAG');
+    }
+  }
+
+  test('can encode CID using inlined custom class', () => {
+    const encoder = new IpfsCborEncoder();
     const encoded = encoder.encode({a: 'a', b: new JsonPackExtension(42, 'b')});
     const val = decoder.read(encoded);
     expect(val).toStrictEqual({a: 'a', b: new JsonPackExtension(42, 'b')});
     const encoded2 = encoder.encode({a: 'a', b: new CID('b')});
-    const val2 = decoder.read(encoded2);
+    const val2 = decoder.decode(encoded2);
     expect(val).toStrictEqual({a: 'a', b: new JsonPackExtension(42, 'b')});
+    expect(val2).toStrictEqual({a: 'a', b: new JsonPackExtension(42, 'b')});
+  });
+
+  test('can encode CID inside a nested array', () => {
+    const encoder = new IpfsCborEncoder();
+    const decoder = new IpfsCborDecoder();
+    const cid = new CID('my-cid');
+    const data = [1, [2, [3, cid, 4], 5], 6];
+    const encoded = encoder.encode(data);
+    const decoded = decoder.decode(encoded);
+    expect(decoded).toStrictEqual(data);
   });
 
   test('can throw on unknown custom class', () => {
-    class CID {
-      constructor(public readonly value: string) {}
-    }
-    class NotCID {
-      constructor(public readonly value: string) {}
-    }
-    const encoder = new (class extends CborEncoderDag {
-      public writeUnknown(val: unknown): void {
-        if (val instanceof CID) encoder.writeTag(42, val.value);
-        else throw new Error('Unknown value type');
-      }
-    })();
+    const encoder = new IpfsCborEncoder();
     const encoded1 = encoder.encode({a: 'a', b: new CID('b')});
     expect(() => encoder.encode({a: 'a', b: new NotCID('b')})).toThrowError(new Error('Unknown value type'));
   });
