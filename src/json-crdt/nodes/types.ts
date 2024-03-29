0bb20ad74b1fb7f5531f8fb4a98036199d192add
streamich <streamich@gmail.com>
2024-03-29 20:57:13 +0100

    refactor(json-crdt): 💡 rename schema inference utility class

--- src/json-crdt/nodes/types.ts
+++ src/json-crdt/nodes/types.ts
@@ -49,18 +49,18 @@ export interface JsonNode<View = unknown> extends Identifiable {
 export type JsonNodeView<N> = N extends JsonNode<infer V> ? V : {[K in keyof N]: JsonNodeView<N[K]>};
 
 // prettier-ignore
-export type BuilderNodeToJsonNode<S> = S extends builder.str<infer T>
+export type SchemaToJsonNode<S> = S extends builder.str<infer T>
   ? nodes.StrNode<T>
   : S extends builder.bin
     ? nodes.BinNode
     : S extends builder.con<infer T>
       ? nodes.ConNode<T>
       : S extends builder.val<infer T>
-        ? nodes.ValNode<BuilderNodeToJsonNode<T>>
+        ? nodes.ValNode<SchemaToJsonNode<T>>
         : S extends builder.vec<infer T>
-          ? nodes.VecNode<{[K in keyof T]: BuilderNodeToJsonNode<T[K]>}>
+          ? nodes.VecNode<{[K in keyof T]: SchemaToJsonNode<T[K]>}>
           : S extends builder.obj<infer T>
-            ? nodes.ObjNode<{[K in keyof T]: BuilderNodeToJsonNode<T[K]>}>
+            ? nodes.ObjNode<{[K in keyof T]: SchemaToJsonNode<T[K]>}>
             : S extends builder.arr<infer T>
-              ? nodes.ArrNode<BuilderNodeToJsonNode<T>>
+              ? nodes.ArrNode<SchemaToJsonNode<T>>
               : JsonNode;
