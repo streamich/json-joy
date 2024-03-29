1a3ec666ddcdc6292550642b054205a6bf925c9c
streamich <streamich@gmail.com>
2024-03-29 21:35:13 +0100

    feat(json-crdt): 🎸 add type which infers schema from JSON CRDT nodes

--- src/json-crdt/schema/types.ts
+++ src/json-crdt/schema/types.ts
@@ -17,3 +17,20 @@ export type SchemaToJsonNode<S> = S extends builder.str<infer T>
             : S extends builder.arr<infer T>
               ? nodes.ArrNode<SchemaToJsonNode<T>>
               : nodes.JsonNode;
+
+// prettier-ignore
+export type JsonNodeToSchema<N> = N extends nodes.StrNode<infer T>
+  ? builder.str<T>
+  : N extends nodes.BinNode
+    ? builder.bin
+    : N extends nodes.ConNode<infer T>
+      ? builder.con<T>
+      : N extends nodes.ValNode<infer T>
+        ? builder.val<JsonNodeToSchema<T>>
+        : N extends nodes.VecNode<infer T>
+          ? builder.vec<{[K in keyof T]: JsonNodeToSchema<T[K]>}>
+          : N extends nodes.ObjNode<infer T>
+            ? builder.obj<{[K in keyof T]: JsonNodeToSchema<T[K]>}>
+            : N extends nodes.ArrNode<infer T>
+              ? builder.arr<JsonNodeToSchema<T>>
+              : builder.con<undefined>;
