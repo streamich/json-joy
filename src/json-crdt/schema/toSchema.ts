410ce43dc02a671c61097a61bcf048b1a3391ccb
streamich <streamich@gmail.com>
2024-03-29 20:51:52 +0100

    docs(json-crdt): ✏️ add toSchema() JSDoc

--- src/json-crdt/schema/toSchema.ts
+++ src/json-crdt/schema/toSchema.ts
@@ -1,6 +1,16 @@
 import {JsonNode, ConNode, ValNode, ObjNode, VecNode, StrNode, BinNode, ArrNode} from "../nodes";
 import {NodeBuilder, s} from "../../json-crdt-patch";
 
+/**
+ * Converts any JSON CRDT node to a schema representation. The schema can be
+ * used to copy the structure of the JSON CRDT node to another document or
+ * another location in the same document.
+ * 
+ * @todo Add type generic.
+ * 
+ * @param node JSON CRDT node to recursively convert to schema.
+ * @returns Schema representation of the JSON CRDT node.
+ */
 export const toSchema = (node: JsonNode): NodeBuilder => {
   if (node instanceof ConNode) return s.con(node.val);
   if (node instanceof ValNode) return s.val(toSchema(node.node()));
