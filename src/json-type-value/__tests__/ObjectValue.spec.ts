e14655a1c91ad12464b14537de03af20d7d935bc
streamich <streamich@gmail.com>
2024-04-24 10:21:22 +0200

    feat(json-type-value): 🎸 add ability to print ObjectValue

--- src/json-type-value/__tests__/ObjectValue.spec.ts
+++ src/json-type-value/__tests__/ObjectValue.spec.ts
@@ -9,3 +9,10 @@ test('can retrieve field as Value', () => {
   expect(foo.type.getTypeName()).toBe('str');
   expect(foo.data).toBe('bar');
 });
+
+test('can print to string', () => {
+  const system = new TypeSystem();
+  const {t} = system;
+  const obj = new ObjectValue(t.Object(t.prop('foo', t.str)), {foo: 'bar'});
+  expect(obj + '').toMatchSnapshot();
+});
