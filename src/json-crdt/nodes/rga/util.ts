5e7e66120233120d5fc8f9fc0dab3da0d7396299
streamich <streamich@gmail.com>
2024-04-30 10:27:53 +0200

    perf(json-crdt): ⚡️ improve first chunk finding implementation

--- src/json-crdt/nodes/rga/util.ts
+++ src/json-crdt/nodes/rga/util.ts
@@ -3,10 +3,6 @@ import type {AbstractRga, Chunk} from "./AbstractRga";
 /** Find the first visible chunk, if any. */
 export const firstVis = <T>(rga: AbstractRga<T>): Chunk<T> | undefined => {
   let curr = rga.first();
-  if (!curr) return;
-  while (curr.del) {
-    curr = rga.next(curr);
-    if (!curr) return;
-  }
+  while (curr && curr.del) curr = rga.next(curr);
   return curr;
 };
