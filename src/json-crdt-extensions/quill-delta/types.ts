524ae1be8976a94f930ed1df33b2242b1d91cb0d
streamich <streamich@gmail.com>
2024-07-10 11:04:17 +0200

    feat(json-crdt-extensions): 🎸 add Quill types

---
+++ src/json-crdt-extensions/quill-delta/types.ts
@@ -0,0 +1,41 @@
+export interface QuillDeltaPatch {
+  ops: QuillDeltaOp[];
+}
+
+export type QuillDeltaOp = QuillDeltaOpInsert | QuillDeltaOpRetain | QuillDeltaOpDelete;
+
+export type QuillDeltaAttributes = Record<string, unknown | null>;
+
+export interface QuillDeltaOpInsert {
+  insert: string | Record<string, unknown>;
+  attributes?: QuillDeltaAttributes;
+}
+
+export interface QuillDeltaOpRetain {
+  retain: number;
+  attributes?: QuillDeltaAttributes;
+}
+
+export interface QuillDeltaOpDelete {
+  delete: number;
+}
+
+export type QuillDeltaSliceDto = [
+  /** First character session ID, inclusive. */
+  sid1: number,
+  /** First character clock time, inclusive. */
+  time1: number,
+  /** Last character session ID, inclusive. */
+  sid2: number,
+  /** Last character clock time, inclusive. */
+  time2: number,
+  /** Slice markup. */
+  attributes: QuillDeltaAttributes,
+  /** Non-textual insert data. */
+  insert?: Record<string, unknown>,
+];
+
+export interface QuillTrace {
+  contents: QuillDeltaPatch;
+  transactions: QuillDeltaPatch['ops'][];
+}
