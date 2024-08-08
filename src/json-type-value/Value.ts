24d5adb96622b3f146a05a597cd2a61fd84c8ed9
streamich <streamich@gmail.com>
2024-08-08 10:44:37 +0200

    fix(json-type-value): 🐛 show value encoding problemsn in development mode

--- src/json-type-value/Value.ts
+++ src/json-type-value/Value.ts
@@ -6,7 +6,7 @@ export class Value<T extends Type = Type> {
     public type: T,
     public data: ResolveType<T>,
   ) {}
-
+  
   public encode(codec: JsonValueCodec): void {
     const value = this.data;
     const type = this.type;
@@ -15,4 +15,25 @@ export class Value<T extends Type = Type> {
     if (!type) encoder.writeAny(value);
     else type.encoder(codec.format)(value, encoder);
   }
+}
+
+if (process.env.NODE_ENV !== 'production') {
+  const encode = Value.prototype.encode;
+  Value.prototype.encode = function (codec: JsonValueCodec): void {
+    try {
+      encode.call(this, codec);
+    } catch (error) {
+      try {
+        // tslint:disable-next-line no-console
+        console.error(error);
+        const type = this.type;
+        if (type) {
+          const err = type.validator('object')(this.data);
+          // tslint:disable-next-line no-console
+          console.error(err);
+        }
+      } catch {}
+      throw error;
+    }
+  };
 }
