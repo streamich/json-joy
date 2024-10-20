9646c205d7f7fd6c6a61a22f1210f80273828571
Vadim Dalecky <streamich@gmail.com>
2024-10-20 20:15:10 +0200

    feat(json-crdt-peritext-ui): 🎸 improve view of hash

--- src/json-crdt-peritext-ui/renderers/debug/index.tsx
+++ src/json-crdt-peritext-ui/renderers/debug/index.tsx
@@ -22,7 +22,7 @@ export const renderers: RendererMap = {
     return (
       <div style={{position: 'relative'}}>
         <div contentEditable={false} style={{position: 'absolute', top: '-24px', left: 0}}>
-          <span style={{fontSize: '9px', padding: '2px 4px', borderRadius: 3, background: 'rgba(0,0,0)', color: 'white', display: 'inline-block'}}>#{hash}</span>
+          <span style={{fontSize: '9px', padding: '2px 4px', borderRadius: 3, background: 'rgba(0,0,0)', color: 'white', display: 'inline-block'}}>{hash.toString(36)}</span>
         </div>
         <div style={{outline: '1px dotted blue'}}>
           {children}
