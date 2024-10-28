241e8302dc80fac456087c52f4d82078545d71ad
streamich <streamich@gmail.com>
2024-10-28 12:57:03 +0100

    test(json-crdt-peritext-ui): 💍 fix all existing movement tests

--- src/json-crdt-extensions/peritext/editor/__tests__/Editor-selection.spec.ts
+++ src/json-crdt-extensions/peritext/editor/__tests__/Editor-selection.spec.ts
@@ -26,55 +26,6 @@ describe('.selectAll()', () => {
   });
 });
 
-describe('.range()', () => {
-  test('can select a word', () => {
-    const {editor, peritext} = setup((editor) => {
-      editor.insert("const {editor} = setup(editor => editor.insert('Hello world!'));");
-    });
-    peritext.overlay.refresh();
-    const range = editor.range(peritext.pointAt(2), 'word');
-    expect(range?.text()).toBe('const');
-    const range2 = editor.range(peritext.pointAt(9), 'word');
-    expect(range2?.text()).toBe('editor');
-    const range3 = editor.range(peritext.pointAt(20), 'word');
-    expect(range3?.text()).toBe('setup');
-  });
-
-  test('can select punctuation', () => {
-    const {editor, peritext} = setup((editor) => {
-      editor.insert("const {editor} = setup(editor => editor.insert('Hello world!'));");
-    });
-    peritext.refresh();
-    console.log(peritext + '');
-    const point = peritext.pointAt(6);
-    console.log(point + '');
-    const range = editor.range(point, 'word');
-    expect(range?.text()).toBe('{');
-    // const range1 = editor.range(peritext.pointAt(30), 'word');
-    // expect(range1?.text()).toBe('=>');
-  });
-
-  // test('can select punctuation at the end of text', () => {
-  //   const {editor, peritext} = setup((editor) => {
-  //     editor.insert("const {editor} = setup(editor => editor.insert('Hello world!'));");
-  //   });
-  //   peritext.overlay.refresh();
-  //   const range = editor.wordRange(peritext.pointAt(peritext.str.length() - 1));
-  //   expect(range?.text()).toBe("!'));");
-  //   const range2 = editor.wordRange(peritext.pointAt(peritext.str.length() - 3));
-  //   expect(range2?.text()).toBe("!'));");
-  // });
-
-  // test('can select whitespace', () => {
-  //   const {editor, peritext} = setup((editor) => {
-  //     editor.insert("const {editor} = setup(editor => editor.insert('Hello world!'));");
-  //   });
-  //   peritext.overlay.refresh();
-  //   const range = editor.wordRange(peritext.pointAt(5));
-  //   expect(range?.text()).toBe(' ');
-  // });
-});
-
 describe('.rangeWord()', () => {
   test('can select a word', () => {
     const {editor, peritext} = setup((editor) => {
@@ -189,25 +140,74 @@ describe('.setEndpoint()', () => {
   });
 });
 
-describe('.range(..., "block")', () => {
-  test('can select paragraphs', () => {
-    const {editor, peritext} = setup((editor) => {
-      editor.insert('foobarbaz');
+describe('.range()', () => {
+  describe('.range(..., "word")', () => {
+    test('can select a word', () => {
+      const {editor, peritext} = setup((editor) => {
+        editor.insert("const {editor} = setup(editor => editor.insert('Hello world!'));");
+      });
+      peritext.overlay.refresh();
+      const range = editor.range(peritext.pointAt(2), 'word');
+      expect(range?.text()).toBe('const');
+      const range2 = editor.range(peritext.pointAt(9), 'word');
+      expect(range2?.text()).toBe('editor');
+      const range3 = editor.range(peritext.pointAt(20), 'word');
+      expect(range3?.text()).toBe('setup');
     });
-    editor.cursor.setAt(6);
-    editor.saved.insMarker(['p']);
-    editor.cursor.setAt(3);
-    editor.saved.insMarker('p');
-    peritext.refresh();
-    const range1 = editor.range(peritext.pointAt(0), 'block');
-    expect(range1?.text()).toBe('foo');
-    const range2 = editor.range(peritext.pointAt(5), 'block');
-    expect(range2?.text()).toBe('\nbar');
-    const range3 = editor.range(peritext.pointAt(8), 'block');
-    expect(range3?.text()).toBe('\nbaz');
-    const range4 = editor.range(peritext.pointAt(peritext.str.length() - 1), 'block');
-    expect(range4?.text()).toBe('\nbaz');
-    const range5 = editor.range(peritext.pointAt(peritext.str.length() - 1, Anchor.After), 'block');
-    expect(range5?.text()).toBe('\nbaz');
+  
+    test('can select punctuation', () => {
+      const {editor, peritext} = setup((editor) => {
+        editor.insert("const {editor} = setup(editor => editor.insert('Hello world!'));");
+      });
+      peritext.refresh();
+      const point = peritext.pointAt(6);
+      const range = editor.range(point, 'word');
+      expect(range?.text()).toBe('{');
+      const range1 = editor.range(peritext.pointAt(30), 'word');
+      expect(range1?.text()).toBe('=>');
+    });
+  
+    test('can select punctuation at the end of text', () => {
+      const {editor, peritext} = setup((editor) => {
+        editor.insert("const {editor} = setup(editor => editor.insert('Hello world!'));");
+      });
+      peritext.overlay.refresh();
+      const range = editor.range(peritext.pointAt(peritext.str.length() - 1), 'word');
+      expect(range?.text()).toBe("!'));");
+      const range2 = editor.range(peritext.pointAt(peritext.str.length() - 3), 'word');
+      expect(range2?.text()).toBe("!'));");
+    });
+  
+    test('can select whitespace', () => {
+      const {editor, peritext} = setup((editor) => {
+        editor.insert("const {editor} = setup(editor => editor.insert('Hello world!'));");
+      });
+      peritext.overlay.refresh();
+      const range = editor.range(peritext.pointAt(5), 'word');
+      expect(range?.text()).toBe(' ');
+    });
+  });
+
+  describe('.range(..., "block")', () => {
+    test('can select paragraphs', () => {
+      const {editor, peritext} = setup((editor) => {
+        editor.insert('foobarbaz');
+      });
+      editor.cursor.setAt(6);
+      editor.saved.insMarker(['p']);
+      editor.cursor.setAt(3);
+      editor.saved.insMarker('p');
+      peritext.refresh();
+      const range1 = editor.range(peritext.pointAt(0), 'block');
+      expect(range1?.text()).toBe('foo');
+      const range2 = editor.range(peritext.pointAt(5), 'block');
+      expect(range2?.text()).toBe('\nbar');
+      const range3 = editor.range(peritext.pointAt(8), 'block');
+      expect(range3?.text()).toBe('\nbaz');
+      const range4 = editor.range(peritext.pointAt(peritext.str.length() - 1), 'block');
+      expect(range4?.text()).toBe('\nbaz');
+      const range5 = editor.range(peritext.pointAt(peritext.str.length() - 1, Anchor.After), 'block');
+      expect(range5?.text()).toBe('\nbaz');
+    });
   });
 });
