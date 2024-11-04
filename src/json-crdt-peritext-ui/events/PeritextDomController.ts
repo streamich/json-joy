585f5d407f026566e2e503f33bbd12d97f2bf3a4
streamich <streamich@gmail.com>
2024-11-04 12:26:44 +0100

    feat(json-crdt-peritext-ui): 🎸 add CompositionController

--- src/json-crdt-peritext-ui/events/PeritextDomController.ts
+++ src/json-crdt-peritext-ui/events/PeritextDomController.ts
@@ -4,6 +4,7 @@ import {RichTextController} from '../dom/RichTextController';
 import {PeritextEventDefaults} from './PeritextEventDefaults';
 import {PeritextEventTarget} from './PeritextEventTarget';
 import {KeyController} from '../dom/KeyController';
+import {CompositionController} from '../dom/CompositionController';
 import type {UiLifeCycles} from '../dom/types';
 import type {Peritext} from '../../json-crdt-extensions';
 
@@ -15,6 +16,7 @@ export interface PeritextDomControllerOpts {
 export class PeritextDomController implements UiLifeCycles {
   public readonly et: PeritextEventTarget;
   public readonly keys: KeyController;
+  public readonly comp: CompositionController;
   public readonly input: InputController;
   public readonly selection: SelectionController;
   public readonly richText: RichTextController;
@@ -25,7 +27,8 @@ export class PeritextDomController implements UiLifeCycles {
     const defaults = new PeritextEventDefaults(txt, et);
     et.defaults = defaults;
     const keys = (this.keys = new KeyController());
-    this.input = new InputController({et, source, txt});
+    const comp = this.comp = new CompositionController({et, source, txt});
+    this.input = new InputController({et, source, txt, comp});
     this.selection = new SelectionController({et, source, txt, keys});
     this.richText = new RichTextController({et, source, txt});
   }
@@ -34,6 +37,7 @@ export class PeritextDomController implements UiLifeCycles {
 
   public start(): void {
     this.keys.start();
+    this.comp.start();
     this.input.start();
     this.selection.start();
     this.richText.start();
@@ -41,6 +45,7 @@ export class PeritextDomController implements UiLifeCycles {
 
   public stop(): void {
     this.keys.stop();
+    this.comp.stop();
     this.input.stop();
     this.selection.stop();
     this.richText.stop();
