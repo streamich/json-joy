be2e551cb75f87eb02c703195085908c47db95e1
Vadim Dalecky <streamich@gmail.com>
2024-11-04 08:44:17 +0100

    feat(json-crdt-peritext-ui): 🎸 use Position as "inline" event position parameters

---
+++ src/json-crdt-peritext-ui/events/__tests__/inline.spec.ts
@@ -0,0 +1,27 @@
+import {type Kit, runAlphabetKitTestSuite} from '../../../json-crdt-extensions/peritext/__tests__/setup';
+import {PeritextEventDefaults} from '../PeritextEventDefaults';
+import {PeritextEventTarget} from '../PeritextEventTarget';
+
+const testSuite = (getKit: () => Kit) => {
+  const setup = () => {
+    const kit = getKit();
+    const et = new PeritextEventTarget();
+    const defaults = new PeritextEventDefaults(kit.peritext, et);
+    et.defaults = defaults;
+    return {...kit, et};
+  };
+
+  test('can add annotation to the current selection', async () => {
+    const kit = setup();
+    kit.et.cursor({at: 3, len: 3});
+    kit.et.inline({type: 'bold'});
+    kit.peritext.refresh();
+    const slices = kit.peritext.overlay.findOverlapping(kit.peritext.rangeAt(5));
+    console.log([...slices]);
+    console.log(kit.peritext + '');
+  });
+};
+
+describe('"insert" event', () => {
+  runAlphabetKitTestSuite(testSuite);
+});
