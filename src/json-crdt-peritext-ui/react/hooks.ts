254efb7ca04c5ac14b617198522db2c165cc003b
streamich <streamich@gmail.com>
2024-11-04 18:57:29 +0100

    feat(json-crdt-peritext-ui): 🎸 add useSyncStore React hook

--- src/json-crdt-peritext-ui/react/hooks.ts
+++ src/json-crdt-peritext-ui/react/hooks.ts
@@ -1,7 +1,11 @@
 import * as React from 'react';
+import type {SyncStore} from '../../util/events/sync-store';
 
 export const useIsoLayoutEffect =
   typeof window === 'object' && !!window.document ? React.useLayoutEffect : React.useEffect;
 
 export const useBrowserLayoutEffect =
   typeof window === 'object' && !!window.document ? React.useLayoutEffect : () => {};
+
+export const useSyncStore = <T>(store: SyncStore<T>): T =>
+  React.useSyncExternalStore(store.subscribe, store.getSnapshot);
