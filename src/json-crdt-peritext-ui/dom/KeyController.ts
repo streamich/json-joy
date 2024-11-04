57663ab28cf56df230a738d38c139fc66c0c18fc
streamich <streamich@gmail.com>
2024-11-04 12:47:54 +0100

    feat(json-crdt-peritext-ui): 🎸 improve key trackign reset behavior

--- src/json-crdt-peritext-ui/dom/KeyController.ts
+++ src/json-crdt-peritext-ui/dom/KeyController.ts
@@ -13,13 +13,17 @@ export class KeyController implements UiLifeCycles, Printable {
   public start(): void {
     document.addEventListener('keydown', this.onKeyDown);
     document.addEventListener('keyup', this.onKeyUp);
-    document.addEventListener('focus', this.onFocus);
+    document.addEventListener('focus', this.onReset);
+    document.addEventListener('compositionstart', this.onReset);
+    document.addEventListener('compositionend', this.onReset);
   }
 
   public stop(): void {
     document.removeEventListener('keydown', this.onKeyDown);
     document.removeEventListener('keyup', this.onKeyUp);
-    document.removeEventListener('focus', this.onFocus);
+    document.removeEventListener('focus', this.onReset);
+    document.removeEventListener('compositionstart', this.onReset);
+    document.removeEventListener('compositionend', this.onReset);
   }
 
   private onKeyDown = (event: KeyboardEvent): void => {
@@ -32,7 +36,7 @@ export class KeyController implements UiLifeCycles, Printable {
     this.pressed.delete(event.key);
   };
 
-  private onFocus = (): void => {
+  private onReset = (): void => {
     this.pressed.clear();
   };
 
