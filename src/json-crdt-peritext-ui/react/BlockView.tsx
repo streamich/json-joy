b77ddc31ad787827f34882f87f11f0a1061fdb4b
streamich <streamich@gmail.com>
2024-11-09 18:58:31 +0100

    refactor(json-crdt-peritext-ui): 💡 move main code of cursor/selection rendering to block

--- src/json-crdt-peritext-ui/react/BlockView.tsx
+++ src/json-crdt-peritext-ui/react/BlockView.tsx
@@ -4,6 +4,11 @@ import type {Block} from '../../json-crdt-extensions/peritext/block/Block';
 import {InlineView} from './InlineView';
 import {Char} from '../constants';
 import {usePeritext} from './context';
+import {CommonSliceType} from '../../json-crdt-extensions';
+import {CaretView} from './selection/CaretView';
+import {FocusView} from './selection/FocusView';
+import {InlineAttrEnd, InlineAttrStart} from '../../json-crdt-extensions/peritext/block/Inline';
+import {AnchorView} from './selection/AnchorView';
 
 export interface BlockViewProps {
   hash: number;
@@ -18,7 +23,52 @@ export const BlockView: React.FC<BlockViewProps> = React.memo(
 
     const elements: React.ReactNode[] = [];
     if (block instanceof LeafBlock) {
-      for (const inline of block.texts()) elements.push(<InlineView key={inline.key()} inline={inline} />);
+      for (const inline of block.texts()) {
+        const hasCursor = inline.hasCursor();
+        if (hasCursor) {
+          console.log('hasCursor');
+          const elements: React.ReactNode[] = [];
+          const attr = inline.attr();
+          const italic = attr[CommonSliceType.i] && attr[CommonSliceType.i][0];
+          const key = inline.key();
+          const cursorStart = inline.cursorStart();
+          if (cursorStart) {
+            console.log('cursorStart.isCollapsed()', cursorStart.isStartFocused(), cursorStart.isCollapsed());
+            const k = 'selection-start';
+            elements.push(
+              cursorStart.isStartFocused() ? (
+                cursorStart.isCollapsed() ? (
+                  <CaretView key={k} italic={!!italic} />
+                ) : (
+                  <FocusView key={k} italic={italic instanceof InlineAttrEnd} />
+                )
+              ) : (
+                <AnchorView key={k} />
+              ),
+            );
+          }
+        }
+        elements.push(<InlineView key={inline.key()} inline={inline} />);
+        if (hasCursor) {
+          const cursorEnd = inline.cursorEnd();
+          const attr = inline.attr();
+          const italic = attr[CommonSliceType.i] && attr[CommonSliceType.i][0];
+          if (cursorEnd) {
+            const k = 'selection-end';
+            elements.push(
+              cursorEnd.isEndFocused() ? (
+                cursorEnd.isCollapsed() ? (
+                  <CaretView key={k} italic={!!italic} />
+                ) : (
+                  <FocusView key={k} left italic={italic instanceof InlineAttrStart} />
+                )
+              ) : (
+                <AnchorView key={k} />
+              ),
+            );
+          }
+        }
+      }
     } else {
       const children = block.children;
       const length = children.length;
