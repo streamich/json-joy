3a938fc8094a353b44f78071be3f5ccd2ea40627
streamich <streamich@gmail.com>
2024-11-11 10:03:58 +0100

    refactor(json-crdt-peritext-ui): 💡 separate caret score into its own component

--- src/json-crdt-peritext-ui/plugins/minimal/RenderCaret.tsx
+++ src/json-crdt-peritext-ui/plugins/minimal/RenderCaret.tsx
@@ -19,28 +19,6 @@ const moveAnimation = keyframes({
   },
 });
 
-const scoreAnimation = keyframes({
-  from: {
-    op: .7,
-    tr: 'scale(1.2)',
-  },
-  to: {
-    op: 0,
-    tr: 'scale(.7)',
-    vis: 'hidden',
-  },
-});
-
-const shakingAnimation = keyframes({
-  '0%': { tr: 'translateX(0), scale(1.2)', op: 1 },
-  '10%': { tr: 'translateX(-2px)' },
-  '20%': { tr: 'translateX(2px)' },
-  '30%': { tr: 'translateX(-1px)' },
-  '40%': { tr: 'translateX(1px)' },
-  '50%': { tr: 'translateX(0), scale(1)' },
-  '100%': { op: 0, vis: 'hidden' },
-});
-
 const blockClass = rule({
   pos: 'relative',
   d: 'inline-block',
@@ -64,6 +42,63 @@ const innerClass = rule({
   bdrad: '0.0625em',
   'mix-blend-mode': 'multiply',
   an: moveAnimation + ' .25s ease-out forwards',
+});
+
+export interface RenderCaretProps extends CaretViewProps {
+  children: React.ReactNode;
+}
+
+export const RenderCaret: React.FC<RenderCaretProps> = ({italic, children}) => {
+  const ctx = usePeritext();
+  const pending = useSyncStore(ctx.peritext.editor.pending);
+  const [show, setShow] = React.useState(true);
+  useHarmonicIntervalFn(() => setShow(Date.now() % (ms + ms) > ms), ms);
+  const {dom} = usePeritext();
+  const focus = useSyncStore(dom.cursor.focus);
+  const {score, scoreDelta} = usePlugin();
+
+  const style: React.CSSProperties = {
+    background: !focus
+      ? DefaultRendererColors.InactiveCursor
+      : show
+        ? DefaultRendererColors.ActiveCursor
+        : 'transparent',
+  };
+
+  if (italic || pending.has(CommonSliceType.i)) {
+    style.rotate = '11deg';
+  }
+
+  return (
+    <span className={blockClass}>
+      <CaretScore score={score.value} delta={scoreDelta.value} />
+      <span className={innerClass} style={style}>
+        {children}
+      </span>
+    </span>
+  );
+};
+
+const scoreAnimation = keyframes({
+  from: {
+    op: .7,
+    tr: 'scale(1.2)',
+  },
+  to: {
+    op: 0,
+    tr: 'scale(.7)',
+    vis: 'hidden',
+  },
+});
+
+const shakingAnimation = keyframes({
+  '0%': { tr: 'translateX(0), scale(1.2)', op: 1 },
+  '10%': { tr: 'translateX(-2px)' },
+  '20%': { tr: 'translateX(2px)' },
+  '30%': { tr: 'translateX(-1px)' },
+  '40%': { tr: 'translateX(1px)' },
+  '50%': { tr: 'translateX(0), scale(1)' },
+  '100%': { op: 0, vis: 'hidden' },
 });
 
 const scoreClass = rule({
@@ -92,62 +127,40 @@ const scoreDeltaClass = rule({
   us: 'none',
 });
 
-export interface RenderCaretProps extends CaretViewProps {
-  children: React.ReactNode;
+interface CaretScoreProps {
+  score: number;
+  delta: number;
 }
 
-export const RenderCaret: React.FC<RenderCaretProps> = ({italic, children}) => {
-  const ctx = usePeritext();
-  const pending = useSyncStore(ctx.peritext.editor.pending);
-  const {score, scoreDelta} = usePlugin();
-  const [show, setShow] = React.useState(true);
-  useHarmonicIntervalFn(() => setShow(Date.now() % (ms + ms) > ms), ms);
-  const {dom} = usePeritext();
-  const focus = useSyncStore(dom.cursor.focus);
-
-  const style: React.CSSProperties = {
-    background: !focus
-      ? DefaultRendererColors.InactiveCursor
-      : show
-        ? DefaultRendererColors.ActiveCursor
-        : 'transparent',
-  };
-
-  if (italic || pending.has(CommonSliceType.i)) {
-    style.rotate = '11deg';
-  }
-
-  const s = score.value;
-  const d = scoreDelta.value;
-  const scoreMsg = s > 100 && s <= 120
-    ? 'Killing Spree!'
-      : s > 300 && s <= 320
-        ? 'Rampage!'
-        : s > 500 && s <= 520
-          ? 'Unstoppable!'
-          : s > 1000 && s <= 1030
-            ? 'Godlike!'
-            : s > 2000 && s <= 2030
-              ? 'Legendary!'
-              : s > 3000 && s <= 3040
-                ? 'Beyond Godlike!'
-                : s > 5000 && s <= 5040
-                  ? 'Wicked Sick!'
-                  : s > 10000 && s <= 10050
-                    ? 'Monster Kill!'
-                    : s > 20000 && s <= 20050
-                      ? 'Ultra Kill!'
-                      : s > 50000 && s <= 50100
-                        ? 'M-M-M-Monster Kill!'
-                        : s;
+const CaretScore: React.FC<CaretScoreProps> = React.memo(({score, delta}) => {
+  const scoreMsg = score > 100 && score <= 120
+    ? 'Typing Spree!'
+      : score > 200 && score <= 208
+      ? 'Go! Go! Go!'
+        : score > 300 && score <= 320
+          ? 'Rampage!'
+          : score > 500 && score <= 520
+            ? 'Unstoppable!'
+            : score > 1000 && score <= 1030
+              ? 'Godlike!'
+              : score > 2000 && score <= 2030
+                ? 'Legendary!'
+                : score > 3000 && score <= 3040
+                  ? 'Beyond Godlike!'
+                  : score > 5000 && score <= 5040
+                    ? 'Wicked Sick!'
+                    : score > 10000 && score <= 10050
+                      ? 'Monster Type!'
+                      : score > 20000 && score <= 20050
+                        ? 'Ultra Type!'
+                        : score > 50000 && score <= 50100
+                          ? 'M-M-M-Monster Type!'
+                          : score;
 
   return (
-    <span className={blockClass}>
-      {s >=24 && <span contentEditable={false} className={scoreClass} style={{animation: typeof scoreMsg === 'string' ? shakingAnimation + ' .7s ease-out forwards' : undefined}}>{scoreMsg}</span>}
-      {(typeof scoreMsg === 'string' || (s > 42 && d > 1)) && <span contentEditable={false} className={scoreDeltaClass}>+{d}</span>}
-      <span className={innerClass} style={style}>
-        {children}
-      </span>
-    </span>
+    <>
+      {score >=24 && <span contentEditable={false} className={scoreClass} style={{animation: typeof scoreMsg === 'string' ? shakingAnimation + ' .7s ease-out forwards' : undefined}}>{scoreMsg}</span>}
+      {(typeof scoreMsg === 'string' || (score > 42 && delta > 1)) && <span contentEditable={false} className={scoreDeltaClass}>+{delta}</span>}
+    </>
   );
-};
+});
