4d9df821dd34cbbfe34575072737ebeeeb8643d0
streamich <streamich@gmail.com>
2024-11-12 10:16:31 +0100

    feat(json-crdt-peritext-ui): 🎸 improve cursor JSDocs

--- src/json-crdt-peritext-ui/react/types.ts
+++ src/json-crdt-peritext-ui/react/types.ts
@@ -9,17 +9,74 @@ import type {PeritextSurfaceContextValue} from './context';
 import type {Inline} from '../../json-crdt-extensions/peritext/block/Inline';
 
 export interface PeritextPlugin {
-  focus?: (props: FocusViewProps, children: React.ReactNode) => React.ReactNode;
-  caret?: (props: CaretViewProps, children: React.ReactNode) => React.ReactNode;
-  anchor?: (props: AnchorViewProps, children: React.ReactNode) => React.ReactNode;
-  text?: (props: SpanProps, inline: Inline, ctx: PeritextSurfaceContextValue) => SpanProps | undefined;
-  inline?: (props: InlineViewProps, children: React.ReactNode) => React.ReactNode;
-  block?: (props: BlockViewProps, children: React.ReactNode) => React.ReactNode;
+
+  // --------------------------------------------------- Block-level formatting
+
   peritext?: (
     props: PeritextViewProps,
     children: React.ReactNode,
     ctx?: PeritextSurfaceContextValue,
   ) => React.ReactNode;
+  block?: (props: BlockViewProps, children: React.ReactNode) => React.ReactNode;
+
+  // -------------------------------------------------------- Inline formatting
+
+  text?: (props: SpanProps, inline: Inline, ctx: PeritextSurfaceContextValue) => SpanProps | undefined;
+  inline?: (props: InlineViewProps, children: React.ReactNode) => React.ReactNode;
+
+  // ------------------------------------------------------------------ Cursors
+
+  /**
+   * Renders a custom view for the *caret* (collapsed cursor) in the editor.
+   * 
+   * Make sure that this component is treated as `{ display: inline }` by the
+   * browser. Otherwise, it will break words, which will result in word
+   * wrapping, and the layout will shift when the caret is moved.
+   *
+   * @param props Props for the caret component.
+   * @param children React children that must be rendered inside the caret
+   *     component. The children are the output of the `caret` renderer of the
+   *     previous plugin in the chain, or the system caret if this is the first
+   *     plugin in the chain.
+   * @returns A react node.
+   */
+  caret?: (props: CaretViewProps, children: React.ReactNode) => React.ReactNode;
+
+  /**
+   * Renders a custom view for the *focus* edge of a selection
+   * (range expanded cursor) in the editor. The focus edge of the selection is
+   * the one that moves when the user presses the arrow keys.
+   * 
+   * Make sure that this component is treated as `{ display: inline }` by the
+   * browser. Otherwise, it will break words, which will result in word
+   * wrapping, and the layout will shift when the selection is moved.
+   *
+   * @param props Props for the focus component.
+   * @param children React children that must be rendered inside the focus
+   *     component. The children are the output of the `focus` renderer of the
+   *     previous plugin in the chain, or the system focus if this is the first
+   *     plugin in the chain.
+   * @returns A react node.
+   */
+  focus?: (props: FocusViewProps, children: React.ReactNode) => React.ReactNode;
+
+  /**
+   * Renders a custom view for the *anchor* edge of a selection
+   * (range expanded cursor) in the editor. The anchor edge of the selection is
+   * the one that stays in place when the user presses the arrow keys.
+   * 
+   * Make sure that this component is treated as `{ display: inline }` by the
+   * browser. Otherwise, it will break words, which will result in word
+   * wrapping, and the layout will shift when the selection is moved.
+   *
+   * @param props Props for the anchor component.
+   * @param children React children that must be rendered inside the anchor
+   *     component. The children are the output of the `anchor` renderer of the
+   *     previous plugin in the chain, or the system anchor if this is the first
+   *     plugin in the chain.
+   * @returns A react node.
+   */
+  anchor?: (props: AnchorViewProps, children: React.ReactNode) => React.ReactNode;
 }
 
 export type SpanProps = React.DetailedHTMLProps<React.HTMLAttributes<HTMLSpanElement>, HTMLSpanElement>;
