0e7c5955aac052e6261b1dc091edf231dda1dd2b
streamich <streamich@gmail.com>
2024-11-12 09:39:53 +0100

    refactor(json-crdt-peritext-ui): 💡 share text formatting function

--- src/json-crdt-peritext-ui/plugins/minimal/index.ts
+++ src/json-crdt-peritext-ui/plugins/minimal/index.ts
@@ -4,31 +4,14 @@ import {RenderFocus} from './RenderFocus';
 import {RenderAnchor} from './RenderAnchor';
 import {RenderInline} from './RenderInline';
 import {RenderPeritext} from './RenderPeritext';
-import {CommonSliceType} from '../../../json-crdt-extensions';
 import {RenderBlock} from './RenderBlock';
+import {text} from './text';
 import type {PeritextPlugin} from '../../react/types';
-import type {InlineAttrStack} from '../../../json-crdt-extensions/peritext/block/Inline';
 
 const h = React.createElement;
 
 export const renderers: PeritextPlugin = {
-  text: (props, inline) => {
-    const style = (props.style || (props.style = {})) as React.CSSProperties;
-    const attrs = inline.attr();
-
-    let textDecoration = '';
-    let attr: InlineAttrStack | undefined;
-
-    if (attrs[CommonSliceType.b]) style.fontWeight = 'bold';
-    if (attrs[CommonSliceType.i]) style.fontStyle = 'italic';
-    if (attrs[CommonSliceType.u]) textDecoration = 'underline';
-    if (attrs[CommonSliceType.s]) textDecoration = textDecoration ? textDecoration + ' line-through' : 'line-through';
-    if ((attr = attrs[CommonSliceType.col])) style.color = attr[0].slice.data() + '';
-
-    style.textDecoration = textDecoration;
-
-    return props;
-  },
+  text,
   caret: (props, children) => h(RenderCaret, <any>props, children),
   focus: (props, children) => h(RenderFocus, <any>props, children),
   anchor: (props) => h(RenderAnchor, <any>props),
