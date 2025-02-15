97f9f3a5f1a755b75a2d9d51d734c2ead201042c
streamich <streamich@gmail.com>
2025-02-15 23:31:07 +0100

    feat(json-crdt-peritext-ui): 🎸 render <kbd> inline annotation

--- src/json-crdt-peritext-ui/plugins/toolbar/RenderInline/index.tsx
+++ src/json-crdt-peritext-ui/plugins/toolbar/RenderInline/index.tsx
@@ -3,6 +3,7 @@ import * as React from 'react';
 import {CommonSliceType} from '../../../../json-crdt-extensions';
 import {Spoiler} from './Spoiler';
 import {Code} from './Code';
+import {Kbd} from './Kbd';
 import type {InlineViewProps} from '../../../react/InlineView';
 
 export interface RenderInlineProps extends InlineViewProps {
@@ -13,17 +14,20 @@ export const RenderInline: React.FC<RenderInlineProps> = (props) => {
   const {inline, children} = props;
   const attrs = inline.attr();
   let element = children;
-  if (attrs[CommonSliceType.code]) {
-    const attr = attrs[CommonSliceType.code][0];
-    if (attr) element = <Code attr={attr}>{element}</Code>;
-  }
   if (attrs[CommonSliceType.mark]) element = <mark>{element}</mark>;
   if (attrs[CommonSliceType.del]) element = <del>{element}</del>;
   if (attrs[CommonSliceType.ins]) element = <ins>{element}</ins>;
   if (attrs[CommonSliceType.sup]) element = <sup>{element}</sup>;
   if (attrs[CommonSliceType.sub]) element = <sub>{element}</sub>;
   if (attrs[CommonSliceType.math]) element = <code>{element}</code>;
-  if (attrs[CommonSliceType.kbd]) element = <kbd>{element}</kbd>;
+  if (attrs[CommonSliceType.code]) {
+    const attr = attrs[CommonSliceType.code][0];
+    if (attr) element = <Code attr={attr}>{element}</Code>;
+  }
+  if (attrs[CommonSliceType.kbd]) {
+    const attr = attrs[CommonSliceType.kbd][0];
+    if (attr) element = <Kbd attr={attr}>{element}</Kbd>;
+  }
   if (attrs[CommonSliceType.spoiler]) element = <Spoiler>{element}</Spoiler>;
   return element;
 };
