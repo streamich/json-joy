dd34387869289c4f47b883440690fafbf7591266
streamich <streamich@gmail.com>
2025-02-28 21:08:04 +0100

    feat(json-crdt-peritext-ui): 🎸 implement synchronous copyt-to-clipboard method

--- src/json-crdt-peritext-ui/events/clipboard/ClipboardFacade.ts
+++ src/json-crdt-peritext-ui/events/clipboard/ClipboardFacade.ts
@@ -1,17 +1,50 @@
 import type {PeritextClipboard, PeritextClipboardData} from './types';
 
+const writeSync = (data: PeritextClipboardData): boolean => {
+  if (typeof document !== 'object') return false;
+  const value = data['text/plain'] ?? '';
+  const text = typeof value === 'string' ? value : '';
+  const span = document.createElement('span');
+  span.textContent = text;
+  span.style.whiteSpace = 'pre';
+  span.style.userSelect = 'all';
+  const selection = window.getSelection();
+  if (!selection) return false;
+  let ranges = [];
+  for (let i = 0; i < selection.rangeCount; i++)
+    ranges.push(selection.getRangeAt(i));
+  selection.removeAllRanges();
+  try {
+    document.body.appendChild(span);
+    const range = document.createRange();
+    range.selectNode(span);
+    selection.addRange(range)
+    document.execCommand('cut');
+  } catch {
+    return false;
+  } finally {
+    try {
+      selection.removeAllRanges();
+      document.body.removeChild(span);
+      for (const range of ranges) selection.addRange(range);
+    } catch {}
+  }
+  return true;
+};
+
 export class ClipboardFacade implements PeritextClipboard {
   constructor (protected readonly clipboard: Clipboard) {}
 
-  public async writeText(text: string): Promise<void> {
-    return await this.clipboard.writeText(text);
+  public writeText(text: string): undefined | Promise<void> {
+    const success = writeSync({'text/plain': text});
+    if (success) return;
+    return this.clipboard.writeText(text);
   }
 
-  public async write(data: PeritextClipboardData): Promise<void> {
+  public write(data: PeritextClipboardData): undefined | Promise<void> {
     // const item = new ClipboardItem(data, {presentationStyle: 'inline'});
     const item = new ClipboardItem(data);
     const items: ClipboardItem[] = [item];
-    console.log(items);
-    return await this.clipboard.write(items);
+    return this.clipboard.write(items);
   }
 }
--- src/json-crdt-peritext-ui/events/clipboard/types.ts
+++ src/json-crdt-peritext-ui/events/clipboard/types.ts
@@ -1,15 +1,21 @@
 export interface PeritextClipboard {
   /**
    * @param text The text to write to the clipboard.
+   * @returns Returns `undefined` if text was written synchronously. If text
+   *     was written using an async API returns a promise which resolves when
+   *     the text has been written.
    */
-  writeText(text: string): Promise<void>;
+  writeText(text: string): undefined | Promise<void>;
 
   /**
    * Writes data to the clipboard. Can contain multiple mime-types.
    * @param data The data to write to the clipboard. Can contain multiple
    *     mime-types.
+   * @returns Returns `undefined` if data was written synchronously. If data
+   *     was written using an async API returns a promise which resolves when
+   *     the data has been written.
    */
-  write(data: PeritextClipboardData): Promise<void>;
+  write(data: PeritextClipboardData): undefined | Promise<void>;
 }
 
 export interface PeritextClipboardData {
