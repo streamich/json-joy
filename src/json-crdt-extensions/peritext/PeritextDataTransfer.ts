ee65e462732c48c8fa4a5b6703a5e7ff0b1a5013
streamich <streamich@gmail.com>
2025-03-01 15:46:35 +0100

    feat(json-crdt-peritext-ui): 🎸 implement Markdown data transfer

--- src/json-crdt-extensions/peritext/PeritextDataTransfer.ts
+++ src/json-crdt-extensions/peritext/PeritextDataTransfer.ts
@@ -6,14 +6,19 @@ import type {Peritext} from './Peritext';
 import type {PeritextMlElement, PeritextMlNode} from './block/types';
 import type {JsonMlNode} from 'very-small-parser/lib/html/json-ml/types';
 import type {THtmlToken} from 'very-small-parser/lib/html/types';
+import type {IRoot} from 'very-small-parser/lib/markdown/block/types';
 
 export type PeritextDataTransferHtmlExportTools = typeof import('./lazy/export-html');
 export type PeritextDataTransferHtmlImportTools = typeof import('./lazy/import-html');
+export type PeritextDataTransferMarkdownExportTools = typeof import('./lazy/export-markdown');
+export type PeritextDataTransferMarkdownImportTools = typeof import('./lazy/import-markdown');
 
 export interface PeritextDataTransferOpts {
   registry?: SliceRegistry;
   htmlExport?: PeritextDataTransferHtmlExportTools;
   htmlImport?: PeritextDataTransferHtmlImportTools;
+  mdExport?: PeritextDataTransferMarkdownExportTools;
+  mdImport?: PeritextDataTransferMarkdownImportTools;
 }
 
 export class PeritextDataTransfer<T = string> {
@@ -58,6 +63,24 @@ export class PeritextDataTransfer<T = string> {
     return tools.toHtml(json, tab, indent);
   }
 
+  protected mdE(): PeritextDataTransferMarkdownExportTools {
+    const tools = this.opts.mdExport;
+    if (!tools) throw new Error('NO_MD_EXPORT_TOOLS');
+    return tools;
+  }
+  
+  public toMdast(range: Range<T>): IRoot {
+    const tools = this.mdE();
+    const json = this.toJson(range);
+    return tools.toMdast(json);
+  };
+  
+  public toMarkdown(range: Range<T>): string {
+    const tools = this.mdE();
+    const json = this.toJson(range);
+    return tools.toMarkdown(json);
+  }
+
   // ------------------------------------------------------------------ imports
 
   public fromView(pos: number, view: ViewRange): void {
@@ -70,6 +93,12 @@ export class PeritextDataTransfer<T = string> {
     this.fromView(pos, view);
   }
 
+  private _imp<D>(pos: number, data: D, transform: (data: D, registry: SliceRegistry) => PeritextMlNode): void {
+    const registry = this.opts.registry ?? defaultRegistry;
+    const json = transform(data, registry);
+    this.fromJson(pos, json);
+  }
+
   protected htmlI(): PeritextDataTransferHtmlImportTools {
     const tools = this.opts.htmlImport;
     if (!tools) throw new Error('NO_HTML_IMPORT_TOOLS');
@@ -77,23 +106,28 @@ export class PeritextDataTransfer<T = string> {
   }
 
   public fromJsonMl(pos: number, jsonml: JsonMlNode): void {
-    const tools = this.htmlI();
-    const registry = this.opts.registry ?? defaultRegistry;
-    const json = tools.fromJsonMl(jsonml, registry);
-    this.fromJson(pos, json);
+    this._imp(pos, jsonml, this.htmlI().fromJsonMl);
   }
   
   public fromHast(pos: number, hast: THtmlToken): void {
-    const tools = this.htmlI();
-    const registry = this.opts.registry ?? defaultRegistry;
-    const json = tools.fromHast(hast, registry);
-    this.fromJson(pos, json);
+    this._imp(pos, hast, this.htmlI().fromHast);
   }
   
   public fromHtml(pos: number, html: string): void {
-    const tools = this.htmlI();
-    const registry = this.opts.registry ?? defaultRegistry;
-    const json = tools.fromHtml(html, registry);
-    this.fromJson(pos, json);
+    this._imp(pos, html, this.htmlI().fromHtml);
+  }
+
+  protected mdI(): PeritextDataTransferMarkdownImportTools {
+    const tools = this.opts.mdImport;
+    if (!tools) throw new Error('NO_MD_IMPORT_TOOLS');
+    return tools;
+  }
+
+  public fromMdast(pos: number, mdast: IRoot): void {
+    this._imp(pos, mdast, this.mdI().fromMdast);
+  }
+
+  public fromMarkdown(pos: number, markdown: string): void {
+    this._imp(pos, markdown, this.mdI().fromMarkdown);
   }
 }
