55b0afe79a82fabb75fab5e04a8905de6dad0ea8
streamich <streamich@gmail.com>
2025-03-08 17:58:49 +0100

    test(json-crdt-extensions): 💍 harden data transfer tests

--- src/json-crdt-extensions/peritext/transfer/__tests__/PeritextDataTransfer.spec.ts
+++ src/json-crdt-extensions/peritext/transfer/__tests__/PeritextDataTransfer.spec.ts
@@ -4,6 +4,7 @@ import * as mdExport from '../export-markdown';
 import * as mdImport from '../import-markdown';
 import {PeritextDataTransfer} from '../PeritextDataTransfer';
 import {setupKit} from '../../__tests__/setup';
+import {CommonSliceType} from '../../slice';
 
 const setup = () => {
   const kit = setupKit();
@@ -131,5 +132,65 @@ describe('Markdown', () => {
     expect(html).toBe('<p>a<b>123</b>b</p>');
     const md2 = transfer.toMarkdown(all);
     expect(md2).toBe('a__123__b');
+  });
+
+  test('can insert multiple annotations into text', () => {
+    const {peritext, transfer} = setup();
+    const md = '__123__ and *456*';
+    peritext.strApi().ins(0, 'ab');
+    peritext.refresh();
+    transfer.fromMarkdown(1, md);
+    peritext.refresh();
+    const all = peritext.rangeAll()!;
+    const html = transfer.toHtml(all);
+    expect(html).toBe('<p>a<b>123</b> and <i>456</i>b</p>');
+    const md2 = transfer.toMarkdown(all);
+    expect(md2).toBe('a__123__ and _456_b');
+  });
+
+  test('can insert two paragraphs into text', () => {
+    const {peritext, transfer} = setup();
+    const md = '123\n\n456';
+    peritext.strApi().ins(0, 'ab');
+    peritext.refresh();
+    transfer.fromMarkdown(1, md);
+    peritext.refresh();
+    const all = peritext.rangeAll()!;
+    const html = transfer.toHtml(all);
+    expect(html).toBe('<p>a123</p><p>456b</p>');
+    const md2 = transfer.toMarkdown(all);
+    expect(md2).toBe('a123\n\n456b');
+  });
+
+  test('can insert two paragraphs into the first paragraph of two-paragraph text', () => {
+    const {peritext, transfer} = setup();
+    const md = '__123__\n\n++456++';
+    peritext.strApi().ins(0, 'abcd');
+    peritext.editor.cursor.setAt(2);
+    peritext.editor.saved.insMarker(CommonSliceType.p);
+    peritext.refresh();
+    transfer.fromMarkdown(1, md);
+    peritext.refresh();
+    const all = peritext.rangeAll()!;
+    const html = transfer.toHtml(all);
+    expect(html).toBe('<p>a<b>123</b></p><p><u>456</u>b</p><p>cd</p>');
+    const md2 = transfer.toMarkdown(all);
+    expect(md2).toBe('a__123__\n\n++456++b\n\ncd');
+  });
+
+  test('can insert two paragraphs into the second paragraph of two-paragraph text', () => {
+    const {peritext, transfer} = setup();
+    const md = '__123__\n\n++456++';
+    peritext.strApi().ins(0, 'abcd');
+    peritext.editor.cursor.setAt(2);
+    peritext.editor.saved.insMarker(CommonSliceType.p);
+    peritext.refresh();
+    transfer.fromMarkdown(4, md);
+    peritext.refresh();
+    const all = peritext.rangeAll()!;
+    const html = transfer.toHtml(all);
+    expect(html).toBe('<p>ab</p><p>c<b>123</b></p><p><u>456</u>d</p>');
+    const md2 = transfer.toMarkdown(all);
+    expect(md2).toBe('ab\n\nc__123__\n\n++456++d');
   });
 });
