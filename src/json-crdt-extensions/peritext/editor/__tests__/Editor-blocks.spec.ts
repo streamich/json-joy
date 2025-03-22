a9c2b1040bcb269eef5c09ead552dd4f23414255
streamich <streamich@gmail.com>
2025-03-22 22:22:31 +0100

    test(json-crdt-extensions): 💍 add <blockquote> split and join tests

---
+++ src/json-crdt-extensions/peritext/editor/__tests__/Editor-blocks.spec.ts
@@ -0,0 +1,43 @@
+import {Kit, runAlphabetKitTestSuite} from '../../__tests__/setup';
+import {SliceTypeCon} from '../../slice/constants';
+import {create} from '../../transfer/create';
+
+const runTests = (setup: () => Kit) => {
+  test('can join two blockquotes', () => {
+    const {peritext, editor} = setup();
+    editor.cursor.setAt(3);
+    editor.saved.insMarker([SliceTypeCon.blockquote, SliceTypeCon.p]);
+    editor.cursor.setAt(7);
+    editor.saved.insMarker([[SliceTypeCon.blockquote, 1], SliceTypeCon.p]);
+    peritext.refresh();
+    const transfer = create(peritext);
+    const html1 = transfer.toHtml(peritext.rangeAll()!);
+    expect(html1).toBe('<p>abc</p><blockquote><p>def</p></blockquote><blockquote><p>ghijklmnopqrstuvwxyz</p></blockquote>');
+    editor.cursor.setAt(10);
+    const marker = peritext.overlay.getOrNextLowerMarker(editor.cursor.start)!;
+    marker.marker.update({type: [SliceTypeCon.blockquote, SliceTypeCon.p]});
+    peritext.refresh();
+    const html2 = transfer.toHtml(peritext.rangeAll()!);
+    expect(html2).toBe('<p>abc</p><blockquote><p>def</p><p>ghijklmnopqrstuvwxyz</p></blockquote>');
+  });
+
+  test('can split two blockquotes', () => {
+    const {peritext, editor} = setup();
+    editor.cursor.setAt(3);
+    editor.saved.insMarker([SliceTypeCon.blockquote, SliceTypeCon.p]);
+    editor.cursor.setAt(7);
+    editor.saved.insMarker([SliceTypeCon.blockquote, SliceTypeCon.p]);
+    peritext.refresh();
+    const transfer = create(peritext);
+    const html1 = transfer.toHtml(peritext.rangeAll()!);
+    expect(html1).toBe('<p>abc</p><blockquote><p>def</p><p>ghijklmnopqrstuvwxyz</p></blockquote>');
+    editor.cursor.setAt(10);
+    const marker = peritext.overlay.getOrNextLowerMarker(editor.cursor.start)!;
+    marker.marker.update({type: [[SliceTypeCon.blockquote, 1], SliceTypeCon.p]});
+    peritext.refresh();
+    const html2 = transfer.toHtml(peritext.rangeAll()!);
+    expect(html2).toBe('<p>abc</p><blockquote><p>def</p></blockquote><blockquote><p>ghijklmnopqrstuvwxyz</p></blockquote>');
+  });
+};
+
+runAlphabetKitTestSuite(runTests);
