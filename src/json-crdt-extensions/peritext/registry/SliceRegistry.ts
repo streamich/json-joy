8fcefc85c1fe7edf1f2ab7e097c0a3fac2b6833b
Vadim Dalecky <streamich@gmail.com>
2025-03-23 12:55:53 +0100

    feat(json-crdt-extensions): 🎸 improve slice registry endtry definition

--- src/json-crdt-extensions/peritext/registry/SliceRegistry.ts
+++ src/json-crdt-extensions/peritext/registry/SliceRegistry.ts
@@ -1,4 +1,4 @@
-import {SliceBehavior} from '../slice/constants';
+import {SliceBehavior, SliceTypeCon} from '../slice/constants';
 import {CommonSliceType} from '../slice';
 import type {PeritextMlElement} from '../block/types';
 import type {NodeBuilder} from '../../../json-crdt-patch';
@@ -9,31 +9,77 @@ import type {SchemaToJsonNode} from '../../../json-crdt/schema/types';
 
 export class SliceRegistryEntry<
   Behavior extends SliceBehavior = SliceBehavior,
-  Type extends number | string = number | string,
+  Tag extends SliceTypeCon | number | string = SliceTypeCon | number | string,
   Schema extends NodeBuilder = NodeBuilder,
 > {
+
+  public isInline(): boolean {
+    return this.behavior !== SliceBehavior.Marker;
+  }
+  
   constructor(
-    behavior: Behavior,
-    type: Type,
-    schema: Schema,
+    public readonly behavior: Behavior,
 
     /**
-     * For block element slices (block splits), the `container` property specifies
-     * whether the type is a container for other block elements. For example, a
-     * `blockquote` is a container for `paragraph` elements, however, a `paragraph`
-     * is not a container (it can only contain inline elements).
+     * The tag name of this slice. The tag is one step in the type path of the
+     * slice. For example, below is a type path composed of three steps:
+     * 
+     * ```js
+     * ['ul', 'li', 'p']
+     * ```
+     * 
+     * Tag types are normally numbers of type {@link SliceTypeCon}, however,
+     * they can also be any arbitrary strings or numbers.
      */
-    container?: boolean,
+    public readonly tag: Tag,
+
+    /**
+     * Default expected schema of the slice data.
+     */
+    public readonly schema: Schema,
+
+    /**
+     * This property is relevant only for block split markers. It specifies
+     * whether the block split marker is a container for other block elements.
+     *
+     * For example, a `blockquote` is a container for `paragraph` elements,
+     * however, a `paragraph` is not a container (it can only contain inline
+     * elements).
+     * 
+     * If the marker slice is of the container sort, they tag can appear in the
+     * path steps of the type:
+     * 
+     * ```
+     * 
+     * ```
+     */
+    public readonly container: boolean = false,
     
-    toHtml?: ToHtmlConverter<PeritextMlElement<Type, JsonNodeView<SchemaToJsonNode<Schema>>, Behavior extends SliceBehavior.Marker ? false : true>>,
-    fromHtml?: {
-      [htmlTag: string]: FromHtmlConverter<PeritextMlElement<Type, JsonNodeView<SchemaToJsonNode<Schema>>, Behavior extends SliceBehavior.Marker ? false : true>>;
+    /**
+     * Converts a node of this type to HTML representation: returns the HTML tag
+     * and attributes. The method receives {@link PeritextMlElement} as an
+     * argument, which is a tuple of internal HTML-like representation of the
+     * node.
+     */
+    public readonly toHtml: ToHtmlConverter<PeritextMlElement<Tag, JsonNodeView<SchemaToJsonNode<Schema>>, Behavior extends SliceBehavior.Marker ? false : true>> | undefined = void 0,
+
+    /**
+     * Specifies a mapping of converters from HTML {@link JsonMlElement} to
+     * {@link PeritextMlElement}. This way a slice type can specify multiple
+     * HTML tags that are converted to the same slice type.
+     * 
+     * For example, both, `<b>` and `<strong>` tags can be converted to the
+     * {@link SliceTypeCon.b} slice type.
+     */
+    public readonly fromHtml?: {
+      [htmlTag: string]: FromHtmlConverter<PeritextMlElement<Tag, JsonNodeView<SchemaToJsonNode<Schema>>, Behavior extends SliceBehavior.Marker ? false : true>>;
     },
   ) {}
 }
 
 /**
- * @todo Consider moving the registry under the `/transfer` directory.
+ * @todo Consider moving the registry under the `/transfer` directory. Or maybe
+ * `/slices` directory.
  */
 export class SliceRegistry {
   private map: Map<string | number, SliceTypeDefinition<any, any, any>> = new Map();
