6eb56a83676edbd599d31b6ce64b8efc14d1702c
streamich <streamich@gmail.com>
2025-03-28 18:26:02 +0100

    feat(json-crdt-peritext-ui): 🎸 create <CharOverlay> helper for debugging overlays

---
+++ src/json-crdt-peritext-ui/plugins/debug/RenderCaret/CharOverlay.tsx
@@ -0,0 +1,36 @@
+import * as React from 'react';
+import useWindowSize from 'react-use/lib/useWindowSize';
+import useWindowScroll from 'react-use/lib/useWindowScroll';
+import type {Rect} from '../../../dom/types';
+
+export type SetRect = (rect?: Rect) => void;
+
+export interface CharOverlayProps extends React.HTMLAttributes<HTMLSpanElement> {
+  rectRef: React.RefObject<SetRect>;
+}
+
+export const CharOverlay: React.FC<CharOverlayProps> = ({rectRef, ...rest}) => {
+  useWindowSize();
+  useWindowScroll();
+  const ref = React.useRef<HTMLSpanElement | null>(null);
+  React.useEffect(() => {
+    (rectRef as any).current = (rect?: Rect) => {
+      const span = ref.current;
+      if (!span) return;
+      const style = span.style;
+      if (rect) {
+        style.top = rect.y + 'px';
+        style.left = rect.x + 'px';
+        style.width = rect.width + 'px';
+        style.height = rect.height + 'px';
+        style.visibility = 'visible';
+      } else {
+        style.visibility = 'hidden';
+      }
+    };
+  }, []);
+
+  return (
+    <span {...rest} ref={ref} />
+  );
+};
