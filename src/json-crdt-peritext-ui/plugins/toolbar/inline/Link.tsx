34bdb11fbf4896847c8cdb026d19dd0696a32d77
Vadim Dalecky <streamich@gmail.com>
2025-04-21 13:11:35 +0200

    feat(json-crdt-peritext-ui): 🎸 add link decoration styles

--- src/json-crdt-peritext-ui/plugins/toolbar/inline/Link.tsx
+++ src/json-crdt-peritext-ui/plugins/toolbar/inline/Link.tsx
@@ -2,9 +2,45 @@
 import * as React from 'react';
 import {rule} from 'nano-theme';
 
-const delClass = rule({
+const blockClass = rule({
   col: 'blue',
+  td: 'underline',
+  textDecorationColor: 'blue',
+  textDecorationThickness: '1px',
+  textDecorationStyle: 'wavy',
+  textUnderlineOffset: '.25em',
+  // textDecorationSkipInk: 'all',
+
+  '&:hover': {
+    col: 'blue',
+  },
+  pd: 0,
+  mr: 0,
+
+  // Needed temporarily, to reset nano-theme styles
+  bdb: 'none',
+  'p &': {
+    bdb: 'none',
+    '&:hover': {
+      bdb: 'none',
+    },
+  },
 });
+
+// RESET THIS
+// a: {
+//   col: lightTheme.color.sem.link[0],
+//   td: 'none',
+//   '&:hover': {
+//     col: lightTheme.color.sem.brand[0],
+//   },
+//   'p &': {
+//     bdb: '1px solid rgba(0,137,255,.3)',
+//     '&:hover': {
+//       bdb: '1px solid rgba(244,18,36,.3)',
+//     },
+//   },
+// },
 
 export interface LinkProps {
   children: React.ReactNode;
@@ -13,5 +49,6 @@ export interface LinkProps {
 export const Link: React.FC<LinkProps> = (props) => {
   const {children} = props;
 
-  return <a className={delClass}>{children}</a>;
+  // return <span className={blockClass}>{children}</span>;
+  return <a className={blockClass}>{children}</a>;
 };
