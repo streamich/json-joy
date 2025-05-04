2a40471cbbed24f6b283b3a50be667f2a52b9276
Vadim Dalecky <streamich@gmail.com>
2025-05-04 22:14:21 +0200

    feat(util): 🎸 initial, outer, implementation of array diff

---
+++ src/util/diff/__tests__/arr.spec.ts
@@ -0,0 +1,133 @@
+import * as arr from '../arr';
+
+test('insert into empty list', () => {
+  const patch = arr.diff('', '1');
+  expect(patch).toEqual([arr.ARR_PATCH_OP_TYPE.INSERT, 1]);
+});
+
+test('delete the only element', () => {
+  const patch = arr.diff('1', '');
+  expect(patch).toEqual([arr.ARR_PATCH_OP_TYPE.DELETE, 1]);
+});
+
+test('delete the only two element', () => {
+  const patch = arr.diff('1\n{}', '');
+  expect(patch).toEqual([arr.ARR_PATCH_OP_TYPE.DELETE, 2]);
+});
+
+test('both empty', () => {
+  const patch = arr.diff('', '');
+  expect(patch).toEqual([]);
+});
+
+test('keep one', () => {
+  const patch = arr.diff('1', '1');
+  expect(patch).toEqual([arr.ARR_PATCH_OP_TYPE.EQUAL, 1]);
+});
+
+test('keep two', () => {
+  const patch = arr.diff('1\n1', '1\n1');
+  expect(patch).toEqual([
+    arr.ARR_PATCH_OP_TYPE.EQUAL, 2,
+  ]);
+});
+
+test('keep three', () => {
+  const patch = arr.diff('1\n1\n2', '1\n1\n2');
+  expect(patch).toEqual([
+    arr.ARR_PATCH_OP_TYPE.EQUAL, 3,
+  ]);
+});
+
+test('keep two, delete one', () => {
+  const patch = arr.diff('1\n1\n2', '1\n1');
+  expect(patch).toEqual([
+    arr.ARR_PATCH_OP_TYPE.EQUAL, 2,
+    arr.ARR_PATCH_OP_TYPE.DELETE, 1,
+  ]);
+});
+
+test('keep two, delete in the middle', () => {
+  const patch = arr.diff('1\n2\n3', '1\n3');
+  expect(patch).toEqual([
+    arr.ARR_PATCH_OP_TYPE.EQUAL, 1,
+    arr.ARR_PATCH_OP_TYPE.DELETE, 1,
+    arr.ARR_PATCH_OP_TYPE.EQUAL, 1,
+  ]);
+});
+
+test('keep two, delete the first one', () => {
+  const patch = arr.diff('1\n2\n3', '2\n3');
+  expect(patch).toEqual([
+    arr.ARR_PATCH_OP_TYPE.DELETE, 1,
+    arr.ARR_PATCH_OP_TYPE.EQUAL, 2,
+  ]);
+});
+
+test('delete two and three in a row', () => {
+  const patch = arr.diff('1\n2\n3\n4\n5\n6', '3');
+  expect(patch).toEqual([
+    arr.ARR_PATCH_OP_TYPE.DELETE, 2,
+    arr.ARR_PATCH_OP_TYPE.EQUAL, 1,
+    arr.ARR_PATCH_OP_TYPE.DELETE, 3,
+  ]);
+});
+
+test('delete the first one', () => {
+  const patch = arr.diff('1\n2\n3', '2\n3');
+  expect(patch).toEqual([
+    arr.ARR_PATCH_OP_TYPE.DELETE, 1,
+    arr.ARR_PATCH_OP_TYPE.EQUAL, 2,
+  ]);
+});
+
+test('delete the middle element', () => {
+  const patch = arr.diff('1\n2\n3', '1\n3');
+  expect(patch).toEqual([
+    arr.ARR_PATCH_OP_TYPE.EQUAL, 1,
+    arr.ARR_PATCH_OP_TYPE.DELETE, 1,
+    arr.ARR_PATCH_OP_TYPE.EQUAL, 1,
+  ]);
+});
+
+test('delete the last element', () => {
+  const patch = arr.diff('1\n2\n3', '1\n2');
+  expect(patch).toEqual([
+    arr.ARR_PATCH_OP_TYPE.EQUAL, 2,
+    arr.ARR_PATCH_OP_TYPE.DELETE, 1,
+  ]);
+});
+
+test('delete two first elements', () => {
+  const patch = arr.diff('1\n2\n3\n4', '3\n4');
+  expect(patch).toEqual([
+    arr.ARR_PATCH_OP_TYPE.DELETE, 2,
+    arr.ARR_PATCH_OP_TYPE.EQUAL, 2,
+  ]);
+});
+
+test('preserve one and delete one', () => {
+  const patch = arr.diff('1\n2', '1');
+  expect(patch).toEqual([
+    arr.ARR_PATCH_OP_TYPE.EQUAL, 1,
+    arr.ARR_PATCH_OP_TYPE.DELETE, 1,
+  ]);
+});
+
+test('preserve one and delete one (reverse)', () => {
+  const patch = arr.diff('1\n2', '2');
+  expect(patch).toEqual([
+    arr.ARR_PATCH_OP_TYPE.DELETE, 1,
+    arr.ARR_PATCH_OP_TYPE.EQUAL, 1,
+  ]);
+});
+
+test('various deletes and inserts', () => {
+  const patch = arr.diff('1\n2\n[3]\n3\n5\n{a:4}\n5\n"6"', '1\n2\n[3]\n5\n{a:4}\n5\n"6"\n6');
+  expect(patch).toEqual([
+    arr.ARR_PATCH_OP_TYPE.EQUAL, 3,
+    arr.ARR_PATCH_OP_TYPE.DELETE, 1,
+    arr.ARR_PATCH_OP_TYPE.EQUAL, 4,
+    arr.ARR_PATCH_OP_TYPE.INSERT, 1,
+  ]);
+});
---
+++ src/util/diff/arr.ts
@@ -0,0 +1,80 @@
+import {strCnt} from "../strCnt";
+import * as str from "./str";
+
+export const enum ARR_PATCH_OP_TYPE {
+  DELETE = str.PATCH_OP_TYPE.DELETE,
+  EQUAL = str.PATCH_OP_TYPE.EQUAL,
+  INSERT = str.PATCH_OP_TYPE.INSERT,
+  PUSH = 2,
+  DIFF = 3,
+}
+
+export type ArrPatch = number[];
+
+const enum PARTIAL_TYPE {
+  NONE = 24,
+}
+
+export const diff = (txtSrc: string, txtDst: string): ArrPatch => {
+  const arrPatch: ArrPatch = [];
+  const patch = str.diff(txtSrc, txtDst);
+  if (patch.length === 1) {
+    if ((patch[0][0] as unknown as ARR_PATCH_OP_TYPE) === ARR_PATCH_OP_TYPE.INSERT) {
+      arrPatch.push(ARR_PATCH_OP_TYPE.INSERT, strCnt("\n", txtDst) + 1);
+      return arrPatch;
+    }
+  }
+  const push = (type: ARR_PATCH_OP_TYPE, count: number) => {
+    const length = arrPatch.length;
+    if (length !== 0) {
+      const lastType = arrPatch[length - 2] as unknown as ARR_PATCH_OP_TYPE;
+      if (lastType === type) {
+        arrPatch[length - 1] = (arrPatch[length - 1] as unknown as number) + count;
+        return;
+      }
+    }
+    arrPatch.push(type, count);
+  };
+  // console.log(txtSrc);
+  // console.log(txtDst);
+  // console.log(patch);
+  const patchLen = patch.length;
+  const lastOpIndex = patchLen - 1;
+  let partial: ARR_PATCH_OP_TYPE | PARTIAL_TYPE = PARTIAL_TYPE.NONE;
+  for (let i = 0; i <= lastOpIndex; i++) {
+    const isLastOp = i === lastOpIndex;
+    const op = patch[i];
+    const type: ARR_PATCH_OP_TYPE = op[0] as unknown as ARR_PATCH_OP_TYPE;
+    const txt = op[1];
+    if (!txt) continue;
+    let lineStartOffset = 0;
+    if (partial !== PARTIAL_TYPE.NONE) {
+      const index = txt.indexOf("\n");
+      if (index === 0) {
+        lineStartOffset = 1;
+        push(partial, 1);
+        partial = PARTIAL_TYPE.NONE;
+      } else {
+        lineStartOffset = index + 1;
+        // console.log("DIFF");
+        throw new Error("Not implemented");
+      }
+    }
+    const lineCount = strCnt("\n", txt, lineStartOffset) + (isLastOp ? 1 : 0);
+    const isPartial = txt[txt.length - 1] !== "\n";
+    if (isPartial) {
+      if (partial === PARTIAL_TYPE.NONE) partial = type;
+      else partial = (partial as unknown as ARR_PATCH_OP_TYPE) === type
+        ? (type as unknown as ARR_PATCH_OP_TYPE) : ARR_PATCH_OP_TYPE.DIFF;
+    }
+    if (!lineCount) continue;
+    if (type === ARR_PATCH_OP_TYPE.EQUAL) {
+      push(ARR_PATCH_OP_TYPE.EQUAL, lineCount);
+    } else if (type === ARR_PATCH_OP_TYPE.INSERT) {
+      push(ARR_PATCH_OP_TYPE.INSERT, lineCount);
+    } else {
+      push(ARR_PATCH_OP_TYPE.DELETE, lineCount);
+    }
+  }
+  return arrPatch;
+};
