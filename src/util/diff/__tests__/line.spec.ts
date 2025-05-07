7383d954325643150796eb6dee517f154e76342e
Vadim Dalecky <streamich@gmail.com>
2025-05-07 18:50:40 +0200

    fix(util): 🐛 do not emit empty operations

--- src/util/diff/__tests__/line.spec.ts
+++ src/util/diff/__tests__/line.spec.ts
@@ -197,4 +197,73 @@ describe('diff', () => {
       [ [ -1, '{"id": "abc", "name": "Merry Jane"}' ] ],
     ]);
   });
+
+  test('move last line to the beginning', () => {
+    const src = [
+      '{"id": "xxx-xxxxxxx", "name": "Hello, world"}',
+      '{"id": "xxx-yyyyyyy", "name": "Joe Doe"}',
+      '{"id": "lkasdjflkasjdf", "name": "Winston Churchill"}',
+      '{"id": "abc", "name": "Merry Jane"}',
+    ];
+    const dst = [
+      '{"id": "abc", "name": "Merry Jane"}',
+      '{"id": "xxx-xxxxxxx", "name": "Hello, world"}',
+      '{"id": "xxx-yyyyyyy", "name": "Joe Doe"}',
+      '{"id": "lkasdjflkasjdf", "name": "Winston Churchill"}',
+    ];
+    const patch = line.diffLines(src, dst);
+    expect(patch).toEqual([
+      [ [ 1, '{"id": "abc", "name": "Merry Jane"}' ] ],
+      [ [ 0, '{"id": "xxx-xxxxxxx", "name": "Hello, world"}' ] ],
+      [ [ 0, '{"id": "xxx-yyyyyyy", "name": "Joe Doe"}' ] ],
+      [ [ 0, '{"id": "lkasdjflkasjdf", "name": "Winston Churchill"}' ] ],
+      [ [ -1, '{"id": "abc", "name": "Merry Jane"}' ] ],
+    ]);
+  });
+
+  test('move second to last line to the beginning', () => {
+    const src = [
+      '{"id": "xxx-xxxxxxx", "name": "Hello, world"}',
+      '{"id": "xxx-yyyyyyy", "name": "Joe Doe"}',
+      '{"id": "lkasdjflkasjdf", "name": "Winston Churchill"}',
+      '{"id": "abc", "name": "Merry Jane"}',
+    ];
+    const dst = [
+      '{"id": "lkasdjflkasjdf", "name": "Winston Churchill"}',
+      '{"id": "xxx-xxxxxxx", "name": "Hello, world"}',
+      '{"id": "xxx-yyyyyyy", "name": "Joe Doe"}',
+      '{"id": "abc", "name": "Merry Jane"}',
+    ];
+    const patch = line.diffLines(src, dst);
+    expect(patch).toEqual([
+      [ [ 1, '{"id": "lkasdjflkasjdf", "name": "Winston Churchill"}' ] ],
+      [ [ 0, '{"id": "xxx-xxxxxxx", "name": "Hello, world"}' ] ],
+      [ [ 0, '{"id": "xxx-yyyyyyy", "name": "Joe Doe"}' ] ],
+      [ [ -1, '{"id": "lkasdjflkasjdf", "name": "Winston Churchill"}' ] ],
+      [ [ 0, '{"id": "abc", "name": "Merry Jane"}' ] ],
+    ]);
+  });
+
+  test('swap first and second lines', () => {
+    const src = [
+      '{"id": "xxx-xxxxxxx", "name": "Hello, world!!!!!!!!!!!!!!!!!!!!!!!!!"}',
+      '{"id": "xxx-yyyyyyy", "name": "Joe Doe"}',
+      '{"id": "lkasdjflkasjdf", "name": "Winston Churchill"}',
+      '{"id": "abc", "name": "Merry Jane"}',
+    ];
+    const dst = [
+      '{"id": "xxx-yyyyyyy", "name": "Joe Doe"}',
+      '{"id": "xxx-xxxxxxx", "name": "Hello, world!!!!!!!!!!!!!!!!!!!!!!!!!"}',
+      '{"id": "lkasdjflkasjdf", "name": "Winston Churchill"}',
+      '{"id": "abc", "name": "Merry Jane"}',
+    ];
+    const patch = line.diffLines(src, dst);
+    expect(patch).toEqual([
+      [ [ 1, '{"id": "xxx-yyyyyyy", "name": "Joe Doe"}' ] ],
+      [ [ 0, '{"id": "xxx-xxxxxxx", "name": "Hello, world!!!!!!!!!!!!!!!!!!!!!!!!!"}' ] ],
+      [ [ -1, '{"id": "xxx-yyyyyyy", "name": "Joe Doe"}' ] ],
+      [ [ 0, '{"id": "lkasdjflkasjdf", "name": "Winston Churchill"}' ] ],
+      [ [ 0, '{"id": "abc", "name": "Merry Jane"}' ] ],
+    ]);
+  });
 });
