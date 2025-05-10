5cfa827ef44974487c95762afba8906644fcafd7
Vadim Dalecky <streamich@gmail.com>
2025-05-10 22:19:14 +0200

    style(json-crdt): 💄 update demo os an issue

--- src/json-crdt/__demos__/issue-891.ts
+++ src/json-crdt/__demos__/issue-891.ts
@@ -10,15 +10,14 @@
 
 import {Model} from '..';
 
-
 const model = Model.create(void 0, 12345);
 
 model.api.root({});
 model.api.flush();
 const obj = model.api.obj([]);
 
-obj.set({ foo: 'bar' });
-const patch1 = model.api.flush()
+obj.set({foo: 'bar'});
+const patch1 = model.api.flush();
 console.log(patch1 + '');
 // Patch ..2345.3!5
 // ├─ new_str ..2345.3
@@ -26,8 +25,8 @@ console.log(patch1 + '');
 // └─ ins_obj ..2345.7!1, obj = ..2345.1
 //    └─ "foo": ..2345.3
 
-obj.set({ foo: 'bar' });
-const patch2 = model.api.flush()
+obj.set({foo: 'bar'});
+const patch2 = model.api.flush();
 console.log(patch2 + '');
 // Patch ..2345.8!5
 // ├─ new_str ..2345.8
@@ -35,6 +34,6 @@ console.log(patch2 + '');
 // └─ ins_obj ..2345.12!1, obj = ..2345.1
 //    └─ "foo": ..2345.8
 
-const patch3 = model.api.flush()
+const patch3 = model.api.flush();
 console.log(patch3 + '');
 // Patch (nil)!0
