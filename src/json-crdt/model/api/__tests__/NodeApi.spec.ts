cb55b308a6132b2830c0068a7884a99c78ca125e
streamich <streamich@gmail.com>
2025-06-28 16:54:14 +0200

    test(json-crdt): 💍 add API .find() "val" node resolution tests

--- src/json-crdt/model/api/__tests__/NodeApi.spec.ts
+++ src/json-crdt/model/api/__tests__/NodeApi.spec.ts
@@ -3,6 +3,41 @@ import {Model} from '../../Model';
 import {ConApi, ObjApi} from '../nodes';
 import {createTypedModel, createUntypedModel} from './fixtures';
 
+describe('.find()', () => {
+  test('can resolve multiple nested "val" nodes in the middle of path', () => {
+    const doc = Model.create();
+    doc.api.set(s.obj({
+      obj: s.val(s.val(s.val(s.obj({
+        key: s.str('value'),
+      }))))
+    }));
+    const node = doc.api.find(['obj', 'key']);
+    expect(node.view()).toBe('value');
+  });
+
+  test('can resolve multiple nested "val" nodes at the end of path', () => {
+    const doc = Model.create();
+    doc.api.set(s.obj({
+      obj: s.obj({
+        key: s.val(s.val(s.val(s.str('value')))),
+      })
+    }));
+    const node = doc.api.find(['obj', 'key']);
+    expect(node.view()).toBe('value');
+  });
+
+  test('can resolve multiple nested "val" nodes in the beginning of path', () => {
+    const doc = Model.create();
+    doc.api.set(s.val(s.val(s.val(s.obj({
+      obj: s.obj({
+        key: s.str('value'),
+      })
+    })))));
+    const node = doc.api.find(['obj', 'key']);
+    expect(node.view()).toBe('value');
+  });
+});
+
 describe('.read()', () => {
   test('can retrieve own view', () => {
     const doc = Model.create();
