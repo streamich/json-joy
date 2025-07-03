c134cf4cf943cbf6f18ef16a403ed0aec61404ec
streamich <streamich@gmail.com>
2025-07-03 17:22:56 +0200

    feat(json-patch-diff): 🎸 update diff API

--- src/json-crdt-diff/index.ts
+++ src/json-crdt-diff/index.ts
@@ -2,13 +2,14 @@ import type {Patch} from '../json-crdt-patch';
 import type {NodeApi} from '../json-crdt/model';
 import {JsonCrdtDiff} from './JsonCrdtDiff';
 
-export const diff = (src: NodeApi<any>, dst: unknown): Patch => {
+export const diff = (src: NodeApi<any>, dst: unknown): Patch | undefined => {
   const diff = new JsonCrdtDiff(src.api.model);
   const patch = diff.diff(src.node, dst);
-  return patch;
+  return patch.ops.length ? patch : void 0;
 };
 
-export const merge = (src: NodeApi<any>, dst: unknown): void => {
+export const merge = (src: NodeApi<any>, dst: unknown): Patch | undefined => {
   const patch = diff(src, dst);
-  if (patch.ops.length) src.api.model.applyPatch(patch);
+  if (patch) src.api.model.applyPatch(patch);
+  return patch;
 };
