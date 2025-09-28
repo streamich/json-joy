475a1d128749570b3997a497173999a9a5269f7e
streamich <streamich@gmail.com>
2025-09-28 17:37:52 +0200

    test: 💍 refactor test

--- src/json-path/__tests__/JsonPathEval.spec.ts
+++ src/json-path/__tests__/JsonPathEval.spec.ts
@@ -1,24 +1,30 @@
 import {JsonPathEval} from "../JsonPathEval";
 import {JsonPathParser} from "../JsonPathParser";
 
-test('basic object selection', () => {
-  const expr = '$.store.book[0].title';
-  const ast = JsonPathParser.parse(expr);
-  const data = {
-    store: {
-      book: [
-        { title: 'Harry Potter', author: 'J.K. Rowling' },
-        { title: 'The Hobbit', author: 'J.R.R. Tolkien' }
-      ]
-    }
-  };
-  const evaluator = new JsonPathEval(ast.path!, data);
-  const result = evaluator.eval();
-  expect(result).toMatchObject([
-    {
-      data: 'Harry Potter',
-    },
-  ]);
-  expect(result[0].path()).toEqual(['$', 'store', 'book', 0, 'title']);
-  expect(result[0].pointer()).toBe("$['store']['book'][0]['title']");
+const data0 = {
+  store: {
+    book: [
+      { title: 'Harry Potter', author: 'J.K. Rowling' },
+      { title: 'The Hobbit', author: 'J.R.R. Tolkien' }
+    ]
+  }
+};
+
+describe('selector', () => {
+  describe('named', () => {
+    test('basic object selection', () => {
+      const expr = '$.store.book[0].title';
+      const ast = JsonPathParser.parse(expr);
+      const evaluator = new JsonPathEval(ast.path!, data0);
+      const result = evaluator.eval();
+      expect(result).toMatchObject([
+        {
+          data: 'Harry Potter',
+        },
+      ]);
+      expect(result[0].path()).toEqual(['$', 'store', 'book', 0, 'title']);
+      expect(result[0].pointer()).toBe("$['store']['book'][0]['title']");
+    });
+  });
 });
+
