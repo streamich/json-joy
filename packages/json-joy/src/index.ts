50819955547df37c37934901271e021b1f0b7d9b
streamich <streamich@gmail.com>
2025-10-21 23:58:22 +0200

    fix: 🐛 correct build artifact output in json-joy main package

--- packages/json-joy/src/index.ts
+++ packages/json-joy/src/index.ts
@@ -12,4 +12,3 @@ export type * from './json-crdt';
 export type * from './json-crdt-patch';
 export type * from './json-crdt-extensions';
 export type * from './json-patch/types';
-export type * from '../../json-path/src/types';
