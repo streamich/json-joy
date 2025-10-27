aa7d164521a837b88aa94e1d6093e1de30606241
streamich <streamich@gmail.com>
2025-10-28 00:58:01 +0100

    feat(json-crdt): 🎸 create comparison function for CRDT nodes

--- packages/json-joy/src/json-crdt/equal/__tests__/index.automated.spec.ts
+++ packages/json-joy/src/json-crdt/equal/__tests__/index.automated.spec.ts
@@ -1,11 +1,11 @@
 import {Model} from '../../model';
-import {equalSchema} from '..';
+import {cmp} from '..';
 import {documents} from '../../../__tests__/json-documents';
 
 const assertSchemasEqual = (a: unknown, b: unknown): void => {
   const model1 = Model.create(a);
   const model2 = Model.create(b);
-  const result = equalSchema(model1.root, model2.root, false);
+  const result = cmp(model1.root, model2.root, false);
   expect(result).toBe(true);
 };
 
