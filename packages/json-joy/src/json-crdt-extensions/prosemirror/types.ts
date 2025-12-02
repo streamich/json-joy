39a336d4b3ca5d96161b4354b37b96b5ea1a62f0
streamich <streamich@gmail.com>
2025-12-02 19:16:37 +0100

    feat(json-crdt-peritext-ui): 🎸 implement ProseMirror format to Peritext converter

---
+++ packages/json-joy/src/json-crdt-extensions/prosemirror/types.ts
@@ -0,0 +1,44 @@
+import type {PeritextDataNode} from '../peritext/types';
+
+export type PmDataNode = PeritextDataNode;
+
+export interface PmJsonNode {
+  type: string;
+  attrs?: Record<string, unknown>;
+  content?: (PmJsonNode | PmJsonTextNode)[];
+  marks?: PmJsonMark[];
+}
+
+export interface PmJsonTextNode extends PmJsonNode {
+  text: string;
+}
+export interface PmJsonMark {
+  type: string;
+  attrs?: PmAttrs;
+}
+
+export type PmAttrs = Record<string, unknown>;
+
+export interface PmNode {
+  readonly type: PmNodeType;
+  readonly attrs?: PmAttrs;
+  readonly content?: PmFragment;
+  readonly marks?: readonly PmMark[];
+}
+
+export interface PmTextNode extends PmNode {
+  readonly text: string;
+}
+
+export interface PmNodeType {
+  readonly name: string;
+}
+
+export interface PmFragment {
+  readonly content: readonly (PmNode | PmTextNode)[];
+}
+
+export interface PmMark {
+  readonly type: PmNodeType;
+  readonly attrs?: PmAttrs;
+}
