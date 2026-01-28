1782b268396e6dc9265c51f21720294f760e1dcb
streamich <streamich@gmail.com>
2026-01-28 11:29:46 +0100

    feat: 🎸 improve demo preview component

--- packages/collaborative-input/src/__stories__/side-by-side.stories.tsx
+++ packages/collaborative-input/src/__stories__/side-by-side.stories.tsx
@@ -15,11 +15,31 @@ const schema0 = s.obj({
 
 const model = Model.create(schema0);
 
+const description = `
+This demo showcases synchronization of two text areas using
+the \`CollaborativeInput\` component. You can type in either text
+area, and the changes will be reflected in both areas after synchronization.
+Use the top bar buttons to manually synchronize or adjust auto-sync settings.
+
+The \`<CollaborartiveInput>\` React component binds a JSON CRDT "str" node to
+either \`<input>\` or \`<textarea>\` DOM element. You provide the JSON "str" node
+via the \`str\` prop, and a render prop \`input\` that receives a \`ref\` to be
+attached to the desired DOM element:
+
+\`\`\`jsx
+<CollaborativeInput
+  str={() => model.api.str(['text'])}
+  input={(connect) => <textarea ref={connect} />}
+/>
+\`\`\`
+`;
+
 export const Default = {
   render: () => (
     <DemoCard
       title={<Markdown inline src={'`CollaborativeInput` Component'} />}
       subtitle={<Markdown inline src={'Two-way synchronization of basic DOM `<input>` element rendered by `<CollaborativeInput>` React component'} />}
+      description={<Markdown src={description} />}
     >
       <SideBySideSync
         model={model}
--- packages/collaborative-ui/src/DemoCard/index.tsx
+++ packages/collaborative-ui/src/DemoCard/index.tsx
@@ -1,7 +1,11 @@
 import * as React from 'react';
-import {Text} from '@jsonjoy.com/ui/lib/1-inline/Text';
-import {Paper} from '@jsonjoy.com/ui/lib/4-card/Paper';
 import {rule} from 'nano-theme';
+import {Text} from '@jsonjoy.com/ui/lib/1-inline/Text';
+import {Split} from '@jsonjoy.com/ui/lib/3-list-item/Split';
+import {Paper} from '@jsonjoy.com/ui/lib/4-card/Paper';
+import {Iconista} from '@jsonjoy.com/ui/lib/icons/Iconista';
+import BasicButton from '@jsonjoy.com/ui/lib/2-inline-block/BasicButton';
+import {BasicTooltip} from '@jsonjoy.com/ui/lib/4-card/BasicTooltip';
 
 const blockClass = rule({
   // pd: '16px',
@@ -9,6 +13,7 @@ const blockClass = rule({
 
 const headerClass = rule({
   pd: '16px 16px 0',
+  bxz: 'border-box',
 });
 
 const bodyClass = rule({
@@ -25,28 +30,67 @@ const titleClass = rule({
 
 const subtitleClass = rule({
   pd: 0,
-  mr: '-2px 0 0',
+  mr: '2px 0 0',
   fz: '19px',
   op: 0.8,
-  lh: '1.8em',
+  lh: '1.5em',
+});
+
+const descriptionWrapClass = rule({
+  pd: '16px',
+  mr: '0 auto',
+});
+
+const descriptionClass = rule({
+  pd: '32px',
+  mr: '0 auto',
+  lh: '1.7em',
+  maxW: '800px',
 });
 
 export interface DemoCardProps {
   title?: React.ReactNode;
   subtitle?: React.ReactNode;
+  description?: React.ReactNode;
   children?: React.ReactNode;
 }
 
-export const DemoCard: React.FC<DemoCardProps> = ({title, subtitle, children}) => {
+export const DemoCard: React.FC<DemoCardProps> = ({title, subtitle, description, children}) => {
+  const [showDescription, setShowDescription] = React.useState(false);
+
   return (
     <Paper className={blockClass} fill={1} noOutline round>
-      {(!!title || !!subtitle) && (
+      {(!!title || !!subtitle || !!description) && (
         <div className={headerClass}>
-          <Text as='h3' font='sans' kind='bold' className={titleClass}>{title}</Text>
-          <Text as='h3' font='slab' kind='mid' className={subtitleClass}>{subtitle}</Text>
+          <Split style={{alignItems: 'center'}}>
+            <div>
+              {!!title && <Text as='h3' font='sans' kind='bold' className={titleClass}>{title}</Text>}
+              {!!subtitle && <Text as='h3' font='slab' kind='mid' className={subtitleClass}>{subtitle}</Text>}
+            </div>
+            <div>
+              {!!description && (
+                <BasicTooltip renderTooltip={() => (showDescription ? 'Hide description' : 'Show description')} nowrap>
+                  <BasicButton fill={showDescription} rounder size={40} onClick={() => setShowDescription(x => !x)}>
+                    <Iconista set='tabler' icon='book' width={16} height={16} />
+                  </BasicButton>
+                </BasicTooltip>
+              )}
+            </div>
+          </Split>
         </div>
       )}
-      <div className={bodyClass}>{children}</div>
+      {!!showDescription ? (
+        <div className={descriptionWrapClass}>
+          <Paper round>
+            <div className={descriptionClass}>
+              <Text as='div' font='sans' kind='mid' size={1} className={subtitleClass}>{description}</Text>
+              {/* {description} */}
+            </div>
+          </Paper>
+        </div>
+      ) : (
+        <div className={bodyClass}>{children}</div>
+      )}
     </Paper>
   );
 };
