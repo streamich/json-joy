59d426c9ddf8f5d916c3026a3055e9f6c7b51ccb
streamich <streamich@gmail.com>
2026-02-23 20:16:16 +0100

    feat(collaborative-quill): 🎸 add initial remote cursor support to Quill

--- packages/collaborative-quill-react/src/__stories__/side-by-side.stories.tsx
+++ packages/collaborative-quill-react/src/__stories__/side-by-side.stories.tsx
@@ -7,6 +7,7 @@ import {Markdown} from '@jsonjoy.com/ui/lib/markdown/Markdown';
 import Paper from '@jsonjoy.com/ui/lib/4-card/Paper';
 import {CollaborativeQuill} from '..';
 import type {Model} from 'json-joy/lib/json-crdt';
+import type {PresenceManager} from '@jsonjoy.com/collaborative-presence';
 
 const DESCRIPTION = `
 \`@jsonjoy.com/collaborative-quill-react\` integrates [json-joy](https://github.com/streamich/json-joy)
@@ -50,10 +51,15 @@ const Demo: React.FC = () => {
       <SideBySideSync
         model={model}
         noDisplayHdr
-        renderDisplay={(m: Model<any>) => (
+        renderDisplay={(m: Model<any>, _readonly: boolean, presence?: PresenceManager) => (
           <Paper style={{overflow: 'hidden', width: '100%'}} contrast>
             <div style={{width: '100%', height: 200}} onKeyDown={(e) => e.stopPropagation()}>
-              <CollaborativeQuill style={{height: '200px'}} api={() => (m as any).s.toExt()} />
+              <CollaborativeQuill
+                style={{height: '200px'}}
+                api={() => (m as any).s.toExt()}
+                presence={presence}
+                userFromMeta={(m: any) => m}
+              />
             </div>
           </Paper>
         )}
