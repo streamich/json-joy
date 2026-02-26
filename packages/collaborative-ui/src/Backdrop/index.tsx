cf4549edbe96312241c1b0114b6aac6e82bf2928
streamich <streamich@gmail.com>
2026-02-26 14:30:22 +0100

    feat(collaborative-ui): 🎸 implement <Backdrop> component

---
+++ packages/collaborative-ui/src/Backdrop/index.tsx
@@ -0,0 +1,69 @@
+import {rule} from 'nano-theme';
+import * as React from 'react';
+
+const blockClass = rule({
+  pos: 'relative',
+  overflowY: 'visible',
+  '& input': {
+    ff: 'inherit',
+    fz: 'inherit',
+    fw: 'inherit',
+  },
+});
+
+const backdropClass = rule({
+  col: 'inherit',
+  w: '100%',
+  bd: 0,
+  pad: '1px',
+  overflow: 'hidden',
+  pos: 'relative',
+  bxz: 'border-box',
+  ta: 'start',
+  whiteSpace: 'pre',
+});
+
+const inputClass = rule({
+  pos: 'absolute',
+  top: 0,
+  left: 0,
+  w: '100%',
+  bd: 0,
+  m: 0,
+  d: 'block',
+  pad: '1px',
+  fz: 'inherit',
+  ff: 'inherit',
+  letterSpacing: 'inherit',
+  bxz: 'border-box',
+  bgc: 'transparent',
+});
+
+export interface BackdropProps {
+  backdrop?: React.ReactNode;
+  value: string;
+  attr?: React.AllHTMLAttributes<HTMLDivElement>;
+  inputRef?: React.RefObject<HTMLInputElement | HTMLTextAreaElement>;
+  renderInput?: (className: string, props: BackdropProps) => React.ReactNode;
+}
+
+export const Backdrop: React.FC<BackdropProps> = (props) => {
+  const {value, attr, inputRef, renderInput} = props;
+
+  const backdrop = (
+    <div className={backdropClass}>
+      {props.backdrop}
+    </div>
+  );
+
+  const input = renderInput ? renderInput(inputClass, props) : (
+    <input ref={inputRef as React.RefObject<HTMLInputElement>} className={inputClass} value={value} />
+  );
+
+  return (
+    <div {...attr} className={(attr?.className || '') + blockClass}>
+      {backdrop}
+      {input}
+    </div>
+  );
+};
---
+++ packages/collaborative-ui/src/Backdrop/stories/index.stories.tsx
@@ -0,0 +1,27 @@
+import {Backdrop} from '..';
+
+export default {
+  component: Backdrop,
+  title: '<Backdrop>',
+  tags: ['autodocs'],
+};
+
+export const Default = {
+  args: {
+    value: '123',
+    backdrop: '123',
+  },
+};
+
+export const RedChar = {
+  args: {
+    value: '123',
+    backdrop: (
+      <>
+        1
+        <span style={{background: 'red'}}>2</span>
+        3
+      </>
+    ),
+  },
+};
