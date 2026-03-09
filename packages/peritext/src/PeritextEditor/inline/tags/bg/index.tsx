7d49e34842d6b6e88e25e1146defaa41636b7424
streamich <streamich@gmail.com>
2026-03-09 11:22:19 +0100

    feat(peritext): 🎸 reuse color elements in background component

--- packages/peritext/src/PeritextEditor/inline/tags/bg/index.tsx
+++ packages/peritext/src/PeritextEditor/inline/tags/bg/index.tsx
@@ -8,9 +8,7 @@ import {
 } from 'json-joy/lib/json-crdt-extensions';
 import {InlineSliceBehavior} from '../../InlineSliceBehavior';
 import {makeIcon} from '@jsonjoy.com/ui/lib/icons/Iconista';
-import {View} from './components/View';
-import {Edit} from './components/Edit';
-import {isValid} from '../../../util/color';
+import {behavior as col} from '../col';
 import type {IconProps, ValidationResult} from '../../InlineSliceBehavior';
 import type {ToolbarFormatting} from '../../../state/formattings';
 
@@ -46,28 +44,9 @@ export const behavior = new (class BgBehavior extends InlineSliceBehavior<
     icon: () => <Icon width={16} height={16} />,
   };
 
-  // TODO: Reuse all these...
-
-  public readonly validate = (formatting: ToolbarFormatting<any, any>): ValidationResult => {
-    const obj = formatting.conf()?.view() as Data;
-    if (!obj || typeof obj !== 'object') return [{code: 'INVALID_CONFIG'}];
-    const color = obj.col || '';
-    if (typeof color !== 'string' || !isValid(color)) return [{code: 'INVALID_COLOR'}];
-    if (color.length < 4) return 'empty';
-    return 'good';
-  };
-
-  public readonly previewText = (formatting: ToolbarFormatting<any, any>): string => {
-    const data = formatting.conf()?.view() as Data;
-    if (!data || typeof data !== 'object') return '';
-    return data.col || '';
-  };
-
-  public readonly renderIcon = ({formatting}: IconProps) => {
-    const color = String(formatting.conf()?.read('/col') || void 0);
-    return <span style={{backgroundColor: color, display: 'inline-block', width: 16, height: 16, borderRadius: 3}} />;
-  };
-
-  public readonly Edit = Edit;
-  public readonly View = View;
+  public readonly validate = col.validate;
+  public readonly previewText = col.previewText;
+  public readonly renderIcon = col.renderIcon;
+  public readonly Edit = col.Edit;
+  public readonly View = col.View;
 })();
