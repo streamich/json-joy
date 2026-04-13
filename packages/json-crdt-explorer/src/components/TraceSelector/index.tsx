112b8b2e7a64cf6768dd7f2d51ce259e1bdc83a9
streamich <streamich@gmail.com>
2026-04-13 19:08:47 +0200

    feat: 🎸 use structural menu

--- packages/json-crdt-explorer/src/components/TraceSelector/index.tsx
+++ packages/json-crdt-explorer/src/components/TraceSelector/index.tsx
@@ -1,8 +1,9 @@
 import * as React from 'react';
 import {Popup} from '@jsonjoy.com/ui/lib/4-card/Popup';
-import {ContextItem, ContextPane, ContextSep, ContextTitle} from '@jsonjoy.com/ui/lib/4-card/ContextMenu';
+import {ContextMenu} from '@jsonjoy.com/ui/lib/4-card/ContextMenu';
 import {Button} from '@jsonjoy.com/ui/lib/2-inline-block/Button';
 import {makeIcon} from '@jsonjoy.com/ui/lib/icons/Iconista';
+import type {MenuItem} from '@jsonjoy.com/ui/lib/4-card/StructuralMenu/types';
 import * as traces from './traces';
 import {useT} from 'use-t';
 import {useExplorer} from '../../context';
@@ -12,16 +13,16 @@ const VisTextIcon = makeIcon({set: 'elastic', icon: 'vis_text'});
 const HighlightIcon = makeIcon({set: 'ant_outline', icon: 'highlight'});
 const ShopIcon = makeIcon({set: 'ant_outline', icon: 'shop'});
 
-const icon = (trace: traces.TraceDefinition) => {
+const traceIcon = (trace: traces.TraceDefinition) => {
   switch (trace.type) {
     case 'json':
-      return <ApartmentIcon width={16} height={16} />;
+      return () => <ApartmentIcon width={16} height={16} />;
     case 'text':
-      return <VisTextIcon width={16} height={16} />;
+      return () => <VisTextIcon width={16} height={16} />;
     case 'rich-text':
-      return <HighlightIcon width={16} height={16} />;
+      return () => <HighlightIcon width={16} height={16} />;
     default:
-      return null;
+      return undefined;
   }
 };
 
@@ -42,47 +43,65 @@ export const TraceSelector: React.FC<TraceSelectorProps> = ({width = 240, expand
     setLoading(false);
   };
 
+  const menu: MenuItem = React.useMemo(() => ({
+    name: 'Load trace',
+    minWidth: width,
+    children: [
+      {
+        name: 'Rich-text',
+        expand: 4,
+        children: traces.quill.map((trace) => ({
+          id: trace.id,
+          name: trace.name,
+          icon: traceIcon(trace),
+          onSelect: () => load(trace),
+        })),
+      },
+      {sep: true, name: 'sep-1'},
+      {
+        name: 'Forms',
+        expand: 4,
+        children: traces.json.map((trace) => ({
+          id: trace.id,
+          name: trace.name,
+          icon: traceIcon(trace),
+          onSelect: () => load(trace),
+        })),
+      },
+      {sep: true, name: 'sep-2'},
+      {
+        name: t('Plain text'),
+        expand: 4,
+        children: traces.text.map((trace) => ({
+          id: trace.id,
+          name: trace.name,
+          icon: traceIcon(trace),
+          onSelect: () => load(trace),
+        })),
+      },
+      {sep: true, name: 'sep-3'},
+      {
+        name: t('Fuzzer'),
+        expand: 4,
+        children: traces.fuzzer.map((trace) => ({
+          id: trace.id,
+          name: trace.name,
+          icon: traceIcon(trace),
+          onSelect: () => load(trace),
+        })),
+      },
+    ],
+  }), [width, t]);
+
   return (
     <Popup
       block
       renderContext={() => (
-        <ContextPane style={{width: width + 8}}>
-          <ContextSep />
-          <ContextTitle>Rich-text</ContextTitle>
-          {traces.quill.map((trace) => (
-            <ContextItem closePopup key={trace.id} inset icon={icon(trace)} onClick={() => load(trace)}>
-              {trace.name}
-            </ContextItem>
-          ))}
-          <ContextSep />
-          <ContextSep line />
-          <ContextSep />
-          <ContextTitle>Forms</ContextTitle>
-          {traces.json.map((trace) => (
-            <ContextItem closePopup key={trace.id} inset icon={icon(trace)} onClick={() => load(trace)}>
-              {trace.name}
-            </ContextItem>
-          ))}
-          <ContextSep />
-          <ContextSep line />
-          <ContextSep />
-          <ContextTitle>{t('Plain text')}</ContextTitle>
-          {traces.text.map((trace) => (
-            <ContextItem closePopup key={trace.id} inset icon={icon(trace)} onClick={() => load(trace)}>
-              {trace.name}
-            </ContextItem>
-          ))}
-          <ContextSep />
-          <ContextSep line />
-          <ContextSep />
-          <ContextTitle>{t('Fuzzer')}</ContextTitle>
-          {traces.fuzzer.map((trace) => (
-            <ContextItem closePopup key={trace.id} inset icon={icon(trace)} onClick={() => load(trace)}>
-              {trace.name}
-            </ContextItem>
-          ))}
-          <ContextSep />
-        </ContextPane>
+        <ContextMenu
+          inset
+          menu={menu}
+          pane={{style: {width}}}
+        />
       )}
     >
       <Button
@@ -99,3 +118,4 @@ export const TraceSelector: React.FC<TraceSelectorProps> = ({width = 240, expand
     </Popup>
   );
 };
+
