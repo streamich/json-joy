3b81f3a5620c7cad2a30a806a32f7544fece42b5
streamich <streamich@gmail.com>
2026-04-21 18:39:29 +0200

    feat: 🎸 add mutxt logo in sidebar

--- packages/mutxt/src/components/LeftSidebar/BrandLogo.tsx
+++ packages/mutxt/src/components/LeftSidebar/BrandLogo.tsx
@@ -4,14 +4,27 @@ import {BasicTooltip} from '@jsonjoy.com/ui/src/4-card/BasicTooltip';
 
 const rootClass = rule({
   d: 'inline-flex',
+  h: '42px',
+  w: '48px',
+  mr: '0 0 4px',
   ai: 'center',
-  pd: '0 0 4px',
+  jc: 'center',
   us: 'none',
 });
 
 const muClass = rule({
   ...theme.font.serif.mid,
-  fz: '20.5px',
+  letterSpacing: '.05em',
+  d: 'inline-flex',
+  tr: 'translateY(1px)',
+  sub: {
+    fz: '15.3px',
+  },
+  trs: 'letter-spacing 0.1s ease, transform 0.1s ease',
+  [`.${rootClass.trim()}:hover &`]: {
+    letterSpacing: '-0.065em',
+    tr: 'translateY(4px)',
+  },
 });
 
 const txtClass = rule({
@@ -19,19 +32,64 @@ const txtClass = rule({
   fz: '22.5px',
 });
 
+const txt1Class = rule({
+  letterSpacing: '.03em',
+  d: 'inline-flex',
+  trs: 'letter-spacing 0.1s ease, transform 0.1s ease',
+  [`.${rootClass.trim()}:hover &`]: {
+    letterSpacing: '-0.03em',
+    tr: 'translateY(-2px)',
+  },
+});
+
+const txt2Class = rule({
+  letterSpacing: '.02em',
+  d: 'inline-flex',
+  trs: 'letter-spacing 0.1s ease, transform 0.1s ease',
+  [`.${rootClass.trim()}:hover &`]: {
+    letterSpacing: '0em',
+    tr: 'translateY(3px)',
+  },
+});
+
+const txt3Class = rule({
+  d: 'inline-flex',
+  trs: 'letter-spacing 0.1s ease, transform 0.1s ease',
+  [`.${rootClass.trim()}:hover &`]: {
+    letterSpacing: '.01em',
+    tr: 'translateY(8px)',
+  },
+});
+
+const buttonHoverClass = rule({
+  // ['&:hover .' + muClass.trim()]: {
+  //   letterSpacing: '0.05em',
+  // },
+  
+  // ['&:hover .' + txt2Class.trim()]: {
+  //   letterSpacing: '.01em',
+  //   // tr: 'translateY(3px)',
+  // },
+  // ['&:hover .' + txt3Class.trim()]: {
+  //   letterSpacing: '.01em',
+  //   // tr: 'translateY(8px)',
+  // },
+});
+
 export interface BrandLogoProps {}
 
 export const BrandLogo: React.FC<BrandLogoProps> = () => {
-  const label = '\\mu txt: Micro rich-text editor';
+  const label = '\\mu txt — Micro rich-text editor';
 
   return (
     <BasicTooltip nowrap renderTooltip={() => label}>
-      <span className={rootClass} role="img" aria-label={label}>
+      <span className={rootClass + buttonHoverClass} role="img" aria-label={label}>
         <span className={muClass}><sub>μ</sub></span>
+        {/* <span className={mu2Class}>mu</span> */}
         <span className={txtClass}>
-          <span style={{letterSpacing: '.03em'}}>t</span>
-          <span style={{letterSpacing: '.01em'}}>x</span>
-          <span>t</span>
+          <span className={txt1Class}>t</span>
+          <span className={txt2Class}>x</span>
+          <span className={txt3Class}>t</span>
         </span>
       </span>
     </BasicTooltip>
