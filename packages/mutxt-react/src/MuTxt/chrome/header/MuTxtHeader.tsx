d5917a6cc0a38648bdd157ab9a573d050092f0bc
streamich <streamich@gmail.com>
2026-05-04 16:09:14 +0200

    feat: 🎸 improve header presentation

--- packages/mutxt-react/src/MuTxt/chrome/header/MuTxtHeader.tsx
+++ packages/mutxt-react/src/MuTxt/chrome/header/MuTxtHeader.tsx
@@ -1,19 +1,14 @@
 import * as React from 'react';
 import {rule} from 'nano-theme';
 import {useStyles} from '@jsonjoy.com/ui/lib/styles/context';
-import {Iconista} from '@jsonjoy.com/ui/lib/icons/Iconista';
-import {ToolbarItem} from '@jsonjoy.com/ui/lib/4-card/Toolbar/ToolbarItem';
 import {ToolbarMenu} from '@jsonjoy.com/ui/lib/4-card/Toolbar/ToolbarMenu';
 import {AutoExpandableToolbar} from '@jsonjoy.com/ui/lib/4-card/Toolbar/ToolbarMenu/AutoExpandableToolbar';
 import {ToolbarSep} from '@jsonjoy.com/ui/lib/4-card/Toolbar/ToolbarSep';
 import type {Editor} from 'slate';
-import {ACTION_BUTTONS, canRedo, canUndo, redo, undo} from '../../behavior';
 import {Split} from '@jsonjoy.com/ui/lib/3-list-item/Split';
 import {DocumentOutlineButton} from '../../chrome/DocumentOutlineButton';
 import {useMuTxt} from '../../context';
-import {formatKeys} from '../../util/keys';
 import {SetNamedTrace} from '@jsonjoy.com/ui';
-import type {MenuItem} from '../../types';
 
 const HEIGHT = 48;
 
@@ -34,11 +29,9 @@ const toolbarContainerClass = rule({
 
 export interface MuTxtHeaderProps {
   editor: Editor;
-  readOnly?: boolean;
-  onVisualChange: () => void;
 }
 
-export const MuTxtHeader: React.FC<MuTxtHeaderProps> = ({editor, readOnly, onVisualChange}) => {
+export const MuTxtHeader: React.FC<MuTxtHeaderProps> = ({editor}) => {
   const mutxt = useMuTxt();
   const availableWidth = mutxt.sizer.width.use();
   const desiredWidth = mutxt.sizer.content.use();
@@ -46,69 +39,6 @@ export const MuTxtHeader: React.FC<MuTxtHeaderProps> = ({editor, readOnly, onVis
   mutxt.version.use();
 
   const width = Math.min(availableWidth, desiredWidth);
-
-  const execute = React.useCallback(
-    (callback: () => void): React.MouseEventHandler =>
-      (event) => {
-        event.preventDefault();
-        if (readOnly) return;
-        callback();
-        onVisualChange();
-      },
-    [onVisualChange, readOnly],
-  );
-
-  const renderItem = React.useCallback(
-    ({
-      key,
-      title,
-      iconSet,
-      icon,
-      shortcut,
-      active,
-      disabled,
-      onMouseDown,
-    }: {
-      key: string;
-      title: string;
-      iconSet: string;
-      icon: string;
-      shortcut?: string;
-      active?: boolean;
-      disabled?: boolean;
-      onMouseDown: React.MouseEventHandler;
-    }) => (
-      <ToolbarItem
-        key={key}
-        type="button"
-        // fill
-        // outline
-        // title={title}
-        selected={active}
-        disabled={disabled}
-        onMouseDown={onMouseDown}
-        tooltip={{nowrap: true, renderTooltip: () => title, shortcut: shortcut}}
-      >
-        <Iconista set={iconSet as any} icon={icon as any} width={16} height={16} />
-      </ToolbarItem>
-    ),
-    [],
-  );
-
-  const renderMenuItem = React.useCallback((item: MenuItem) => {
-    return (
-      <ToolbarItem
-        key={item.id ?? item.name}
-        type="button"
-        selected={!!item.active?.getSnapshot()}
-        disabled={!!item.disabled?.getSnapshot()}
-        onMouseDown={item.onSelect}
-        tooltip={{nowrap: true, renderTooltip: () => item.name, shortcut: item.keys ? formatKeys(item.keys) : void 0}}
-      >
-        {item.icon?.()}
-      </ToolbarItem>
-    );
-  }, []);
 
   const inlineMenu = mutxt.inline.menu.buildToolbarMenu();
   const voidsMenu = mutxt.voids.menu.buildToolbarMenu();
@@ -158,20 +88,10 @@ export const MuTxtHeader: React.FC<MuTxtHeaderProps> = ({editor, readOnly, onVis
         <div className={toolbarContainerClass}>
           <DocumentOutlineButton editor={editor} contentWidth={300} />
           <ToolbarSep line />
-          <ToolbarMenu menu={mutxt.docMenu.buildHeaderToolbar()} pane={{transparent: true, inline: true}} more={{small: true}} />
-          {width > 1000 && <ToolbarSep line />}
-          {width > 1000 &&
-            renderItem({
-              ...ACTION_BUTTONS[0],
-              disabled: readOnly || !canUndo(editor),
-              onMouseDown: execute(() => undo(editor)),
-            })}
-          {width > 1000 &&
-            renderItem({
-              ...ACTION_BUTTONS[1],
-              disabled: readOnly || !canRedo(editor),
-              onMouseDown: execute(() => redo(editor)),
-            })}
+          <ToolbarMenu
+            pane={{transparent: true, inline: true}}
+            menu={mutxt.docMenu.buildHeaderToolbar()}
+          />
         </div>
       </Split>
     </SetNamedTrace>
