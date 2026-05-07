76c5a91f89bc914c422f3579baf884dbc71e43db
streamich <streamich@gmail.com>
2026-05-07 12:21:10 +0200

    feat: 🎸 support dark mode in 5-block components

--- packages/ui/src/5-block/CommandPalette/CommandPaletteList/CommandPaletteTitle.tsx
+++ packages/ui/src/5-block/CommandPalette/CommandPaletteList/CommandPaletteTitle.tsx
@@ -1,16 +1,16 @@
 import * as React from 'react';
-import {lightTheme as theme, rule, useTheme} from 'nano-theme';
+import {makeRule, useTheme} from 'nano-theme';
 
-const blockClass = rule({
-  ...theme.font.ui3,
-  col: theme.g(0.4),
+const useBlockClass = makeRule((t) => ({
+  ...t.font.ui3,
+  col: t.g(0.4),
   fz: '12px',
   d: 'block',
   pad: '8px 24px',
   mar: 0,
   textTransform: 'uppercase',
   letterSpacing: '1px',
-});
+}));
 
 export interface CommandPaletteTitleProps {
   contrast?: boolean;
@@ -19,6 +19,7 @@ export interface CommandPaletteTitleProps {
 
 export const CommandPaletteTitle: React.FC<CommandPaletteTitleProps> = ({contrast, children}) => {
   const theme = useTheme();
+  const blockClass = useBlockClass();
 
   return (
     <h5 className={blockClass} style={{color: contrast ? theme.g(0) : undefined}}>
--- packages/ui/src/5-block/CommandPalette/CommandPaletteSeparator.tsx
+++ packages/ui/src/5-block/CommandPalette/CommandPaletteSeparator.tsx
@@ -1,20 +1,21 @@
 import * as React from 'react';
-import {rule} from 'nano-theme';
+import {makeRule} from 'nano-theme';
 
 const height = 64;
 
-const blockClass = rule({
+const useBlockClass = makeRule((t) => ({
   w: '100%',
   h: `${height}px`,
-  bd: '1px solid red',
+  bd: `1px solid ${t.g(0, 0.1)}`,
   bxz: 'border-box',
   pad: '0 16px',
-});
+}));
 
 export interface CommandPaletteInputProps {
   value: React.ReactNode;
 }
 
 export const CommandPaletteInput: React.FC<CommandPaletteInputProps> = ({value}) => {
+  const blockClass = useBlockClass();
   return <div className={blockClass}>{value}</div>;
 };
