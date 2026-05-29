650e730a541ddc1a4ff647ca66a076b5396f50cd
streamich <streamich@gmail.com>
2026-05-30 01:16:46 +0200

    chore: remove old site package

--- packages/site/routes.ts
+++
@@ -1,8 +0,0 @@
-/**
- * All public URL paths that the prerender script will generate static HTML for.
- * Add a new entry here whenever a new page is added to the site.
- */
-export const routes: string[] = [
-  '/',
-  '/docs',
-];
